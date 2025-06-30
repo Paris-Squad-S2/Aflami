64a0149e6982543ec1e05a214fedc60f7cc95211
IslamMagd <eslam752001.magdy54@gmail.com>
2025-06-30 06:50:33 +0300

    add Text style

---
+++ designsystem/src/main/java/com/paris_2/aflami/designsystem/text_style/Font.kt
@@ -0,0 +1,12 @@
+package com.paris_2.aflami.designsystem.text_style
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.paris_2.aflami.designsystem.R
+
+val popPins = FontFamily(
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold),
+)
