ed8abc03e2434e32fa40dcddd86570d74e97fc90
Renad_Alalfy <renadalalfy@gmail.com>
2025-07-01 17:15:46 +0300

    refactor icons tint

--- designsystem/src/main/java/com/paris_2/aflami/designsystem/components/TopAppBar.kt
+++ designsystem/src/main/java/com/paris_2/aflami/designsystem/components/TopAppBar.kt
@@ -36,6 +36,7 @@ data class IconItem(
     val icon: ImageVector,
     val onClick: (() -> Unit)? = null,
     val backgroundColor: Color,
+    val tint: Color,
 )
 
 @Composable
@@ -43,11 +44,13 @@ fun iconItemWithDefaults(
     icon: ImageVector,
     onClick: (() -> Unit)? = null,
     backgroundColor: Color = Theme.colors.surfaceHigh,
+    tint: Color = Theme.colors.text.title,
 ): IconItem {
     return IconItem(
         icon = icon,
         onClick = onClick,
-        backgroundColor = backgroundColor
+        backgroundColor = backgroundColor,
+        tint = tint
     )
 }
 
@@ -62,7 +65,6 @@ fun TopAppBar(
     trailingIcons: List<IconItem> = emptyList(),
     scrollBehavior: TopAppBarScrollBehavior? = null,
 ) {
-
     val scrollModifier = scrollBehavior?.let {
         Modifier.nestedScroll(it.nestedScrollConnection)
     } ?: Modifier
@@ -75,7 +77,6 @@ fun TopAppBar(
             .padding(horizontal = 12.dp),
         contentAlignment = Alignment.CenterStart
     ) {
-
         Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier.fillMaxSize()
@@ -85,7 +86,7 @@ fun TopAppBar(
             }
 
             logo?.let {
-                IconBox(it, tint = Color.Unspecified,)
+                IconBox(it, modifier = Modifier, tint = Color.Unspecified)
             }
 
             Column(
@@ -97,11 +98,7 @@ fun TopAppBar(
                 title?.let {
                     Text(
                         text = it,
-                        style = if (subtitle != null) {
-                            Theme.textStyle.label.medium
-                        } else {
-                            Theme.textStyle.title.large
-                        },
+                        style = if (subtitle != null) Theme.textStyle.label.medium else Theme.textStyle.title.large,
                         color = Theme.colors.text.title,
                         maxLines = 1,
                     )
@@ -114,7 +111,6 @@ fun TopAppBar(
                         maxLines = 1,
                     )
                 }
-
             }
 
             trailingIcons.forEachIndexed { index, iconItem ->
@@ -128,7 +124,7 @@ fun TopAppBar(
 @Composable
 private fun IconBox(
     iconItem: IconItem,
-    tint: Color = Theme.colors.text.title,
+    tint: Color = iconItem.tint,
     modifier: Modifier = Modifier,
 ) {
     Box(
@@ -164,87 +160,70 @@ fun PreviewTopAppBar() {
             .background(color = Theme.colors.surface)
             .padding(bottom = 16.dp)
     ) {
-        Box(
-            Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-        ) {
-            TopAppBar(
-                title = "AFLAMI",
-                subtitle = "More than just watching.",
-                logo = iconItemWithDefaults(
-                    ImageVector.vectorResource(R.drawable.ic_aflami_logo), {},
-                    Theme.colors.primaryVariant
+        TopAppBar(
+            title = "AFLAMI",
+            subtitle = "More than just watching.",
+            logo = iconItemWithDefaults(
+                ImageVector.vectorResource(R.drawable.ic_aflami_logo), {},
+                Theme.colors.primaryVariant,
+            ),
+            trailingIcons = listOf(
+                iconItemWithDefaults(
+                    ImageVector.vectorResource(R.drawable.ic_search),
+                    {},
+                    Theme.colors.primaryVariant,
+                    tint = Theme.colors.text.body
                 ),
-                trailingIcons = listOf(
-                    iconItemWithDefaults(
-                        ImageVector.vectorResource(R.drawable.ic_search),
-                        {},
-                        Theme.colors.primaryVariant,
-                    ),
-                )
             )
-        }
+        )
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        Box(
-            Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-        ) {
-            TopAppBar(
-                title = "My Account",
-                leadingIcons = listOf(
-                    iconItemWithDefaults(
-                        ImageVector.vectorResource(R.drawable.ic_back),
-                        {})
+        TopAppBar(
+            title = "My Account",
+            leadingIcons = listOf(
+                iconItemWithDefaults(
+                    ImageVector.vectorResource(R.drawable.ic_back),
+                    {},
+                )
+            ),
+            trailingIcons = listOf(
+                iconItemWithDefaults(
+                    ImageVector.vectorResource(R.drawable.ic_sort),
+                    {},
+                )
+            )
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TopAppBar(
+            title = "My Account",
+            trailingIcons = listOf(
+                iconItemWithDefaults(
+                    ImageVector.vectorResource(R.drawable.ic_sort),
+                )
+            )
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TopAppBar(title = "Settings")
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TopAppBar(
+            title = "My Account",
+            trailingIcons = listOf(
+                iconItemWithDefaults(
+                    ImageVector.vectorResource(R.drawable.ic_star),
+                    {},
                 ),
-                trailingIcons = listOf(
-                    iconItemWithDefaults(
-                        ImageVector.vectorResource(R.drawable.ic_sort),
-                        {})
+                iconItemWithDefaults(
+                    ImageVector.vectorResource(R.drawable.ic_sort),
+                    {},
                 )
             )
-        }
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Box(
-            Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-        ) {
-            TopAppBar(
-                title = "My Account",
-                trailingIcons = listOf(iconItemWithDefaults(ImageVector.vectorResource(R.drawable.ic_sort)))
-            )
-        }
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Box(
-            Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-        ) {
-            TopAppBar(title = "Settings")
-        }
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Box(
-            Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-        ) {
-            TopAppBar(
-                title = "My Account",
-                trailingIcons = listOf(
-                    iconItemWithDefaults(ImageVector.vectorResource(R.drawable.ic_star), {}),
-                    iconItemWithDefaults(ImageVector.vectorResource(R.drawable.ic_sort), {})
-                )
-            )
-        }
+        )
     }
 }
