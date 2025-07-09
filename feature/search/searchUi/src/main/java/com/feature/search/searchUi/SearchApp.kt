8f616692c85215671ca9c6c88d3eaee38f7c0c22
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-07-10 00:20:53 +0300

    integrate remote search datasource and configure network module

--- feature/search/searchUi/src/main/java/com/feature/search/searchUi/SearchApp.kt
+++ feature/search/searchUi/src/main/java/com/feature/search/searchUi/SearchApp.kt
@@ -1,6 +1,8 @@
 package com.feature.search.searchUi
 
 import android.app.Application
+import com.example.search.di.SearchModule
+import com.feature.search.searchUi.di.NetworkModule
 import com.feature.search.searchUi.di.dataSourceModule
 import com.feature.search.searchUi.di.roomModule
 import com.feature.search.searchUi.di.viewModelModule
@@ -15,7 +17,7 @@ class SearchApp : Application() {
         startKoin {
             androidLogger()
             androidContext(this@SearchApp)
-            modules(viewModelModule, roomModule,dataSourceModule)
+            modules(viewModelModule, roomModule,dataSourceModule , NetworkModule , SearchModule)
         }
     }
 }
