bcd0a27167b605eb40833839c673210941c62a3c
mohammed-akkad <momoo362514@gmail.com>
2025-07-09 14:53:19 +0300

    update history local data source

--- datasource/local/search/src/main/java/com/datasource/local/search/datasource/HistoryLocalDataSourceImpl.kt
+++ datasource/local/search/src/main/java/com/datasource/local/search/datasource/HistoryLocalDataSourceImpl.kt
@@ -9,22 +9,17 @@ class HistoryLocalDataSourceImpl(
 ) : HistoryLocalDataSource {
     override suspend fun addSearchQuery(title: String) {
         val entity = SearchHistoryEntity(
-            searchTitle = title
+            searchQuery = title
         )
         dao.addSearchQuery(entity)
     }
 
     override suspend fun getAllSearchQueries(): List<SearchHistoryEntity> {
-        return dao.getAllSearchQueries().map { entity ->
-            SearchHistoryEntity(
-                id = entity.id,
-                searchTitle = entity.searchTitle
-            )
-        }
+        return dao.getAllSearchQueries()
     }
 
-    override suspend fun clearSearchQueryById(id: Long) {
-        dao.clearSearchQueryById(id)
+    override suspend fun clearSearchQueryByQuery(query: String) {
+        dao.clearSearchQueryByQuery(query)
     }
 
     override suspend fun clearAll() {
