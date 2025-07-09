2713d7d7e783957155cbf0e0e35211b96c430058
mohammed-akkad <momoo362514@gmail.com>
2025-07-09 15:26:02 +0300

    refactor search converter

--- datasource/local/search/src/main/java/com/datasource/local/search/SearchConverter.kt
+++ datasource/local/search/src/main/java/com/datasource/local/search/SearchConverter.kt
@@ -13,12 +13,14 @@ class SearchConverter {
     private val listSerializer = ListSerializer(String.Companion.serializer())
 
     @TypeConverter
-    fun fromStringToListOfString(value: String): List<String> =
-        json.decodeFromString(listSerializer, value)
+    fun fromStringToListOfString(value: String?): List<String>? {
+        return value?.let { json.decodeFromString(listSerializer, it) } ?: emptyList()
+    }
 
     @TypeConverter
-    fun fromListOfStringToString(list: List<String>): String =
-        json.encodeToString(listSerializer, list)
+    fun fromListOfStringToString(list: List<String>?): String? {
+        return list?.let { json.encodeToString(listSerializer, it) }
+    }
 
     @TypeConverter
     fun fromEnumToString(mediaTypeEntity: MediaTypeEntity): String = mediaTypeEntity.name
@@ -27,23 +29,23 @@ class SearchConverter {
     fun fromStringToEnum(type: String): MediaTypeEntity = MediaTypeEntity.valueOf(type)
 
     @TypeConverter
-    fun fromLocalDate(date: LocalDate): String {
-        return date.toString()
+    fun fromLocalDate(date: LocalDate?): String? {
+        return date?.toString()
     }
 
     @TypeConverter
-    fun toLocalDate(dateString: String): LocalDate {
-        return LocalDate.Companion.parse(dateString)
+    fun toLocalDate(dateString: String?): LocalDate? {
+        return dateString?.let { LocalDate.Companion.parse(it) }
     }
 
     @TypeConverter
-    fun fromLocalDateTime(date: LocalDateTime): String {
-        return date.toString()
+    fun fromLocalDateTime(date: LocalDateTime?): String? {
+        return date?.toString()
     }
 
     @TypeConverter
-    fun toLocalDateTime(dateString: String): LocalDateTime {
-        return LocalDateTime.Companion.parse(dateString)
+    fun toLocalDateTime(dateString: String?): LocalDateTime? {
+        return dateString?.let {  LocalDateTime.Companion.parse(it) }
     }
 
 }
