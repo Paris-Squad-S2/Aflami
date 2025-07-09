f0a7cde0319daa6f76bd4a14680b4b31d496fc98
mohammed-akkad <momoo362514@gmail.com>
2025-07-09 14:55:17 +0300

    update media entity

--- repository/search/src/main/java/com/repository/search/entity/MediaEntity.kt
+++ repository/search/src/main/java/com/repository/search/entity/MediaEntity.kt
@@ -1,13 +1,22 @@
 package com.repository.search.entity
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import kotlinx.datetime.LocalDate
 
-@Entity(tableName = "media_table")
+@Entity(tableName = "media_table",foreignKeys = [
+    ForeignKey(
+        entity = SearchHistoryEntity::class,
+        parentColumns = ["search_query"],
+        childColumns = ["searchQuery"],
+        onDelete = ForeignKey.CASCADE
+    )
+])
 data class MediaEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
+    val searchQuery: String,
     val imageUri: String,
     val title: String,
     val type: MediaTypeEntity,
