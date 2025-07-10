45bceac79ec61cfa6a1efbb2387d731fe15d3dce
IslamMagd <eslam752001.magdy54@gmail.com>
2025-07-10 11:30:47 +0300

    implement getAllCategories method in CountryRepositoryImpl

--- repository/search/src/main/java/com/repository/search/mapper/SearchRemoteMapper.kt
+++ repository/search/src/main/java/com/repository/search/mapper/SearchRemoteMapper.kt
@@ -1,16 +1,24 @@
 package com.repository.search.mapper
 
+import com.domain.search.model.CategoryModel
 import com.domain.search.model.Media
 import com.example.search.mapper.toMediaOrNull
 import com.example.search.models.Genre
-import com.example.search.models.GenreDto
 import com.example.search.models.SearchDto
-import com.repository.search.entity.CountryEntity
 
 fun SearchDto.toMediaList(): List<Media> {
     return this.results?.mapNotNull { it.toMediaOrNull() } ?: emptyList()
 }
 
-fun List<Genre>.toCountriesName(): List<String> = this.map { it.name }
+fun Genre.toCategoryModel(): CategoryModel {
+    return CategoryModel(
+        id = this.id,
+        name = this.name
+    )
+}
+
+fun List<Genre>.toCategoryModels(): List<CategoryModel> {
+    return this.map { it.toCategoryModel() }
+}
 
 
--- repository/search/src/main/java/com/repository/search/repository/CountryRepositoryImpl.kt
+++ repository/search/src/main/java/com/repository/search/repository/CountryRepositoryImpl.kt
@@ -1,27 +1,21 @@
 package com.repository.search.repository
 
 import com.domain.search.repository.CountryRepository
-import com.example.search.GenresRemoteDataSource
 import com.repository.search.NetworkConnectionChecker
 import com.repository.search.dataSource.CountriesLocalDataSource
 import com.repository.search.entity.CountryEntity
-import com.repository.search.mapper.toCountriesName
 
 class CountryRepositoryImpl(
     private val networkConnectionChecker: NetworkConnectionChecker,
     private val countriesLocalDataSource: CountriesLocalDataSource,
-    private val genresRemoteDataSource: GenresRemoteDataSource
+    // private val countriesRemoteDataSource: CountriesRemoteDataSource,
 ): CountryRepository {
 
     override suspend fun getAllCountries(): List<String> {
-        return try {
-            return if (networkConnectionChecker.isConnected.value)
-                genresRemoteDataSource.getAllGenres().genres.toCountriesName()
-            else
-                countriesLocalDataSource.getCountries().toCountriesName()
-        } catch (e: Exception) {
-            throw e
-        }
+        return if (networkConnectionChecker.isConnected.value)
+            TODO()
+        else
+            countriesLocalDataSource.getCountries().toCountriesName()
     }
 }
 
