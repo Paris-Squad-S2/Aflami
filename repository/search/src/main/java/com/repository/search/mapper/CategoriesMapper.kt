cf7595511c489eb04c67e8320198999b406fd148
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-07-10 17:46:58 +0300

    refactor CategoriesMapper to use GenreDto instead of Genre

--- repository/search/src/main/java/com/repository/search/mapper/CategoriesMapper.kt
+++ repository/search/src/main/java/com/repository/search/mapper/CategoriesMapper.kt
@@ -1,14 +1,12 @@
 package com.repository.search.mapper
 
 import com.domain.search.model.CategoryModel
-import com.repository.search.dto.Genre
+import com.repository.search.dto.GenreDto
 import com.repository.search.entity.GenreEntity
-
 
 fun List<GenreEntity>.toCategories(): List<CategoryModel> {
     return this.map { it.toCategoryModel() }
 }
-
 
 fun GenreEntity.toCategoryModel(): CategoryModel {
     return CategoryModel(
@@ -17,9 +15,9 @@ fun GenreEntity.toCategoryModel(): CategoryModel {
     )
 }
 
-fun Genre.toEntity(): GenreEntity {
+fun GenreDto.toEntity(): GenreEntity {
     return GenreEntity(
         id = this.id.toString(),
-        name = this.name
+        name = this.name.orEmpty()
     )
 }
