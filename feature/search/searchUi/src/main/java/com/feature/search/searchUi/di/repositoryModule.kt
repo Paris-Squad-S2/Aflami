f1bc2ec851e3ec1d72bdbeabb9a833ce7cce47f1
Mustafa Ibrahim <118114457+MustafaIbrahim96@users.noreply.github.com>
2025-07-10 22:05:12 +0300

    handle di and replace repo fake

--- feature/search/searchUi/src/main/java/com/feature/search/searchUi/di/repositoryModule.kt
+++ feature/search/searchUi/src/main/java/com/feature/search/searchUi/di/repositoryModule.kt
@@ -1,19 +1,19 @@
 package com.feature.search.searchUi.di
 
 import com.domain.search.repository.CategoriesRepository
-import com.domain.search.repository.CategoriesRepositoryFakeImpl
 import com.domain.search.repository.CountryRepository
-import com.domain.search.repository.CountryRepositoryFakeImpl
 import com.domain.search.repository.SearchHistoryRepository
-import com.domain.search.repository.SearchHistoryRepositoryFakeImpl
 import com.domain.search.repository.SearchMediaRepository
-import com.domain.search.repository.SearchMediaRepositoryFakeImpl
+import com.repository.search.repository.CategoriesRepositoryImpl
+import com.repository.search.repository.CountryRepositoryImpl
+import com.repository.search.repository.SearchHistoryRepositoryImpl
+import com.repository.search.repository.SearchMediaRepositoryImpl
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    single<SearchHistoryRepository> { SearchHistoryRepositoryFakeImpl() }
-    single<CategoriesRepository> { CategoriesRepositoryFakeImpl() }
-    single<CountryRepository> { CountryRepositoryFakeImpl() }
-    single<SearchMediaRepository> { SearchMediaRepositoryFakeImpl() }
+    single<SearchHistoryRepository> { SearchHistoryRepositoryImpl(get()) }
+    single<CategoriesRepository> { CategoriesRepositoryImpl(get(), get(), get()) }
+    single<CountryRepository> { CountryRepositoryImpl(get()) }
+    single<SearchMediaRepository> { SearchMediaRepositoryImpl(get(), get(), get(), get()) }
 
 }
