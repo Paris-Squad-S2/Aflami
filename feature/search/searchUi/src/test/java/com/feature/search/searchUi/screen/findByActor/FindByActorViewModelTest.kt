49858f4e14353f44cc48f63d03ca8e3369c0d521
IslamMagd <eslam752001.magdy54@gmail.com>
2025-07-11 01:06:22 +0300

    setUp FindByActorViewModelTest.kt with test initial state

--- feature/search/searchUi/src/test/java/com/feature/search/searchUi/screen/findByActor/FindByActorViewModelTest.kt
+++ feature/search/searchUi/src/test/java/com/feature/search/searchUi/screen/findByActor/FindByActorViewModelTest.kt
@@ -1,19 +1,56 @@
 package com.feature.search.searchUi.screen.findByActor
 
+import com.domain.search.useCases.AddRecentSearchUseCase
+import com.domain.search.useCases.GetMediaByActorNameUseCase
+import com.google.common.truth.Truth.assertThat
+import io.mockk.coVerify
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class FindByActorViewModelTest {
-    @Test
-    fun onSearchQueryChange() {
+    private lateinit var viewModel: FindByActorViewModel
+    private lateinit var getMediaByActorNameUseCase: GetMediaByActorNameUseCase
+    private lateinit var addRecentSearchesUseCase: AddRecentSearchUseCase
+
+    private val testDispatcher = StandardTestDispatcher()
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @BeforeEach
+    fun setup() {
+        Dispatchers.setMain(testDispatcher)
+        getMediaByActorNameUseCase = mockk(relaxed = true)
+        addRecentSearchesUseCase = mockk(relaxed = true)
+        viewModel = FindByActorViewModel(
+            getMediaByActorNameUseCase = getMediaByActorNameUseCase,
+            addRecentSearchesUseCase = addRecentSearchesUseCase
+        )
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @AfterEach
+    fun tearDown() {
+        Dispatchers.resetMain()
     }
 
     @Test
-    fun onRetrySearchQuery() {
-    }
+    fun `initial state should be correct`() {
+        val initialState = viewModel.screenState.value
 
-    @Test
-    fun onMediaCardClick() {
+        assertThat(initialState.uiState.searchQuery).isEqualTo("")
+        assertThat(initialState.uiState.searchResult).isEmpty()
+        assertThat(initialState.isLoading).isFalse()
+        assertThat(initialState.errorMessage).isNull()
     }
 
 }
