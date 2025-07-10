1e1af928b5607dd3c76ff73e1db4b61d2948092f
Renad_Alalfy <renadalalfy@gmail.com>
2025-07-10 22:17:46 +0300

    add test for GetAllRecentSearchesUseCase

---
+++ domain/search/src/test/java/com/domain/search/useCases/GetAllRecentSearchesUseCaseTest.kt
@@ -0,0 +1,57 @@
+package com.domain.search.useCases
+
+import com.domain.search.model.SearchHistoryModel
+import com.domain.search.repository.SearchHistoryRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class GetAllRecentSearchesUseCaseTest {
+
+    private lateinit var searchHistoryRepository: SearchHistoryRepository
+    private lateinit var useCase: GetAllRecentSearchesUseCase
+
+    @BeforeEach
+    fun setUp() {
+        searchHistoryRepository = mockk()
+        useCase = GetAllRecentSearchesUseCase(searchHistoryRepository)
+    }
+
+    @Test
+    fun `invoke should return all recent searches from repository`() = runTest {
+
+        //Given
+        val recentSearches = listOf(
+            SearchHistoryModel(id = 1, searchTitle = "Movie1"),
+            SearchHistoryModel(id = 2, searchTitle = "Movie2")
+        )
+
+        coEvery { searchHistoryRepository.getAllSearchHistory() } returns recentSearches
+
+        //When
+        val result = useCase()
+
+        //Then
+        assertEquals(2, result.size)
+        assertEquals(recentSearches, result)
+    }
+
+
+    @Test
+    fun `invoke should return empty list when repository returns no recent searches`() = runTest {
+
+        //Given
+        coEvery { searchHistoryRepository.getAllSearchHistory() } returns emptyList()
+
+        //When
+        val result = useCase()
+
+        //Then
+        assertTrue { result.isEmpty() }
+    }
+
+}
