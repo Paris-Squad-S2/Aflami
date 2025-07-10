0b88b32b03a91f9b98d0b8d0b4c3ff508cd884fc
AhmedSalah <ahmedsaaalah21@gmail.com>
2025-07-11 00:04:45 +0300

    Add init of the test classes

---
+++ feature/search/searchUi/src/test/java/com/feature/search/searchUi/screen/findByActor/FindByActorViewModelTest.kt
@@ -0,0 +1,19 @@
+package com.feature.search.searchUi.screen.findByActor
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class FindByActorViewModelTest {
+    @Test
+    fun onSearchQueryChange() {
+    }
+
+    @Test
+    fun onRetrySearchQuery() {
+    }
+
+    @Test
+    fun onMediaCardClick() {
+    }
+
+}
---
+++ feature/search/searchUi/src/test/java/com/feature/search/searchUi/screen/worldTour/WorldTourViewModelTest.kt
@@ -0,0 +1,15 @@
+package com.feature.search.searchUi.screen.worldTour
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class WorldTourViewModelTest {
+    @Test
+    fun onNavigateBack() {
+    }
+
+    @Test
+    fun onSearchQueryChange() {
+    }
+
+}
