f400ecdec897e89a578e886080ef4003e77ebf01
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-07-12 10:06:14 +0300

    Refactor MainActivity to improve intent handling for search feature navigation

--- app/src/main/java/com/paris_2/aflami/MainActivity.kt
+++ app/src/main/java/com/paris_2/aflami/MainActivity.kt
@@ -3,7 +3,6 @@ package com.paris_2.aflami
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 
 
@@ -11,9 +10,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            val intent = Intent(this, Class.forName("com.feature.search.searchUi.MainActivity"))
-            startActivity(intent)
-        }
+        val intent = Intent(this, Class.forName("com.feature.search.searchUi.MainActivity"))
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 }
