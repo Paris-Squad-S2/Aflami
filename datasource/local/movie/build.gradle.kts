44a9cb6a5daf4b0bd7016db59a49466683929746
AhmedSalah <ahmedsaaalah21@gmail.com>
2025-07-14 02:21:37 +0300

    Add the dependence for the movie local datasource

--- datasource/local/movie/build.gradle.kts
+++ datasource/local/movie/build.gradle.kts
@@ -1,10 +1,31 @@
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.ksp)
 }
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+}
+
+dependencies{
+    //Room
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.room.compiler)
+    annotationProcessor(libs.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
+    //Koin
+    implementation(libs.koin.core)
+    implementation(libs.koin.compose)
+    implementation(libs.koin.compose.viewmodel)
+    implementation(libs.koin.android)
+
+    // kotlin date time
+    implementation(libs.kotlinx.datetime)
+
+    //kotlinx serialization
+    implementation(libs.kotlinx.serialization.json)
 }
 kotlin {
     compilerOptions {
