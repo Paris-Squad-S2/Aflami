e99bda50b9cce8d7afe50ebe6ba67a02eceb75a1
Mustafa Ibrahim <118114457+MustafaIbrahim96@users.noreply.github.com>
2025-07-13 15:30:43 +0300

    update version name and release note

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = Configurations.MIN_SDK_26
         targetSdk = Configurations.TARGET_SDK
         versionCode = Configurations.VERSION_CODE
-        versionName = "0.1.4"
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
