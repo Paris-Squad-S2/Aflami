268417f7b1eae39ae154aaf70778e591ce9dd126
mohammed-akkad <momoo362514@gmail.com>
2025-07-14 13:29:18 +0300

    update movie repository

--- domain/mediaDetails/src/main/java/com/domain/mediaDetails/repository/MovieRepository.kt
+++ domain/mediaDetails/src/main/java/com/domain/mediaDetails/repository/MovieRepository.kt
@@ -4,13 +4,15 @@ import com.domain.mediaDetails.model.Cast
 import com.domain.mediaDetails.model.Gallery
 import com.domain.mediaDetails.model.Movie
 import com.domain.mediaDetails.model.ProductionCompany
+import com.domain.mediaDetails.model.Review
 import com.domain.mediaDetails.model.Season
 
 interface MovieRepository {
-    fun getMovieDetails(movieId: Int): Movie
-    fun getMovieCast(movieId: Int): List<Cast>
-    fun getMovieRecommendations(movieId: Int): List<Movie>
-    fun getMovieGallery(movieId: Int): Gallery
-    fun getCompanyProducts(movieId: Int): List<ProductionCompany>
-    fun getMovieReview(movieId: Int): List<Season>
+    suspend fun getMovieDetails(movieId: Int): Movie
+    suspend fun getMovieCast(movieId: Int): List<Cast>
+    suspend fun getMovieRecommendations(movieId: Int): List<Movie>
+    suspend fun getMovieGallery(movieId: Int): Gallery
+    suspend fun getCompanyProducts(movieId: Int): List<ProductionCompany>
+    suspend fun getMovieReview(movieId: Int): List<Review>
+    suspend fun addMovieToFavorite(movieId: Int)
 }
