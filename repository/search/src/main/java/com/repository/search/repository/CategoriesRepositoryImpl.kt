603f13e12ed95523cccdc4cd4761ce12518ea7c6
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-14 19:08:34 +0300

    Remove language parameter from getAllCategories and use default locale

--- repository/search/src/main/java/com/repository/search/repository/CategoriesRepositoryImpl.kt
+++ repository/search/src/main/java/com/repository/search/repository/CategoriesRepositoryImpl.kt
@@ -9,6 +9,7 @@ import com.repository.search.exception.NoCategoriesFoundException
 import com.repository.search.exception.NoInternetConnectionException
 import com.repository.search.mapper.toCategories
 import com.repository.search.mapper.toEntity
+import java.util.Locale
 
 class CategoriesRepositoryImpl(
     private val networkConnectionChecker: NetworkConnectionChecker,
@@ -16,7 +17,8 @@ class CategoriesRepositoryImpl(
     private val genresRemoteDataSource: GenresRemoteDataSource,
 ) : CategoriesRepository {
 
-    override suspend fun getAllCategories(language: String): List<CategoryModel> {
+    override suspend fun getAllCategories(): List<CategoryModel> {
+        val language = Locale.getDefault().language
         return try {
             val genres = genresLocalDataSource.getGenres(language)
             if (genres.isNotEmpty()) return genres.toCategories()
