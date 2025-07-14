6bb631d957e4475abec0b311784e2b8842fcb5d8
mohammed-akkad <momoo362514@gmail.com>
2025-07-14 13:23:25 +0300

    add movie use case test cases

---
+++ domain/mediaDetails/src/test/java/com/domain/mediaDetails/useCases/movie/GetMovieRecommendationsUseCaseTest.kt
@@ -0,0 +1,48 @@
+package com.domain.mediaDetails.useCases.movie
+
+import com.domain.mediaDetails.repository.MovieRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import testUtils.fakeMovies
+import kotlin.test.assertEquals
+
+class GetMovieRecommendationsUseCaseTest {
+    private lateinit var getMovieRecommendationsUseCase: GetMovieRecommendationsUseCase
+    private val movieRepository: MovieRepository = mockk(relaxed = true)
+
+    @Before
+    fun setup() {
+        getMovieRecommendationsUseCase = GetMovieRecommendationsUseCase(movieRepository)
+    }
+
+    @Test
+    fun `should return movie recommendation from repository`() = runTest {
+        // Given
+        val movieId = 1
+
+        // when
+        coEvery { movieRepository.getMovieRecommendations(movieId) } returns fakeMovies
+
+        // Then
+        val result = getMovieRecommendationsUseCase(movieId)
+        assertEquals(result, fakeMovies)
+
+    }
+
+    @Test
+    fun `should return empty list when no cast found`() = runTest{
+        // Given
+        val movieId = 1
+
+        // when
+        coEvery { movieRepository.getMovieRecommendations(movieId) } returns emptyList()
+
+        // Then
+        val result = getMovieRecommendationsUseCase(movieId)
+        assertEquals(result, emptyList())
+    }
+
+}
---
+++ domain/mediaDetails/src/test/java/com/domain/mediaDetails/useCases/movie/GetMovieReviewsUseCaseTest.kt
@@ -0,0 +1,48 @@
+package com.domain.mediaDetails.useCases.movie
+
+import com.domain.mediaDetails.repository.MovieRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import testUtils.fakeReviews
+import kotlin.test.assertEquals
+
+class GetMovieReviewsUseCaseTest {
+    private lateinit var getMovieReviewsUseCase: GetMovieReviewsUseCase
+    private val movieRepository: MovieRepository = mockk(relaxed = true)
+
+    @Before
+    fun setup() {
+        getMovieReviewsUseCase = GetMovieReviewsUseCase(movieRepository)
+    }
+
+    @Test
+    fun `should return movie reviews from repository`() = runTest {
+        // Given
+        val movieId = 1
+
+        // when
+        coEvery { movieRepository.getMovieReview(movieId) } returns fakeReviews
+
+        // Then
+        val result = getMovieReviewsUseCase(movieId)
+        assertEquals(result, fakeReviews)
+
+    }
+
+    @Test
+    fun `should return empty list when no cast found`() = runTest{
+        // Given
+        val movieId = 1
+
+        // when
+        coEvery { movieRepository.getMovieReview(movieId) } returns emptyList()
+
+        // Then
+        val result = getMovieReviewsUseCase(movieId)
+        assertEquals(result, emptyList())
+    }
+
+}
