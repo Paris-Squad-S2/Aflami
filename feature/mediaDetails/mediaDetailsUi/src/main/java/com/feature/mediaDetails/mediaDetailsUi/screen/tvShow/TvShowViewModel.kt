a15d2d3ca86779e191863329ced69943be3e9187
dina othman <dinaosman581@gmail.com>
2025-07-15 12:08:30 +0300

    add TvShowDetailsScreenState

--- feature/mediaDetails/mediaDetailsUi/src/main/java/com/feature/mediaDetails/mediaDetailsUi/screen/tvShow/TvShowViewModel.kt
+++ feature/mediaDetails/mediaDetailsUi/src/main/java/com/feature/mediaDetails/mediaDetailsUi/screen/tvShow/TvShowViewModel.kt
@@ -1,5 +1,6 @@
 package com.feature.mediaDetails.mediaDetailsUi.screen.tvShow
 
+import com.domain.mediaDetails.model.Image
 import com.domain.mediaDetails.useCases.tvShows.AddTvShowToFavoriteUseCase
 import com.domain.mediaDetails.useCases.tvShows.AddTvShowToListUseCase
 import com.domain.mediaDetails.useCases.tvShows.GetSeasonsUseCase
@@ -8,11 +9,52 @@ import com.domain.mediaDetails.useCases.tvShows.GetTvShowDetailsUseCase
 import com.domain.mediaDetails.useCases.tvShows.GetTvShowMediaUseCase
 import com.domain.mediaDetails.useCases.tvShows.GetTvShowReviewsUseCase
 import com.domain.mediaDetails.useCases.tvShows.GetTvShowsProductionCompaniesUseCase
+import com.feature.mediaDetails.mediaDetailsUi.screen.movie.CastUi
+import com.feature.mediaDetails.mediaDetailsUi.screen.movie.ProductionCompanyUi
+import com.feature.mediaDetails.mediaDetailsUi.screen.movie.ReviewUi
 
 data class TvShowDetailsScreenState(
+    val tvShowDetailsUiState : TvShowDetailsUiState,
     val isLoading: Boolean,
     val errorMessage: String?
 )
+
+data class TvShowDetailsUiState(
+    val tvShowUi: TvShowUi,
+    val cast: List<CastUi>,
+    val reviews: List<ReviewUi>,
+    val gallery: List<Image>,
+)
+
+data class TvShowUi(
+    val posterUrl: String,
+    val rating: String,
+    val title: String,
+    val genres: List<String>,
+    val releaseDate: String,
+    val runtime: String,
+    val country: String,
+    val description: String,
+    val productionCompanies: List<ProductionCompanyUi>
+)
+
+data class SeasonUi(
+    val id: String,
+    val name: String,
+    val episodes: List<EpisodeUi>
+)
+
+data class EpisodeUi(
+    val episodeNumber: Int,
+    val posterUrl: String,
+    val voteAverage: Double,
+    val airDate: String,
+    val runtime: String,
+    val description: String,
+    val stillUrl: String
+)
+
+
 
 class TvShowViewModel(
     private val addTvShowToFavoriteUseCase: AddTvShowToFavoriteUseCase,
@@ -23,6 +65,4 @@ class TvShowViewModel(
     private val getTvShowMediaUseCase : GetTvShowMediaUseCase,
     private val getTvShowReviewsUseCase : GetTvShowReviewsUseCase,
     private val getTvShowsProductionCompaniesUseCase : GetTvShowsProductionCompaniesUseCase
-) {
-
-}
+)
