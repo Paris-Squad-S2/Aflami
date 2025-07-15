7be24483c2a45ab2d35e7090097ac8e903c33582
mohammed-akkad <momoo362514@gmail.com>
2025-07-15 15:08:42 +0300

    add tv show excpation

---
+++ domain/mediaDetails/src/main/java/com/domain/mediaDetails/exception/AflamiException.kt
@@ -0,0 +1,11 @@
+package com.domain.mediaDetails.exception
+
+abstract class AflamiException(message:String) : Exception(message)
+open class NetworkConnectionException(message:String):AflamiException(message)
+open class LocalDataSourceException(message:String):AflamiException(message)
+
+
+class UnauthorizedException : NetworkConnectionException("Unauthorized access: Please check your credentials and try again")
+class RequestTimeoutException : NetworkConnectionException("Request timed out: Please check your internet connection and try again")
+class ServerException : NetworkConnectionException("Server error: Please try again later or contact support")
+class UnknownException : NetworkConnectionException("An unknown error occurred: Please try again or contact support")
