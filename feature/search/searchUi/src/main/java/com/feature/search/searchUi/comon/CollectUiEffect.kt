8061315c7d89b183e527b9899ef25c2865e281d8
mohammed-akkad <momoo362514@gmail.com>
2025-07-15 09:44:06 +0300

    fix multiple navigation for the same screen

--- feature/search/searchUi/src/main/java/com/feature/search/searchUi/comon/CollectUiEffect.kt
+++ feature/search/searchUi/src/main/java/com/feature/search/searchUi/comon/CollectUiEffect.kt
@@ -1,18 +1,38 @@
 package com.feature.search.searchUi.comon
 
+import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
+@SuppressLint("FlowOperatorInvokedInComposition")
 @Composable
 fun CollectUiEffect(
     effect: SharedFlow<BaseViewModel.BaseUiEffect>,
     effectHandler: (effect: BaseViewModel.BaseUiEffect) -> Unit
 ) {
+    val throttle = effect.throttleFirst(1000)
     LaunchedEffect(Unit) {
-        effect.collectLatest {
+        throttle.collectLatest {
             effectHandler(it)
+        }
+    }
+}
+
+private fun <T> Flow<T>.throttleFirst(periodMillis: Long): Flow<T> {
+    if (periodMillis < 0) return this
+    return flow {
+        conflate().collect { value ->
+            emit(value)
+            delay(periodMillis)
         }
     }
 }
