a5ef102d91d86e2d95c801557756c0af3204084e
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-07-15 08:43:54 +0300

    Add Ktor API service interfaces and implementations for movie and TV show details; update search and genres services to remove baseUrl dependency; add example unit and instrumented tests

---
+++ datasource/remote/mediaDetails/src/main/java/com/datasource/remote/mediadetails/service/implementation/KtorMediaDetailsApiServiceImpl.kt
@@ -0,0 +1,69 @@
+package com.datasource.remote.mediadetails.service.implementation
+
+import com.datasource.remote.mediadetails.models.movie.MovieCreditsDto
+import com.datasource.remote.mediadetails.models.movie.MovieDto
+import com.datasource.remote.mediadetails.models.movie.MovieImagesDto
+import com.datasource.remote.mediadetails.models.movie.MovieReviewsDto
+import com.datasource.remote.mediadetails.models.movie.MovieSimilarsDto
+import com.datasource.remote.mediadetails.service.KtorMovieDetailsApiService
+import com.datasource.remote.mediadetails.utils.safeApiCall
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+
+class KtorMediaDetailsApiServiceImpl(
+    private val httpClient: HttpClient,
+) : KtorMovieDetailsApiService {
+    override suspend fun getMovieDetails(movieId: Int, language: String): MovieDto {
+        return safeApiCall {
+            httpClient.get(MOVIE_PATH.replace(MOVIE_ID, movieId.toString())) {
+                parameter(LANGUAGE, language)
+            }
+        }
+    }
+
+    override suspend fun getMovieImages(movieId: Int): MovieImagesDto {
+        return safeApiCall { httpClient.get(MOVIE_PATH.replace(MOVIE_ID, movieId.toString()) + IMAGES) }
+    }
+
+    override suspend fun getMovieReviews(movieId: Int, page: Int, language: String): MovieReviewsDto {
+        return safeApiCall {
+            httpClient.get(MOVIE_PATH.replace(MOVIE_ID, movieId.toString()) + REVIEWS) {
+                parameter(PAGE, page)
+                parameter(LANGUAGE, language)
+            }
+        }
+    }
+
+    override suspend fun getSimilarMovies(
+        movieId: Int,
+        page: Int,
+        language: String
+    ): MovieSimilarsDto {
+        return safeApiCall {
+            httpClient.get(MOVIE_PATH.replace(MOVIE_ID, movieId.toString()) + SIMILAR) {
+                parameter(PAGE, page)
+                parameter(LANGUAGE, language)
+            }
+        }
+    }
+
+    override suspend fun getMovieCredits(movieId: Int, language: String): MovieCreditsDto {
+        return safeApiCall {
+            httpClient.get(MOVIE_PATH.replace(MOVIE_ID, movieId.toString()) + CREDITS) {
+                parameter(LANGUAGE, language)
+            }
+        }
+    }
+
+    companion object {
+        private const val MOVIE_ID = "{movieId}"
+        private const val MOVIE_PATH = "movie/$MOVIE_ID"
+        private const val IMAGES = "/images"
+        private const val REVIEWS = "/reviews"
+        private const val SIMILAR = "/similar"
+        private const val CREDITS = "/credits"
+        private const val LANGUAGE = "language"
+        private const val PAGE = "page"
+    }
+}
