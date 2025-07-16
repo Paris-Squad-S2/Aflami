0e2672dfae00a412e91182bcc65fe1472523539f
AhmedSalah <ahmedsaaalah21@gmail.com>
2025-07-17 00:08:13 +0300

    Refactor: Introduce TvShowDetailConverter for Room Type Conversions

--- repository/tvShows/src/main/java/com/repository/datasource/local/SeasonLocalDataSource.kt
+++ repository/tvShows/src/main/java/com/repository/datasource/local/SeasonLocalDataSource.kt
@@ -5,5 +5,4 @@ import com.repository.entity.SeasonEntity
 interface SeasonLocalDataSource {
     suspend fun addSeason(seasons: List<SeasonEntity>)
     suspend fun getSeasonsByTvShowId(tvShowId: Int): List<SeasonEntity>?
-
 }
