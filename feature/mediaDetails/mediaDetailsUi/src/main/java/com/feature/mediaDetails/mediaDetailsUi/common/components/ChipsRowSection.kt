ad00bff7c4d57c1bbc429c576a7255550f57aed0
dina othman <dinaosman581@gmail.com>
2025-07-16 11:06:35 +0300

    add ChipsRowSection

---
+++ feature/mediaDetails/mediaDetailsUi/src/main/java/com/feature/mediaDetails/mediaDetailsUi/common/components/ChipsRowSection.kt
@@ -0,0 +1,59 @@
+package com.feature.mediaDetails.mediaDetailsUi.common.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.paris_2.aflami.designsystem.components.Chips
+import com.paris_2.aflami.designsystem.R
+import com.paris_2.aflami.designsystem.theme.AflamiTheme
+
+@Composable
+fun ChipsRowSection(
+    items: List<Pair<String, Int>>,
+    selectedIndex: Int?,
+    onItemSelected: (Int) -> Unit
+) {
+    LazyRow(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 12.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(items.size) { index ->
+            val isSelected = index == selectedIndex
+            Chips(
+                title = items[index].first,
+                icon = painterResource(id = items[index].second),
+                isSelected = isSelected,
+                onClick = { onItemSelected(index) }
+            )
+        }
+    }
+}
+
+
+@PreviewLightDark
+@Composable
+fun PreviewChipsRowSections() {
+    AflamiTheme {
+        val dummyItems = listOf(
+            "More like this" to R.drawable.ic_camera_video,
+            "Reviews" to R.drawable.ic_starr,
+            "Gallery" to R.drawable.ic_album,
+            "Production" to R.drawable.ic_city
+        )
+        AflamiTheme {
+            ChipsRowSection(
+                items = dummyItems,
+                selectedIndex = null,
+                onItemSelected = {}
+            )
+        }
+    }
+}
