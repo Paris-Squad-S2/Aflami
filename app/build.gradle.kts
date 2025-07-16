0da51079b6ff30f3446bfcd6ba309308bbacd0af
Mustafa Ibrahim <118114457+MustafaIbrahim96@users.noreply.github.com>
2025-07-17 02:58:00 +0300

    - handle ci and cd with key store and upload signed release - Reduce app size

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,38 @@ android {
     namespace = "com.paris_2.aflami"
     compileSdk = Configurations.COMPILE_SDK
 
+    signingConfigs {
+        create("release") {
+            val localProps = Properties().apply {
+                val localFile = rootProject.file("local.properties")
+                if (localFile.exists()) {
+                    load(localFile.inputStream())
+                }
+            }
+
+            val keystorePath = System.getenv("KEYSTORE_PATH")
+                ?: localProps.getProperty("KEYSTORE_PATH")
+                ?: throw GradleException("KEYSTORE_PATH is not set.")
+
+            val keystorePassword = System.getenv("KEYSTORE_PASSWORD")
+                ?: localProps.getProperty("KEYSTORE_PASSWORD")
+                ?: throw GradleException("KEYSTORE_PASSWORD is not set.")
+
+            val keyAliasValue = System.getenv("KEY_ALIAS")
+                ?: localProps.getProperty("KEY_ALIAS")
+                ?: throw GradleException("KEY_ALIAS is not set.")
+
+            val keyPasswordValue = System.getenv("KEY_PASSWORD")
+                ?: localProps.getProperty("KEY_PASSWORD")
+                ?: throw GradleException("KEY_PASSWORD is not set.")
+
+            storeFile = file(keystorePath)
+            storePassword = keystorePassword
+            keyAlias = keyAliasValue
+            keyPassword = keyPasswordValue
+        }
+    }
+
     defaultConfig {
         applicationId = "com.paris_2.aflami"
         minSdk = Configurations.MIN_SDK_26
@@ -28,7 +60,15 @@ android {
         vectorDrawables.useSupportLibrary = true
     }
 
+
+
     buildTypes {
+        getByName("release") {
+            ndk {
+                abiFilters.clear()
+                abiFilters += listOf("armeabi-v7a", "arm64-v8a")
+            }
+        }
         release {
             isMinifyEnabled = true
             isShrinkResources = true
@@ -36,24 +76,30 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
             isDebuggable = false
             isCrunchPngs = true
+
         }
         create("minified") {
-            initWith(getByName("debug"))
+            initWith(buildTypes.getByName("release"))
             isMinifyEnabled = true
             isShrinkResources = true
+            isDebuggable = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            matchingFallbacks.add("debug")
+            matchingFallbacks.add("release")
         }
         getByName("debug") {
             enableUnitTestCoverage = true
             isMinifyEnabled = false
             isShrinkResources = false
+            ndk {
+                abiFilters.clear()
+                abiFilters += listOf("x86", "x86_64", "armeabi-v7a", "arm64-v8a")
+            }
         }
     }
     compileOptions {
