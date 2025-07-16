4c111c29959fcd455d6f6211e15eacc09bc5035f
dina othman <dinaosman581@gmail.com>
2025-07-16 12:03:03 +0300

    add ReviewsSection

---
+++ feature/mediaDetails/mediaDetailsUi/src/main/java/com/feature/mediaDetails/mediaDetailsUi/common/components/ReviewsSection.kt
@@ -0,0 +1,54 @@
+package com.feature.mediaDetails.mediaDetailsUi.common.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.feature.mediaDetails.mediaDetailsUi.screen.movie.ReviewUi
+import com.paris_2.aflami.designsystem.theme.Theme
+
+@Composable
+fun ReviewsSection(
+    reviews: List<ReviewUi>?,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Theme.colors.surface),
+        contentAlignment = Alignment.Center
+    ) {
+        if (!reviews.isNullOrEmpty()) {
+            Column(
+                modifier = modifier
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                reviews.forEach { review ->
+                    ReviewCard(
+                        name = review.name,
+                        createdAt = review.createdAt,
+                        avatarUrl = review.avatarUrl,
+                        username = review.username,
+                        rating = review.rating,
+                        description = "Hmmm! I wasn’t sure if I was watching a sentimental edition of “Hawaii Five-O” here or a collection of outtakes from a “Sonic” movie as this "
+                    )
+                }
+            }
+        } else {
+            Text(
+                text = "There is no reviews!",
+                style = Theme.textStyle.label.large,
+                color = Theme.colors.text.body.copy(alpha = 0.6f)
+            )
+        }
+
+    }
+}
+
