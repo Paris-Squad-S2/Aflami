77e68352c173ce805a6fd21676e0c88baecf8736
dina othman <dinaosman581@gmail.com>
2025-07-17 14:28:28 +0300

    add fake data

--- feature/mediaDetails/mediaDetailsUi/src/main/java/com/feature/mediaDetails/mediaDetailsUi/ui/screen/movie/details/MediaDetailsViewModel.kt
+++ feature/mediaDetails/mediaDetailsUi/src/main/java/com/feature/mediaDetails/mediaDetailsUi/ui/screen/movie/details/MediaDetailsViewModel.kt
@@ -11,36 +11,10 @@ class MovieDetailsViewModelViewModel(
     BaseViewModel<MovieDetailsScreenState>(
         MovieDetailsScreenState(
             movieDetailsUiState = MovieDetailsUiState(
-                movie = MovieUi(
-                    posterUrl = "",
-                    rating = "",
-                    title = "",
-                    genres = emptyList(),
-                    releaseDate = "",
-                    runtime = "",
-                    country = "",
-                    description = "",
-                    productionCompanies = emptyList()
-                ),
-                cast = emptyList(),
-                reviews = emptyList(),
-                gallery = emptyList()
-            ),
-            isLoading = false,
-            errorMessage = null
-        )
-    ) {
-
-    init {
-        val mediaId =
-            savedStateHandle.toRoute<MediaDetailsDestinations.MovieDetailsScreen>().movieId
-        emitState(
-            MovieDetailsScreenState(
-                movieDetailsUiState = MovieDetailsUiState(
                     movie = MovieUi(
                         posterUrl = "",
                         rating = "",
-                        title = "Movie ID: $mediaId",
+                        title = "",
                         genres = emptyList(),
                         releaseDate = "",
                         runtime = "",
@@ -51,6 +25,69 @@ class MovieDetailsViewModelViewModel(
                     cast = emptyList(),
                     reviews = emptyList(),
                     gallery = emptyList()
+                ),
+                isLoading = false,
+                errorMessage = null
+            )
+    ) {
+    init {
+        val mediaId =
+            savedStateHandle.toRoute<MediaDetailsDestinations.MovieDetailsScreen>().movieId
+        emitState(
+            MovieDetailsScreenState(
+                movieDetailsUiState = MovieDetailsUiState(
+                    movie = MovieUi(
+                        posterUrl = "https://image.tmdb.org/t/p/original/iaSA91XEY01hAcftOe9Vc9qfCNa.jpg",
+                        rating = "8.2",
+                        title = "Inception (ID: $mediaId)",
+                        genres = listOf("Action", "Science Fiction", "Thriller"),
+                        releaseDate = "2010-07-16",
+                        runtime = "2h 28m",
+                        country = "USA",
+                        description = "A thief who steals corporate secrets through use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.",
+                        productionCompanies = listOf(
+                            ProductionCompanyUi(
+                                logoUrl = "https://image.tmdb.org/t/p/original/iaSA91XEY01hAcftOe9Vc9qfCNa.jpg",
+                                name = "Legendary Pictures",
+                                originCountry = "USA"
+                            ),
+                            ProductionCompanyUi(
+                                logoUrl = "https://image.tmdb.org/t/p/original/iaSA91XEY01hAcftOe9Vc9qfCNa.jpg",
+                                name = "Syncopy",
+                                originCountry = "UK"
+                            )
+                        )
+                    ),
+                    cast = listOf(
+                        CastUi(
+                            name = "Leonardo DiCaprio",
+                            imageUrl = "https://image.tmdb.org/t/p/original/iaSA91XEY01hAcftOe9Vc9qfCNa.jpg"
+                        ),
+                        CastUi(
+                            name = "Joseph Gordon-Levitt",
+                            imageUrl = "https://image.tmdb.org/t/p/original/iaSA91XEY01hAcftOe9Vc9qfCNa.jpg"
+                        )
+                    ),
+                    reviews = listOf(
+                        ReviewUi(
+                            avatarUrl = "https://image.tmdb.org/t/p/original/iaSA91XEY01hAcftOe9Vc9qfCNa.jpg",
+                            username = "movie_buff_91",
+                            name = "Amazing Movie!",
+                            rating = 9.0,
+                            createdAt = "2023-04-12"
+                        ),
+                        ReviewUi(
+                            avatarUrl = "https://image.tmdb.org/t/p/original/iaSA91XEY01hAcftOe9Vc9qfCNa.jpg",
+                            username = "cinema_fanatic",
+                            name = "Mind-blowing!",
+                            rating = 8.5,
+                            createdAt = "2023-05-20"
+                        )
+                    ),
+                    gallery = listOf(
+                        "https://image.tmdb.org/t/p/original/iaSA91XEY01hAcftOe9Vc9qfCNa.jpg",
+                        "https://image.tmdb.org/t/p/original/iaSA91XEY01hAcftOe9Vc9qfCNa.jpg"
+                    )
                 ),
                 isLoading = false,
                 errorMessage = null
