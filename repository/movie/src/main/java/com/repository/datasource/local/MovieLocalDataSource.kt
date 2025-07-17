768395272d7526aee64705cd1597a4e3204337d9
AhmedSalah <ahmedsaaalah21@gmail.com>
2025-07-18 00:53:55 +0300

    Test: Add unit tests for CastLocalDataSourceImp

--- repository/movie/src/main/java/com/repository/datasource/local/MovieLocalDataSource.kt
+++ repository/movie/src/main/java/com/repository/datasource/local/MovieLocalDataSource.kt
@@ -4,5 +4,5 @@ import com.repository.entity.MovieEntity
 
 interface MovieLocalDataSource {
     suspend fun addMovie(movie: MovieEntity)
-    suspend fun getMovie(movieId:Int): MovieEntity?
+    suspend fun getMovieById(movieId:Int): MovieEntity?
 }
