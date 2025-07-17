47043211001a1320daecc53558687c3ac6213e85
mohammed-akkad <momoo362514@gmail.com>
2025-07-17 14:13:52 +0300

    update local tv show dao

--- datasource/local/tvShow/src/main/java/com/datasource/local/dao/ReviewDao.kt
+++ datasource/local/tvShow/src/main/java/com/datasource/local/dao/ReviewDao.kt
@@ -4,7 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.repository.entity.ReviewEntity
+import com.repository.model.local.ReviewEntity
 
 @Dao
 interface ReviewDao {
