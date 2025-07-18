d87ac2e59d19ad4c1886fc2608d44dccaa31cd09
dina othman <dinaosman581@gmail.com>
2025-07-18 23:50:12 +0300

    handle scroll in DetailsImage

--- feature/mediaDetails/mediaDetailsUi/src/main/java/com/feature/mediaDetails/mediaDetailsUi/ui/comon/components/detailsImage/DetailsImage.kt
+++ feature/mediaDetails/mediaDetailsUi/src/main/java/com/feature/mediaDetails/mediaDetailsUi/ui/comon/components/detailsImage/DetailsImage.kt
@@ -9,10 +9,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
@@ -30,6 +31,7 @@ import com.paris_2.aflami.designsystem.components.MediaPlayButton
 import com.paris_2.aflami.designsystem.components.RatingCard
 import com.paris_2.aflami.designsystem.theme.AflamiTheme
 import com.paris_2.aflami.designsystem.theme.Theme
+import kotlinx.coroutines.delay
 
 
 @Composable
@@ -39,7 +41,19 @@ fun DetailsImage(
     rating: Float,
     onPlayClick: () -> Unit
 ) {
-    val pagerState = rememberPagerState(pageCount = { imageUris.size })
+    val displayImages = imageUris.take(10)
+    val pagerState = rememberPagerState(pageCount = { displayImages.size })
+
+    LaunchedEffect(Unit) {
+        if (displayImages.size > 1) {
+            while (true) {
+                delay(4000)
+                val nextPage = (pagerState.currentPage + 1) % displayImages.size
+                pagerState.animateScrollToPage(nextPage)
+            }
+        }
+    }
+
     Box(
         modifier = modifier
             .fillMaxWidth()
@@ -51,11 +65,12 @@ fun DetailsImage(
                 .height(263.dp)
         ) {
             val color: Color = Theme.colors.surfaceHigh
-            VerticalPager(
+            HorizontalPager(
                 state = pagerState,
                 modifier = Modifier.fillMaxSize()
             ) { page ->
-                val painter = rememberAsyncImagePainter(model = imageUris[page])
+
+                val painter = rememberAsyncImagePainter(model = displayImages[page])
                 val painterState = painter.state
 
                 Box(
@@ -135,10 +150,10 @@ fun DetailsImage(
                     backGroundColor = Theme.colors.surfaceHigh
                 )
             }
-            if (imageUris.isNotEmpty()) {
+            if (displayImages.isNotEmpty()) {
                 ImagePageIndicator(
-                    pageSize = if (imageUris.size == 1) 3 else imageUris.size,
-                    currentPage = if (imageUris.size == 1) 2 else pagerState.currentPage,
+                    pageSize = if (displayImages.size == 1) 3 else displayImages.size,
+                    currentPage = if (displayImages.size == 1) 2 else pagerState.currentPage,
                     modifier = Modifier
                         .align(Alignment.BottomEnd)
                         .padding(end = 4.dp)
@@ -155,6 +170,17 @@ fun DetailsImagePreview() {
     AflamiTheme {
         DetailsImage(
             imageUris = listOf(
+                "https://xl.movieposterdb.com/12_03/1999/120689/xl_120689_c927b987.jpg",
+                "https://xl.movieposterdb.com/12_03/1999/120689/xl_120689_c927b987.jpg",
+                "https://xl.movieposterdb.com/12_03/1999/120689/xl_120689_c927b987.jpg",
+                "https://xl.movieposterdb.com/12_03/1999/120689/xl_120689_c927b987.jpg",
+                "https://xl.movieposterdb.com/12_03/1999/120689/xl_120689_c927b987.jpg",
+                "https://xl.movieposterdb.com/12_03/1999/120689/xl_120689_c927b987.jpg",
+                "https://xl.movieposterdb.com/12_03/1999/120689/xl_120689_c927b987.jpg",
+                "https://xl.movieposterdb.com/12_03/1999/120689/xl_120689_c927b987.jpg",
+                "https://xl.movieposterdb.com/12_03/1999/120689/xl_120689_c927b987.jpg",
+                "https://xl.movieposterdb.com/12_03/1999/120689/xl_120689_c927b987.jpg",
+                "https://xl.movieposterdb.com/12_03/1999/120689/xl_120689_c927b987.jpg",
                 "https://xl.movieposterdb.com/12_03/1999/120689/xl_120689_c927b987.jpg",
             ),
             rating = 9.9f,
