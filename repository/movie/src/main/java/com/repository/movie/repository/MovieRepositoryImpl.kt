d38f831278560274881331a1700232308e724e44
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-07-18 15:53:00 +0300

    Revert "Integrate network connection checker in MovieRepository"
    This reverts commit 8f9f0424688ba43a436d6b6ffff72ee63bd1d0e0.

--- repository/movie/src/main/java/com/repository/movie/repository/MovieRepositoryImpl.kt
+++ repository/movie/src/main/java/com/repository/movie/repository/MovieRepositoryImpl.kt
@@ -1,15 +1,18 @@
 package com.repository.movie.repository
 
 import com.domain.mediaDetails.exception.NoGalleryMovieDetailsFoundException
-import com.domain.mediaDetails.exception.NoInternetConnectionException
 import com.domain.mediaDetails.exception.NoMovieDetailsFoundException
 import com.domain.mediaDetails.model.Cast
 import com.domain.mediaDetails.model.Gallery
 import com.domain.mediaDetails.model.Movie
-import com.domain.mediaDetails.model.MovieSimilar
 import com.domain.mediaDetails.model.ProductionCompany
 import com.domain.mediaDetails.model.Review
 import com.domain.mediaDetails.repository.MovieRepository
+import com.domain.mediaDetails.exception.RequestTimeoutException
+import com.domain.mediaDetails.exception.ServerException
+import com.domain.mediaDetails.exception.UnauthorizedException
+import com.domain.mediaDetails.exception.UnknownException
+import com.domain.mediaDetails.model.MovieSimilar
 import com.repository.movie.dataSource.local.MovieCastLocalDataSource
 import com.repository.movie.dataSource.local.MovieGalleryLocalDataSource
 import com.repository.movie.dataSource.local.MovieLocalDataSource
@@ -18,7 +21,6 @@ import com.repository.movie.dataSource.remote.MovieDetailsRemoteDataSource
 import com.repository.movie.mapper.toEntity
 import com.repository.movie.mapper.toLocalDto
 import com.repository.movie.models.local.GalleryEntity
-import com.repository.movie.util.NetworkConnectionChecker
 import com.repository.movie.util.detectLanguage
 import com.repository.movie.util.getCurrentDate
 import kotlinx.datetime.DateTimeUnit
@@ -29,7 +31,6 @@ import kotlinx.datetime.toInstant
 import kotlin.time.ExperimentalTime
 
 class MovieRepositoryImpl(
-    private val networkConnectionChecker: NetworkConnectionChecker,
     private val movieLocalDataSource: MovieLocalDataSource,
     private val movieCastLocalDataSource: MovieCastLocalDataSource,
     private val movieGalleryLocalDataSource: MovieGalleryLocalDataSource,
@@ -39,131 +40,87 @@ class MovieRepositoryImpl(
     private val language = detectLanguage()
 
     override suspend fun getMovieDetails(movieId: Int): Movie {
-        try {
-            val localMovie = movieLocalDataSource.getMovie(movieId)
-            if (localMovie != null && isDataFresh(localMovie.movieCacheDate)) {
-                return localMovie.toEntity()
-            }
+        return safeCall {
+            val movieRemote =
+                movieDetailsRemoteDataSource.getMovieDetails(movieId, language)
 
-            if (networkConnectionChecker.isConnected.value) {
-                val movieRemote = movieDetailsRemoteDataSource.getMovieDetails(movieId, language)
-                movieLocalDataSource.addMovie(movieRemote.toLocalDto())
-                return movieLocalDataSource.getMovie(movieId)?.toEntity()
-                    ?: throw NoMovieDetailsFoundException()
-            } else {
-                throw NoInternetConnectionException()
-            }
-        } catch (e: NoInternetConnectionException) {
-            throw e
-        } catch (_: Exception) {
-            throw NoMovieDetailsFoundException()
+            movieLocalDataSource.getMovie(movieId)
+                .takeIf { it != null && isDataFresh(it.movieCacheDate) }
+                .also { movieLocalDataSource.addMovie(movieRemote.toLocalDto()) }
+                ?.toEntity()
+                ?: throw NoMovieDetailsFoundException()
         }
     }
 
     override suspend fun getMovieCast(movieId: Int): List<Cast> {
-        try {
-            val localCast = movieCastLocalDataSource.getCastByMovieId(movieId)
-            if (localCast.isNotEmpty() && localCast.all { isDataFresh(it.castCacheDate) }) {
-                return localCast.map { it.toEntity() }
-            }
-
-            if (networkConnectionChecker.isConnected.value) {
-                val remoteCast = movieDetailsRemoteDataSource.getMovieCredits(movieId, language)
+        return safeCall {
+            val movieCast =
+                movieDetailsRemoteDataSource.getMovieCredits(movieId, language)
                     .cast?.map { it.toEntity() } ?: emptyList()
-                movieCastLocalDataSource.addCast(remoteCast.map { it.toLocalDto() })
-                return movieCastLocalDataSource.getCastByMovieId(movieId).map { it.toEntity() }
-            } else {
-                throw NoInternetConnectionException()
-            }
-        } catch (e: NoInternetConnectionException) {
-            throw e
-        } catch (_: Exception) {
-            return emptyList()
+
+            movieCastLocalDataSource.getCastByMovieId(movieId)
+                .filter { isDataFresh(it.castCacheDate) }
+                .takeIf { it.isNotEmpty() }
+                .also { movieCastLocalDataSource.addCast(movieCast.map { it.toLocalDto() }) }
+                ?.map { it.toEntity() }
+                ?: emptyList()
         }
     }
 
     override suspend fun getMovieRecommendations(movieId: Int, page: Int): List<MovieSimilar> {
-        return try {
-            if (networkConnectionChecker.isConnected.value) {
-                movieDetailsRemoteDataSource.getSimilarMovies(
-                    movieId, page, language
-                ).movieSimilarDto?.map { it.toEntity() } ?: emptyList()
-            } else {
-                throw NoInternetConnectionException()
-            }
-        } catch (e: NoInternetConnectionException) {
-            throw e
-        } catch (_: Exception) {
-            emptyList()
+        return safeCall {
+            movieDetailsRemoteDataSource.getSimilarMovies(
+                movieId,
+                page,
+                language
+            ).movieSimilarDto?.map { it.toEntity() } ?: emptyList()
         }
     }
 
     override suspend fun getMovieGallery(movieId: Int): Gallery {
-        try {
-            val localGallery = movieGalleryLocalDataSource.getGalleryByMovieId(movieId)
-            if (localGallery != null && isDataFresh(localGallery.galleryCacheDate)) {
-                return localGallery.toEntity()
-            }
+        return safeCall {
+            val movieImage =
+                movieDetailsRemoteDataSource.getMovieImages(movieId).toEntity().images
 
-            if (networkConnectionChecker.isConnected.value) {
-                val remoteImages =
-                    movieDetailsRemoteDataSource.getMovieImages(movieId).toEntity().images
-                movieGalleryLocalDataSource.addGallery(
-                    GalleryEntity(
-                        movieId = movieId,
-                        images = remoteImages.map { it.toLocalDto() },
-                        id = 0
+            movieGalleryLocalDataSource.getGalleryByMovieId(movieId)
+                .takeIf { it != null && isDataFresh(it.galleryCacheDate) }
+                .also {
+                    movieGalleryLocalDataSource.addGallery(
+                        GalleryEntity(
+                            movieId = movieId,
+                            images = movieImage.map { it.toLocalDto() },
+                            id = 0
+                        )
                     )
-                )
-                return movieGalleryLocalDataSource.getGalleryByMovieId(movieId)?.toEntity()
-                    ?: throw NoGalleryMovieDetailsFoundException()
-            } else {
-                throw NoInternetConnectionException()
-            }
-        } catch (e: NoInternetConnectionException) {
-            throw e
-        } catch (_: Exception) {
-            throw NoGalleryMovieDetailsFoundException()
+                }
+                ?.toEntity()
+                ?: throw NoGalleryMovieDetailsFoundException()
         }
     }
 
     override suspend fun getCompanyProducts(movieId: Int): List<ProductionCompany> {
-        return try {
-            if (networkConnectionChecker.isConnected.value) {
-                movieDetailsRemoteDataSource.getMovieDetails(
-                    movieId, language
-                ).productionCompanies?.map { it.toEntity() } ?: emptyList()
-            } else {
-                throw NoInternetConnectionException()
-            }
-        } catch (e: NoInternetConnectionException) {
-            throw e
-        } catch (_: Exception) {
-            emptyList()
+        return safeCall {
+            movieDetailsRemoteDataSource.getMovieDetails(
+                movieId,
+                language
+            ).productionCompanies
+                ?.map { it.toEntity() }
+                ?: emptyList()
         }
     }
 
-
     override suspend fun getMovieReview(movieId: Int, page: Int): List<Review> {
-        try {
-            val localReviews = movieReviewLocalDataSource.getReviewsForMovie(movieId)
-            if (localReviews.isNotEmpty() && localReviews.all { isDataFresh(it.reviewCacheDate) }) {
-                return localReviews.map { it.toEntity() }
-            }
+        return safeCall {
+            val movieCast =
+                movieDetailsRemoteDataSource.getMovieReviews(movieId, page, language)
+                    .results?.map { it.toEntity() } ?: emptyList()
 
-            if (networkConnectionChecker.isConnected.value) {
-                val remoteReviews =
-                    movieDetailsRemoteDataSource.getMovieReviews(movieId, page, language)
-                        .results?.map { it.toEntity() } ?: emptyList()
-                movieReviewLocalDataSource.addReview(remoteReviews.map { it.toLocalDto() })
-                return movieReviewLocalDataSource.getReviewsForMovie(movieId).map { it.toEntity() }
-            } else {
-                throw NoInternetConnectionException()
-            }
-        } catch (e: NoInternetConnectionException) {
-            throw e
-        } catch (_: Exception) {
-            return emptyList()
+            movieReviewLocalDataSource.getReviewsForMovie(movieId)
+                .filter { isDataFresh(it.reviewCacheDate) }
+                .takeIf { it.isNotEmpty() }
+                .also { movieReviewLocalDataSource.addReview(movieCast.map { it.toLocalDto() }) }
+                ?.map { it.toEntity() }
+                ?: emptyList()
         }
     }
 
@@ -171,6 +128,21 @@ class MovieRepositoryImpl(
         TODO("Not yet implemented")
     }
 
+    private suspend fun <T> safeCall(call: suspend () -> T): T {
+        return try {
+            call()
+        } catch (e: UnauthorizedException) {
+            throw e
+        } catch (e: RequestTimeoutException) {
+            throw e
+        } catch (e: ServerException) {
+            throw e
+        } catch (e: UnknownException) {
+            throw e
+        } catch (e: Exception) {
+            throw e
+        }
+    }
 
     @OptIn(ExperimentalTime::class)
     private fun isDataFresh(date: LocalDateTime): Boolean {
