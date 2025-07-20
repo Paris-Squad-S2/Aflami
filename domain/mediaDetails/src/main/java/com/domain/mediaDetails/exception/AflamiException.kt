70fac582eb939315001e6b65e701753c63b46f89
mohammed-akkad <momoo362514@gmail.com>
2025-07-20 13:54:41 +0300

    update aflami exception

--- domain/mediaDetails/src/main/java/com/domain/mediaDetails/exception/AflamiException.kt
+++ domain/mediaDetails/src/main/java/com/domain/mediaDetails/exception/AflamiException.kt
@@ -4,3 +4,6 @@ abstract class AflamiException(message:String) : Exception(message)
 
 class NetworkException(message: String) : AflamiException(message)
 class NoInternetConnectionException(message: String = "No internet connection") : AflamiException(message)
+class NoFoundTvShowException(message: String = "No found tv show") : AflamiException(message)
+class NoFundGalleryTvShowException(message: String = "no found gallery tv show"): AflamiException(message)
+class NoSeasonFoundException(message: String = "no found season tv show"): AflamiException(message)
