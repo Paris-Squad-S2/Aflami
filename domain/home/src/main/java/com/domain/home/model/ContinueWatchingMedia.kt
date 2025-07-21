7a615e52aefc996bca49cd70e2bcf882a8b67d62
dina othman <dinaosman581@gmail.com>
2025-07-21 11:53:32 +0300

    change yearOfRelease from String to LocalDate

--- domain/home/src/main/java/com/domain/home/model/ContinueWatchingMedia.kt
+++ domain/home/src/main/java/com/domain/home/model/ContinueWatchingMedia.kt
@@ -1,10 +1,11 @@
 package com.domain.home.model
 
+import kotlinx.datetime.LocalDate
 data class ContinueWatchingMedia(
     val id: Int,
     val title: String,
     val voteAverage: Double,
     val posterPath: String,
-    val releaseDate: String,
+    val yearOfRelease: LocalDate,
     val type : MediaType
 )
--- domain/home/src/main/java/com/domain/home/model/TopRatingMedia.kt
+++ domain/home/src/main/java/com/domain/home/model/TopRatingMedia.kt
@@ -1,10 +1,12 @@
 package com.domain.home.model
 
+import kotlinx.datetime.LocalDate
+
 data class TopRatingMedia(
     val id: Int,
     val title: String,
     val voteAverage: Double,
     val posterPath: String,
-    val releaseDate: String,
+    val yearOfRelease: LocalDate,
     val type : MediaType
 )
