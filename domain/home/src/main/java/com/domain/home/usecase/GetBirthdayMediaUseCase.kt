826318eca85f571193eee7d67926cef7da2ab898
dina othman <dinaosman581@gmail.com>
2025-07-21 18:21:49 +0300

    update GetBirthdayMediaUseCase

--- domain/home/src/main/java/com/domain/home/usecase/GetBirthdayMediaUseCase.kt
+++ domain/home/src/main/java/com/domain/home/usecase/GetBirthdayMediaUseCase.kt
@@ -1,12 +1,22 @@
 package com.domain.home.usecase
 
-import com.domain.home.model.BirthdayMedia
+import com.domain.home.model.Media
 import com.domain.home.repository.MediaRepository
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
 
 class GetBirthdayMediaUseCase(
     private val mediaRepository : MediaRepository
 ) {
-    suspend operator fun invoke(): List<BirthdayMedia>{
-        return mediaRepository.getBirthdayMedia()
+    @OptIn(ExperimentalTime::class)
+    suspend operator fun invoke(): List<Media> {
+        val now = Clock.System.now()
+        val today = now.toLocalDateTime(TimeZone.currentSystemDefault()).date
+        return mediaRepository.getPopularMedia().filter { media ->
+            media.yearOfRelease.month == today.month &&
+                    media.yearOfRelease.day == today.day
+        }
     }
 }
