915c5694a2062a0ecce21b3f28ee68784de4c95b
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-07-22 04:37:29 +0300

    feat: add HomeScreenUIState
    This commit introduces the `HomeScreenUIState` data class and associated data classes (`HomeUIState`, `MediaUiState`, `CategoryUiState`) and enum (`MediaTypeUi`) to represent the UI state for the home screen.
    
    These classes will be used to manage and display information such as popular media, continue watching list, top-rated media, movies celebrating birthdays, categories, and upcoming media. They also include states for loading and error messages, and a flag to show a mood picker dialog.

---
+++ feature/home/homeUi/src/main/java/com/feature/home/homeUi/screen/home/HomeScreenUIState.kt
@@ -0,0 +1,40 @@
+package com.feature.home.homeUi.screen.home
+
+import kotlinx.datetime.LocalDateTime
+
+data class HomeScreenUIState(
+    val homeUIState: HomeUIState,
+    val isLoading: Boolean,
+    val errorMessage: String?
+)
+
+data class HomeUIState(
+    val popularMediaList: List<MediaUiState>,
+    val continueWatchingMediaList: List<MediaUiState>,
+    val topRatedMediaList: List<MediaUiState>,
+    val moviesBirthdayMediaList: List<MediaUiState>,
+    val categories: Map<CategoryUiState, Boolean>,
+    val upComingMediaList: List<MediaUiState>,
+    val showMoodPickerDialog : Boolean
+    )
+
+data class MediaUiState(
+    val id: Int,
+    val imageUri: String,
+    val title: String,
+    val type: MediaTypeUi,
+    val categories: List<Int>,
+    val yearOfRelease: LocalDateTime,
+    val rating: Double,
+)
+
+data class CategoryUiState(
+    val id: Int,
+    val name: String,
+)
+
+
+enum class MediaTypeUi(val mediaName: String) {
+    TVSHOW("TV Show"),
+    MOVIE("Movie")
+}
