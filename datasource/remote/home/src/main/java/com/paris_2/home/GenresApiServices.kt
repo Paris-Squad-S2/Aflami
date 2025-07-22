899df9408ccdf737727e0079b4a9c51c9b5b17e5
dina othman <dinaosman581@gmail.com>
2025-07-22 13:01:35 +0300

    update GenresApiServices

--- datasource/remote/home/src/main/java/com/paris_2/home/GenresApiServices.kt
+++ datasource/remote/home/src/main/java/com/paris_2/home/GenresApiServices.kt
@@ -1,7 +1,13 @@
 package com.paris_2.home
 
 import com.paris_2.home.dto.GenresDto
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface GenresApiServices {
-    suspend fun getAllGenres(language: String): GenresDto
+    @GET("genre/movie/list")
+    suspend fun getAllGenres(
+        @Query("language")
+        language: String
+    ): GenresDto
 }
