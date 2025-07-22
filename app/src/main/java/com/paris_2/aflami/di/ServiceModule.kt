ee54bba7e46dcfdb81e5a0ef555cb63044fa8e21
mohammed-akkad <momoo362514@gmail.com>
2025-07-22 16:19:38 +0300

    update service module

--- app/src/main/java/com/paris_2/aflami/di/ServiceModule.kt
+++ app/src/main/java/com/paris_2/aflami/di/ServiceModule.kt
@@ -1,5 +1,7 @@
 package com.paris_2.aflami.di
 
+import com.datasource.remote.movie.service.RetrofitMovieDetailsApiService
+import com.datasource.remote.tvShow.service.RetrofitTvShowDetailsApiService
 import com.paris_2.datasource.remote.authentication.AuthenticationApi
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -7,4 +9,6 @@ import kotlin.jvm.java
 
 val serviceModule = module{
     single{get<Retrofit>().create(AuthenticationApi::class.java)}
+    single{get<Retrofit>().create(RetrofitMovieDetailsApiService::class.java)}
+    single{get<Retrofit>().create(RetrofitTvShowDetailsApiService::class.java)}
 }
