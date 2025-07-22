d6de2c10425e5c29b4cd6043a737cd8bb188de7d
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-07-22 04:39:45 +0300

    Refactor: move HomeScreen to new package
    The `HomeScreen.kt` file has been moved from `com.feature.home.homeUi.screen` to `com.feature.home.homeUi.screen.home`.
    
    The content of the file, which includes a `Composable` function `HomeScreen` and its preview `HomeScreenPreview`, remains largely the same but is now commented out.

---
+++ feature/home/homeUi/src/main/java/com/feature/home/homeUi/screen/home/HomeScreen.kt
@@ -0,0 +1,48 @@
+package com.feature.home.homeUi.screen.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+
+    /* Column(
+         modifier = modifier.fillMaxSize(),
+
+     ){
+         Box {
+             // Todo (Top Bar)
+             Slider(
+                 items = TODO(),
+                 onClick = TODO(),
+                 modifier = TODO()
+             )
+         }
+
+         // Todo ( Continue Watching )
+
+         // Todo (Top Rated )
+
+         // Todo ( Movie birthday )
+
+         MoodPicker(
+             modifier = TODO(),
+             backgroundColor = TODO(),
+             image = TODO(),
+             onEmojiClick = TODO(),
+             title = TODO(),
+             question = TODO()
+         )
+
+         // Todo ( upComing )
+
+         // Todo ( Bottom Bar )
+     }*/
+}
+
+@Preview
+@Composable
+private fun HomeScreenPreview() {
+    HomeScreen()
+}
