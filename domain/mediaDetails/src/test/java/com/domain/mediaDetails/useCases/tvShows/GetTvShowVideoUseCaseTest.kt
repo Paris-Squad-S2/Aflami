b6358acebc908992e194bd97b578e7f34e054377
mohammed-akkad <momoo362514@gmail.com>
2025-07-22 16:29:34 +0300

    add media details video use case test

---
+++ domain/mediaDetails/src/test/java/com/domain/mediaDetails/useCases/tvShows/GetTvShowVideoUseCaseTest.kt
@@ -0,0 +1,35 @@
+package com.domain.mediaDetails.useCases.tvShows
+
+import com.domain.mediaDetails.repository.TvShowRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import testUtils.fakeTvShowVideo
+
+class GetTvShowVideoUseCaseTest {
+    private lateinit var getTvShowVideoUseCase: GetTvShowVideoUseCase
+    private val tvShowRepository: TvShowRepository = mockk(relaxed = true)
+
+    @Before
+    fun setup() {
+        getTvShowVideoUseCase = GetTvShowVideoUseCase(tvShowRepository)
+    }
+
+    @Test
+    fun `should return tv show video from repository`() = runTest {
+        // Given
+        val tvShowId = 1
+
+        // when
+        coEvery { tvShowRepository.getTrailerVideoForTvShow(tvShowId) } returns fakeTvShowVideo
+
+        // Then
+        val result = getTvShowVideoUseCase(tvShowId)
+        assertEquals(result, fakeTvShowVideo.first())
+
+    }
+
+}
