678b7e2d5066389220c327d101ef6e7c29f320bb
a7med naser <ad0112727@gmail.com>
2025-07-23 17:04:52 +0300

    move home slider to components package

--- feature/home/homeUi/src/main/java/com/feature/home/homeUi/screen/home/components/HomeSlider.kt
+++ feature/home/homeUi/src/main/java/com/feature/home/homeUi/screen/home/components/HomeSlider.kt
@@ -1,4 +1,4 @@
-package com.feature.home.homeUi.screen.home.component
+package com.feature.home.homeUi.screen.home.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
