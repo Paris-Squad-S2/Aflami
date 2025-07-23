4730bd04f70abe8d8a5ffa13e33061f92bacefbe
dina othman <dinaosman581@gmail.com>
2025-07-23 10:58:03 +0300

    add MediaDao

---
+++ datasource/local/home/src/main/java/com/datasource/local/home/dao/MediaDao.kt
@@ -0,0 +1,17 @@
+package com.datasource.local.home.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.repository.home.entity.MediaEntity
+
+@Dao
+interface MediaDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addMedia(media: MediaEntity)
+
+    @Query("SELECT * FROM media_table")
+    suspend fun getAllMedia(): List<MediaEntity>
+}
