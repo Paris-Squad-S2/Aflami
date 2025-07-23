d71f42950c4a3e5e327c6971a198349d94a5ac9f
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-07-23 13:15:29 +0300

    Revert "update build gradle"
    This reverts commit ac10887f45b6a0a235b98b8a127eb01f5258b7d9.

--- datasource/remote/movie/build.gradle.kts
+++ datasource/remote/movie/build.gradle.kts
@@ -61,7 +61,7 @@ kover {
             verify {
                 rule {
                     bound {
-                        minValue = 0
+                        minValue = coverageMinValue
                     }
                 }
             }
