5566d8ef396f8089a33e56884fa39e672f87e495
mohammed-akkad <momoo362514@gmail.com>
2025-07-23 14:09:27 +0300

    change search data source to use retrfit instead of ktor

--- app/src/main/java/com/paris_2/aflami/di/ServiceModule.kt
+++ app/src/main/java/com/paris_2/aflami/di/ServiceModule.kt
@@ -3,12 +3,15 @@ package com.paris_2.aflami.di
 import com.datasource.remote.movie.service.RetrofitMovieDetailsApiService
 import com.datasource.remote.tvShow.service.RetrofitTvShowDetailsApiService
 import com.paris_2.datasource.remote.authentication.AuthenticationApi
+import com.repository.search.service.implementation.RetrofitGenresApiServices
+import com.repository.search.service.implementation.RetrofitSearchApiService
 import org.koin.dsl.module
 import retrofit2.Retrofit
-import kotlin.jvm.java
 
 val serviceModule = module{
     single{get<Retrofit>().create(AuthenticationApi::class.java)}
     single{get<Retrofit>().create(RetrofitMovieDetailsApiService::class.java)}
     single{get<Retrofit>().create(RetrofitTvShowDetailsApiService::class.java)}
+    single{get<Retrofit>().create(RetrofitSearchApiService::class.java)}
+    single{get<Retrofit>().create(RetrofitGenresApiServices::class.java)}
 }
