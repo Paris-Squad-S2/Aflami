81751593f5eb594adcf2f95188371b4727a18734
dina othman <dinaosman581@gmail.com>
2025-07-25 01:09:14 +0300

    handle Exceptions

--- domain/home/src/main/java/com/domain/home/exception/AflamiException.kt
+++ domain/home/src/main/java/com/domain/home/exception/AflamiException.kt
@@ -1,6 +1,14 @@
 package com.domain.home.exception
 
 abstract class AflamiException(message:String) : Exception(message)
+class NoCategoriesFoundException(message: String = "No categories found "): AflamiException(message)
+class NoPopularMediaFoundException(message: String = "No popular media found") : AflamiException(message)
 
-class NetworkException(message: String) : AflamiException(message)
-class NoCategoriesFoundException: AflamiException("No categories found ")
+class NoTopRatingMediaFoundException(message: String = "No top rating media found") : AflamiException(message)
+
+class NoUpComingMediaFoundException(message: String = "No UpComing media found") : AflamiException(message)
+
+class NoNowPlayingMediaFoundException(message: String = "No NowPlaying media found") : AflamiException(message)
+
+
+
--- repository/home/src/main/java/com/repository/home/repository/MediaRepositoryImpl.kt
+++ repository/home/src/main/java/com/repository/home/repository/MediaRepositoryImpl.kt
@@ -1,5 +1,10 @@
 package com.repository.home.repository
 
+
+import com.domain.home.exception.NoPopularMediaFoundException
+import com.domain.home.exception.NoTopRatingMediaFoundException
+import com.domain.home.exception.NoUpComingMediaFoundException
+import com.domain.home.exception.NoNowPlayingMediaFoundException
 import com.domain.home.model.Media
 import com.domain.home.model.MediaType
 import com.domain.home.repository.MediaRepository
@@ -16,49 +21,71 @@ class MediaRepositoryImpl(
 
     override suspend fun getPopularMedia(): List<Media> {
         val language = Locale.getDefault().language
-        val popularMovies = mediaRemoteDataSource.getPopularMovies(language = language).results?.mapNotNull {
-            it.toDomain(MediaType.MOVIE)
-        } ?: emptyList()
+        return try {
+            val popularMovies =
+                mediaRemoteDataSource.getPopularMovies(language = language).results?.mapNotNull {
+                    it.toDomain(MediaType.MOVIE)
+                } ?: emptyList()
 
-        val popularTvShows = mediaRemoteDataSource.getPopularTvShows(language = language).results?.mapNotNull {
-            it.toDomain(MediaType.TV_SHOW)
-        } ?: emptyList()
+            val popularTvShows =
+                mediaRemoteDataSource.getPopularTvShows(language = language).results?.mapNotNull {
+                    it.toDomain(MediaType.TV_SHOW)
+                } ?: emptyList()
 
-        val combined = (popularMovies + popularTvShows)
-            .sortedByDescending { it.voteAverage }
-
-        return combined
+            val combined = (popularMovies + popularTvShows)
+                .sortedByDescending { it.voteAverage }
+            if(combined.isEmpty()) throw NoPopularMediaFoundException()
+            combined
+        } catch (e: Exception) {
+            throw NoPopularMediaFoundException()
+        }
     }
 
     override suspend fun getTopRatingMedia(): List<Media> {
         val language = Locale.getDefault().language
-        val topMovies = mediaRemoteDataSource.getTopRatedMovies(language = language).results?.mapNotNull {
-            it.toDomain(MediaType.MOVIE)
-        } ?: emptyList()
+        return try {
+            val topMovies =
+                mediaRemoteDataSource.getTopRatedMovies(language = language).results?.mapNotNull {
+                    it.toDomain(MediaType.MOVIE)
+                } ?: emptyList()
 
-        val topTv = mediaRemoteDataSource.getTopRatedTvShows(language = language).results?.mapNotNull {
-            it.toDomain(MediaType.TV_SHOW)
-        } ?: emptyList()
+            val topTv =
+                mediaRemoteDataSource.getTopRatedTvShows(language = language).results?.mapNotNull {
+                    it.toDomain(MediaType.TV_SHOW)
+                } ?: emptyList()
 
-        val combined = (topMovies + topTv)
-            .sortedByDescending { it.voteAverage }
-
-        return combined
+            val combined = (topMovies + topTv)
+                .sortedByDescending { it.voteAverage }
+            if (combined.isEmpty()) throw NoTopRatingMediaFoundException()
+            combined
+        } catch (e: Exception) {
+            throw NoTopRatingMediaFoundException()
+        }
     }
 
     override suspend fun getUpComingMedia(): List<Media> {
         val language = Locale.getDefault().language
-        val upcomingMovies = mediaRemoteDataSource.getUpcomingMovies(language = language).results?.mapNotNull {
-            it.toDomain(MediaType.MOVIE)
-        } ?: emptyList()
-        return upcomingMovies
+        return try {
+            val upcomingMovies = mediaRemoteDataSource.getUpcomingMovies(language = language).results?.mapNotNull {
+                it.toDomain(MediaType.MOVIE)
+            } ?: emptyList()
+            if (upcomingMovies.isEmpty()) throw NoUpComingMediaFoundException()
+            upcomingMovies
+        } catch (e: Exception) {
+            throw NoUpComingMediaFoundException()
+        }
     }
 
     override suspend fun getNowPlayingMedia(): List<Media> {
-        val nowPlaying = mediaRemoteDataSource.getNowPlayingMovies().results?.mapNotNull {
-            it.toDomain(MediaType.MOVIE)
-        } ?: emptyList()
-        return nowPlaying
+        return try {
+            val nowPlaying = mediaRemoteDataSource.getNowPlayingMovies().results?.mapNotNull {
+                it.toDomain(MediaType.MOVIE)
+            } ?: emptyList()
+            if (nowPlaying.isEmpty()) throw NoNowPlayingMediaFoundException()
+            nowPlaying
+        } catch (e: Exception) {
+            throw NoNowPlayingMediaFoundException()
+        }
     }
 
     override suspend fun addMediaToLocal(media: Media) {
