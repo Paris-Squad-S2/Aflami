56428ece21c4186d6584021c81bbe134450ca06c
Mustafa Ibrahim <118114457+MustafaIbrahim96@users.noreply.github.com>
2025-07-24 04:54:07 +0300

    update ignore test file

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,8 +59,8 @@ val koverExcludedPackages = listOf(
     "**Exception**",
     "**SafeApiCall**",
     "**ApiServiceImpl**",
-    "**.AflamiNavBar",
-    "**.AppScafold",
+    "**.AflamiNavBar.*",
+    "**.AppScafold.*",
     "**/app**"
     /*
     "**.GenreResourceMapper",
