a89264df5f2d284faf79039cc14ec7a794f88aba
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-24 18:44:53 +0300

    add HasAnySessionUseCase to UseCaseModule for session management

--- app/src/main/java/com/paris_2/aflami/di/UseCaseModule.kt
+++ app/src/main/java/com/paris_2/aflami/di/UseCaseModule.kt
@@ -33,6 +33,7 @@ import com.domain.search.useCase.SortingMediaByCategoriesInteractionUseCase
 import com.paris_2.domain.authentication.usecase.GetForgetPasswordUrlUseCase
 import com.paris_2.domain.authentication.usecase.GetRegisterUrlUseCase
 import com.paris_2.domain.authentication.usecase.GuestLoginUseCase
+import com.paris_2.domain.authentication.usecase.HasAnySessionUseCase
 import com.paris_2.domain.authentication.usecase.IsLoggedInUseCase
 import com.paris_2.domain.authentication.usecase.LoginUseCase
 import org.koin.core.module.dsl.factoryOf
@@ -74,5 +75,6 @@ val useCaseModule = module {
     factoryOf(::GetMovieVideoUseCase)
     factoryOf(::GetTvShowVideoUseCase)
     factoryOf(::IsLoggedInUseCase)
+    factoryOf(::HasAnySessionUseCase)
 }
 
