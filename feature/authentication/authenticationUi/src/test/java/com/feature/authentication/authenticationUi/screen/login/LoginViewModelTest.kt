aca1f305450730976feb30cc039ef30368b17123
Mustafa Ibrahim <118114457+MustafaIbrahim96@users.noreply.github.com>
2025-07-24 05:39:20 +0300

    enhancement ci

--- feature/authentication/authenticationUi/src/test/java/com/feature/authentication/authenticationUi/screen/login/LoginViewModelTest.kt
+++ feature/authentication/authenticationUi/src/test/java/com/feature/authentication/authenticationUi/screen/login/LoginViewModelTest.kt
@@ -5,7 +5,6 @@ import com.paris_2.aflami.designsystem.components.ButtonState
 import com.paris_2.domain.authentication.exception.InvalidCredentialsException
 import com.paris_2.domain.authentication.usecase.LoginUseCase
 import io.mockk.coEvery
-import io.mockk.every
 import io.mockk.mockk
 import io.mockk.spyk
 import org.junit.jupiter.api.Assertions
@@ -15,7 +14,8 @@ import org.junit.jupiter.api.Test
 class LoginViewModelTest {
 
     private lateinit var viewModel: LoginViewModel
-    private lateinit var loginUseCase:LoginUseCase
+    private lateinit var loginUseCase: LoginUseCase
+
     @BeforeEach
     fun setup() {
         loginUseCase = mockk()
@@ -73,7 +73,12 @@ class LoginViewModelTest {
 
     @Test
     fun `onClickLogin with invalid credentials sets error message and disables button`() {
-        coEvery { loginUseCase("user", "1234") } throws(InvalidCredentialsException("Invalid credentials"))
+        coEvery {
+            loginUseCase(
+                "user",
+                "1234"
+            )
+        } throws (InvalidCredentialsException("Invalid credentials"))
 
         viewModel.onClickLogin()
 
