4a52724697d78afe6945e5bfff509332ec19a19e
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-25 02:28:00 +0300

    add rating dialog interaction methods to TvShowScreenInteractionListener

--- feature/mediaDetails/mediaDetailsUi/src/main/java/com/feature/mediaDetails/mediaDetailsUi/ui/screen/tvShow/details/TvShowScreenInteractionListener.kt
+++ feature/mediaDetails/mediaDetailsUi/src/main/java/com/feature/mediaDetails/mediaDetailsUi/ui/screen/tvShow/details/TvShowScreenInteractionListener.kt
@@ -8,5 +8,6 @@ interface TvShowScreenInteractionListener {
     fun onSimilarTvShowClick(mediaId: Int)
     fun onClickPlayTrailer()
     fun onRetryLoadTvShowDetails()
-
+    fun onDismissRatingDialog()
+    fun onRatingSubmitted(rating: Float)
 }
