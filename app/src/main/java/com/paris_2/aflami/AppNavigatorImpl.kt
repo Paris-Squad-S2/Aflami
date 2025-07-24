6814fa5d5fc2fc0b2d595d7551fc7a4fe335ba9b
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-24 03:08:38 +0300

    refactor: integrate AuthenticationRepository for dynamic start destination handling

--- app/src/main/java/com/paris_2/aflami/AppNavigatorImpl.kt
+++ app/src/main/java/com/paris_2/aflami/AppNavigatorImpl.kt
@@ -2,19 +2,31 @@ package com.paris_2.aflami
 
 import androidx.navigation.NavOptions
 import com.paris_2.aflami.appnavigation.AppDestination
+import com.paris_2.aflami.appnavigation.AppDestinations
 import com.paris_2.aflami.appnavigation.AppGraph
 import com.paris_2.aflami.appnavigation.AppNavigationEvent
 import com.paris_2.aflami.appnavigation.AppNavigator
+import com.paris_2.domain.authentication.repository.AuthenticationRepository
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 
-class AppNavigatorImpl(override val startGraph: AppGraph) : AppNavigator {
+class AppNavigatorImpl(
+    override val startGraph: AppGraph,
+    private val authRepository: AuthenticationRepository
+) : AppNavigator {
     private val _navigateEvent = Channel<AppNavigationEvent>()
     override val navigationEvent = _navigateEvent.receiveAsFlow()
     private val mutex = Mutex()
     private var lastNavigateTime = 0L
+
+    override val startDestination: AppDestination
+        get() = if (authRepository.isLoggedIn()) {
+            AppDestinations.HomeFeature()
+        } else {
+            AppDestinations.AuthenticationFeature()
+        }
 
     override suspend fun navigate(destination: AppDestination, navOptions: NavOptions?) {
         mutex.withLock {
