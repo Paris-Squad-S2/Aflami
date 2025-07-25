16ca0d90d10aedc956dadd214f7cb21a1f624e29
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-25 03:54:29 +0300

    remove unused addMovieToFavorite function from MovieRepositoryImpl

--- repository/movie/src/main/java/com/repository/movie/repository/MovieRepositoryImpl.kt
+++ repository/movie/src/main/java/com/repository/movie/repository/MovieRepositoryImpl.kt
@@ -178,10 +178,6 @@ class MovieRepositoryImpl(
         }
     }
 
-    override suspend fun addMovieToFavorite(movieId: Int) {
-        TODO("Not yet implemented")
-    }
-
     override suspend fun getTrailerVideoForMovie(movieId: Int): List<MovieVideo> {
         return safeCall(NoVideoFoundException()) {
             movieDetailsRemoteDataSource.getTrailerVideoForMovie(movieId)
