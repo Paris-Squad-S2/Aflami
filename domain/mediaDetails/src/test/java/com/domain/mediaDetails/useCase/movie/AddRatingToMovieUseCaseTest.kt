9237ff58ab87a0332691c4aff5c0a672869c8e12
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-25 05:41:53 +0300

    rename test class to AddRatingToMovieUseCaseTest

--- domain/mediaDetails/src/test/java/com/domain/mediaDetails/useCase/movie/AddRatingToMovieUseCaseTest.kt
+++ domain/mediaDetails/src/test/java/com/domain/mediaDetails/useCase/movie/AddRatingToMovieUseCaseTest.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-class AddRatingToTvShowUseCaseTest {
+class AddRatingToMovieUseCaseTest {
 
     private lateinit var repository: TvShowRepository
     private lateinit var useCase: AddRatingToTvShowUseCase
