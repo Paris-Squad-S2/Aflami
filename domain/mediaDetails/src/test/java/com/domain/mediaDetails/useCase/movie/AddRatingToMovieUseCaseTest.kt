0503170127ac9394dd4cc8a2ce5ea1be0a9e2dff
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-25 04:25:32 +0300

    add unit test for AddRatingToTvShowUseCase

---
+++ domain/mediaDetails/src/test/java/com/domain/mediaDetails/useCase/movie/AddRatingToMovieUseCaseTest.kt
@@ -0,0 +1,34 @@
+package com.domain.mediaDetails.useCase.movie
+
+import com.domain.mediaDetails.repository.TvShowRepository
+import com.domain.mediaDetails.useCase.tvShows.AddRatingToTvShowUseCase
+import io.mockk.coVerify
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class AddRatingToTvShowUseCaseTest {
+
+    private lateinit var repository: TvShowRepository
+    private lateinit var useCase: AddRatingToTvShowUseCase
+
+    @BeforeEach
+    fun setUp() {
+        repository = mockk(relaxed = true)
+        useCase = AddRatingToTvShowUseCase(repository)
+    }
+
+    @Test
+    fun `invoke should call addRatingToTvShow on repository`() = runTest {
+        // Arrange
+        coEvery { repository.addRatingToTvShow() } returns Unit
+
+        // Act
+        useCase()
+
+        // Assert
+        coVerify(exactly = 1) { repository.addRatingToTvShow() }
+    }
+}
