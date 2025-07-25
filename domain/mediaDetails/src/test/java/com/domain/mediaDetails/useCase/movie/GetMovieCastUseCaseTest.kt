bb3c15eeeec4a1b1a0918e876a2905abd5f3f4d7
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-25 04:43:27 +0300

    refactor test setup method to use JUnit 5 conventions

--- domain/mediaDetails/src/test/java/com/domain/mediaDetails/useCase/movie/GetMovieCastUseCaseTest.kt
+++ domain/mediaDetails/src/test/java/com/domain/mediaDetails/useCase/movie/GetMovieCastUseCaseTest.kt
@@ -5,7 +5,7 @@ import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
-import org.junit.Before
+import org.junit.jupiter.api.BeforeEach
 import testUtils.fakeCast
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -15,7 +15,7 @@ class GetMovieCastUseCaseTest {
     private lateinit var getMovieCastUseCase: GetMovieCastUseCase
     private val movieRepository: MovieRepository = mockk(relaxed = true)
 
-    @Before
+    @BeforeEach
     fun setup() {
         getMovieCastUseCase = GetMovieCastUseCase(movieRepository)
     }
