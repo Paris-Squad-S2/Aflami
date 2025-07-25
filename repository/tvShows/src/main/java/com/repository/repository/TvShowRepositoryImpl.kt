f454b274c30f7c18609104c70e4e61bcc5311261
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-25 03:55:58 +0300

    remove unused addTvShowToFavorite function from TvShowRepositoryImpl

--- repository/tvShows/src/main/java/com/repository/repository/TvShowRepositoryImpl.kt
+++ repository/tvShows/src/main/java/com/repository/repository/TvShowRepositoryImpl.kt
@@ -178,10 +178,6 @@ class TvShowRepositoryImpl(
         }
     }
 
-    override suspend fun addTvShowToFavorite(tvShowId: Int) {
-        TODO("Not yet implemented")
-    }
-
     override suspend fun addRatingToTvShow(){
         print("Rating added to Tv Show")
     }
