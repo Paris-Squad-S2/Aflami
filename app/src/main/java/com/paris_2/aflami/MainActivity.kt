20ee7487241da96cf1b0fbe5099e55a31ab19f2f
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-07-27 09:03:46 +0300

    Refactor: Update MainActivity for Hilt integration
    The `MainActivity.kt` has been updated to use Hilt for dependency injection.
    
    - The class is now annotated with `@AndroidEntryPoint`.
    - The previous Koin-based dependency retrieval for `AuthenticationFeatureAPI`, `HasAnySessionUseCase`, and `AppNavigationAPI` has been commented out, along with the `setContent` block that utilized these instances.
    
    This change prepares the `MainActivity` for Hilt's dependency injection mechanism, which will manage the creation and provision of these dependencies. The commented-out code suggests a future implementation where Hilt will provide these dependencies for setting the content based on session state.

--- app/src/main/java/com/paris_2/aflami/MainActivity.kt
+++ app/src/main/java/com/paris_2/aflami/MainActivity.kt
@@ -2,29 +2,25 @@ package com.paris_2.aflami
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.feature.authentication.authenticationApi.AuthenticationFeatureAPI
-import com.paris_2.aflami.appnavigation.AppNavigationAPI
-import com.paris_2.aflami.designsystem.theme.AflamiTheme
-import com.paris_2.domain.authentication.usecase.HasAnySessionUseCase
-import org.koin.android.ext.android.get
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val authenticationFeatureAPI: AuthenticationFeatureAPI = get()
-        val hasAnySessionUseCase : HasAnySessionUseCase = get()
-        val appNavigationAPI : AppNavigationAPI = get()
-        setContent {
-            AflamiTheme {
-                if (hasAnySessionUseCase()){
-                    appNavigationAPI()
-                } else {
-                    authenticationFeatureAPI()
-                }
-            }
-        }
+        /* val authenticationFeatureAPI: AuthenticationFeatureAPI = get()
+         val hasAnySessionUseCase : HasAnySessionUseCase = get()
+         val appNavigationAPI : AppNavigationAPI = get()
+         setContent {
+             AflamiTheme {
+                 if (hasAnySessionUseCase()){
+                     appNavigationAPI()
+                 } else {
+                     authenticationFeatureAPI()
+                 }
+             }
+         }*/
     }
 }
