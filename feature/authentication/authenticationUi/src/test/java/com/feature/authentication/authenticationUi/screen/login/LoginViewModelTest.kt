19f97171a9b5e9d2cd906a110ca3547310602287
dina othman <dinaosman581@gmail.com>
2025-07-27 20:17:49 +0300

    solve build

--- feature/authentication/authenticationUi/src/test/java/com/feature/authentication/authenticationUi/screen/login/LoginViewModelTest.kt
+++ feature/authentication/authenticationUi/src/test/java/com/feature/authentication/authenticationUi/screen/login/LoginViewModelTest.kt
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.Test
 class LoginViewModelTest {
 
 
+
     private lateinit var viewModel: LoginViewModel
     private lateinit var loginUseCase: LoginUseCase
     private lateinit var appNavigationAPI: AppNavigationAPI
