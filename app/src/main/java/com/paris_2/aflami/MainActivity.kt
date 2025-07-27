7476c15f4a59659ceb6c338bf9f535bff33f8474
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-07-27 22:50:45 +0300

    replace Koin DI with Hilt in app navigation and scaffold; update MainActivity to use Hilt-injected APIs

--- app/src/main/java/com/paris_2/aflami/MainActivity.kt
+++ app/src/main/java/com/paris_2/aflami/MainActivity.kt
@@ -2,25 +2,31 @@ package com.paris_2.aflami
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.feature.authentication.authenticationApi.AuthenticationFeatureAPI
+import com.paris_2.aflami.appnavigation.AppNavigationAPI
+import com.paris_2.aflami.designsystem.theme.AflamiTheme
+import com.paris_2.domain.authentication.usecase.HasAnySessionUseCase
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject lateinit var authenticationFeatureAPI: AuthenticationFeatureAPI
+    @Inject lateinit var hasAnySessionUseCase: HasAnySessionUseCase
+    @Inject lateinit var appNavigationAPI: AppNavigationAPI
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        /* val authenticationFeatureAPI: AuthenticationFeatureAPI = get()
-         val hasAnySessionUseCase : HasAnySessionUseCase = get()
-         val appNavigationAPI : AppNavigationAPI = get()
-         setContent {
-             AflamiTheme {
-                 if (hasAnySessionUseCase()){
-                     appNavigationAPI()
-                 } else {
-                     authenticationFeatureAPI()
-                 }
-             }
-         }*/
+        setContent {
+            AflamiTheme {
+                if (hasAnySessionUseCase()){
+                    appNavigationAPI()
+                } else {
+                    authenticationFeatureAPI()
+                }
+            }
+        }
     }
 }
