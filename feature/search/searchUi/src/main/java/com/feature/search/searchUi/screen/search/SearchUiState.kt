396453dcc33ca904f5fb06003eec7b50c84a98ab
Renad_Alalfy <renadalalfy@gmail.com>
2025-07-27 20:59:06 +0300

    Refactor: Move `NoopListUpdateCallback` to `SearchUiState.kt`

--- feature/search/searchUi/src/main/java/com/feature/search/searchUi/screen/search/SearchUiState.kt
+++ feature/search/searchUi/src/main/java/com/feature/search/searchUi/screen/search/SearchUiState.kt
@@ -1,4 +1,5 @@
 import androidx.paging.PagingData
+import androidx.recyclerview.widget.ListUpdateCallback
 import com.feature.search.searchUi.R
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
@@ -58,4 +59,11 @@ enum class SearchTypeUi(val displayNameResId: Int) {
     Country(R.string.country),
     Actor(R.string.actor);
 
+}
+
+class NoopListUpdateCallback : ListUpdateCallback {
+    override fun onInserted(position: Int, count: Int) {}
+    override fun onRemoved(position: Int, count: Int) {}
+    override fun onMoved(fromPosition: Int, toPosition: Int) {}
+    override fun onChanged(position: Int, count: Int, payload: Any?) {}
 }
