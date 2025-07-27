9e9d08ed7c362eb7c584d3e805e2c859ee0138bd
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-07-27 03:21:52 +0300

    adjust fire icon size in top rating section related to issue number #116
    The `HomeSection.kt` composable has been updated:
    - The icon within the section header now has a fixed size of 16dp width and 18dp height.
    - The top padding for the `SectionHeader` component has been removed.

--- feature/home/homeUi/src/main/java/com/feature/home/homeUi/screen/home/components/HomeSection.kt
+++ feature/home/homeUi/src/main/java/com/feature/home/homeUi/screen/home/components/HomeSection.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
@@ -39,12 +41,13 @@ fun HomeSection(
                 leadingIconPainter?.let { it->   Icon(
                     imageVector = it,
                     contentDescription = "$title icon",
-                    modifier = Modifier.padding(start = 8.dp),
+                    modifier = Modifier
+                        .padding(start = 8.dp)
+                        .size(width = 16.dp, height = 18.dp),
                     tint = iconColor ?: Color.Unspecified,
                 )
             }},
             onClickViewAll = onSectionAllClick,
-            modifier = Modifier.padding(top = 6.dp)
         )
 
         LazyRow(
