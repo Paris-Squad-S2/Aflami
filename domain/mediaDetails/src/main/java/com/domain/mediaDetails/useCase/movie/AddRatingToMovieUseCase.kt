e4f065ab0118140c3a3f3403b7e2fcc073c1a08b
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-28 18:19:29 +0300

    implement movie rating functionality

--- domain/mediaDetails/src/main/java/com/domain/mediaDetails/useCase/movie/AddRatingToMovieUseCase.kt
+++ domain/mediaDetails/src/main/java/com/domain/mediaDetails/useCase/movie/AddRatingToMovieUseCase.kt
@@ -5,7 +5,11 @@ import com.domain.mediaDetails.repository.MovieRepository
 class AddRatingToMovieUseCase(
     private val movieRepository: MovieRepository
 ) {
-    suspend operator fun invoke() {
-        return movieRepository.addRatingToMovie()
+    suspend operator fun invoke(movieId: Int, rating: Float, sessionId: String) {
+        return movieRepository.addRatingToMovie(
+            movieId,
+            rating,
+            sessionId
+        )
     }
 }
