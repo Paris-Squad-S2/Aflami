a26191960d6b4ac87ce969c06501aa6ac6c50589
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-28 18:22:33 +0300

    add RatingResponseDto data class

--- datasource/remote/movie/src/main/java/com/datasource/remote/movie/service/RetrofitMovieDetailsApiService.kt
+++ datasource/remote/movie/src/main/java/com/datasource/remote/movie/service/RetrofitMovieDetailsApiService.kt
@@ -6,7 +6,11 @@ import com.repository.movie.models.remote.MovieImagesDto
 import com.repository.movie.models.remote.MovieReviewsDto
 import com.repository.movie.models.remote.MovieSimilarsDto
 import com.repository.movie.models.remote.MovieVideoDto
+import com.repository.movie.models.remote.RatingDto
+import com.repository.movie.models.remote.RatingResponseDto
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -44,4 +48,11 @@ interface RetrofitMovieDetailsApiService {
     suspend fun getTrailerVideoForMovie(
         @Path("movie_id") movieId: Int
     ): MovieVideoDto
+
+    @POST("movie/{movie_id}/rating")
+    suspend fun addRatingToMovie(
+        @Path("movie_id") movieId: Int,
+        @Query("session_id") sessionId: String,
+        @Body rating: RatingDto
+    ): RatingResponseDto
 }
