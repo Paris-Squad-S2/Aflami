135caff8313d4e2b22e7047c10b28f2306bd9148
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-28 19:03:49 +0300

    refactor addRatingToTvShow function

--- domain/mediaDetails/src/main/java/com/domain/mediaDetails/repository/TvShowRepository.kt
+++ domain/mediaDetails/src/main/java/com/domain/mediaDetails/repository/TvShowRepository.kt
@@ -23,5 +23,5 @@ interface TvShowRepository {
         tvShowId: Int, seasonNumber: Int,
         episodeNumber: Int,
     ): List<EpisodeVideo>
-    suspend fun addRatingToTvShow()
+    suspend fun addRatingToTvShow(movieId: Int, rating: Float, sessionId: String)
 }
