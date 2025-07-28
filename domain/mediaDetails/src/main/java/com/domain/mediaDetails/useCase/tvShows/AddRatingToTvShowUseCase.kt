0a00eb890ddc2889ff3d3cd918c9a92be1f66891
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-28 19:02:49 +0300

    refactor AddRatingToTvShowUseCase

--- domain/mediaDetails/src/main/java/com/domain/mediaDetails/useCase/tvShows/AddRatingToTvShowUseCase.kt
+++ domain/mediaDetails/src/main/java/com/domain/mediaDetails/useCase/tvShows/AddRatingToTvShowUseCase.kt
@@ -5,7 +5,11 @@ import com.domain.mediaDetails.repository.TvShowRepository
 class AddRatingToTvShowUseCase(
     private val tvShowRepository: TvShowRepository
 ) {
-    suspend operator fun invoke() {
-        return tvShowRepository.addRatingToTvShow()
+    suspend operator fun invoke(movieId: Int, rating: Float, sessionId: String) {
+        return tvShowRepository.addRatingToTvShow(
+            movieId,
+            rating,
+            sessionId
+        )
     }
 }
