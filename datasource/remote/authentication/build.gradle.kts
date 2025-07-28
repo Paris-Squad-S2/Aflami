329c0fa15b0045cd966b83041d513338aee233df
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-07-28 13:03:02 +0300

    add Hilt dependencies and plugins to authentication build configuration

--- datasource/remote/authentication/build.gradle.kts
+++ datasource/remote/authentication/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -47,6 +49,11 @@ dependencies {
 
     //retrofit
     implementation(libs.retrofit)
+
+    //Hilt
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.hilt.navigation.compose)
+    ksp(libs.hilt.android.compiler)
 }
 
 val coverageMinValue: Int = (findProperty("coverageMinValue") as String).toInt()
