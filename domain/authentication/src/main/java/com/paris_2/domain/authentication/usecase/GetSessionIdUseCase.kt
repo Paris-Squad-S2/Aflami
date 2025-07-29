ae8a96155992059067d2bc4d122d126b62d47dfa
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-29 19:36:01 +0300

    refactor change getSessionId to suspend

--- domain/authentication/src/main/java/com/paris_2/domain/authentication/usecase/GetSessionIdUseCase.kt
+++ domain/authentication/src/main/java/com/paris_2/domain/authentication/usecase/GetSessionIdUseCase.kt
@@ -5,7 +5,7 @@ import com.paris_2.domain.authentication.repository.AuthenticationRepository
 class GetSessionIdUseCase(
     private val authenticationRepository: AuthenticationRepository
 ) {
-    operator fun invoke(): String? {
+    suspend operator fun invoke(): String? {
         return authenticationRepository.getSessionId()
     }
 }
