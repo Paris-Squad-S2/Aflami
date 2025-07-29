ad1ad921d7145c2a5ba6ee0a8cd663da2cf18ed1
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-29 20:57:53 +0300

    refactor update RatingResponseDto to use nullable properties with serialization annotations

--- repository/movie/src/main/java/com/repository/movie/models/remote/RatingResponseDto.kt
+++ repository/movie/src/main/java/com/repository/movie/models/remote/RatingResponseDto.kt
@@ -1,9 +1,10 @@
 package com.repository.movie.models.remote
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class RatingResponseDto(
-    val status_code: Int,
-    val status_message: String
+    @SerialName("status_code") val statusCode: Int? = null,
+    @SerialName("status_message") val statusMessage: String? = null
 )
