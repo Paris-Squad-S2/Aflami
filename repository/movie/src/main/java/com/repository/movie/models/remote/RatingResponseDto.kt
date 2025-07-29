e1458d2c23a7501bcdbedbf641f97a920596f5b9
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-29 19:38:11 +0300

    add serialization support to RatingResponseDto

--- repository/movie/src/main/java/com/repository/movie/models/remote/RatingResponseDto.kt
+++ repository/movie/src/main/java/com/repository/movie/models/remote/RatingResponseDto.kt
@@ -1,5 +1,8 @@
 package com.repository.movie.models.remote
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class RatingResponseDto(
     val status_code: Int,
     val status_message: String
