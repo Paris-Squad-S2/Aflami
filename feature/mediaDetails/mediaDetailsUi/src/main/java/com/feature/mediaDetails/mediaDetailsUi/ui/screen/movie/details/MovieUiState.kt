15dfba53308247f2d5e438b7f79e93eb00f9fb08
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-29 21:03:58 +0300

    refactor update snackBarMessage in MovieUiState and TvShowUiState to use nullable type

--- feature/mediaDetails/mediaDetailsUi/src/main/java/com/feature/mediaDetails/mediaDetailsUi/ui/screen/movie/details/MovieUiState.kt
+++ feature/mediaDetails/mediaDetailsUi/src/main/java/com/feature/mediaDetails/mediaDetailsUi/ui/screen/movie/details/MovieUiState.kt
@@ -2,7 +2,6 @@ package com.feature.mediaDetails.mediaDetailsUi.ui.screen.movie.details
 
 import androidx.annotation.StringRes
 import androidx.paging.PagingData
-import com.feature.mediaDetails.mediaDetailsUi.R
 import com.feature.mediaDetails.mediaDetailsUi.ui.screen.MediaUi
 import com.feature.mediaDetails.mediaDetailsUi.ui.screen.SimilarMediaUI
 import kotlinx.coroutines.flow.Flow
@@ -19,7 +18,7 @@ data class MovieDetailsScreenState(
     val isGalleryLoading: Boolean = false,
     val isProductionCompaniesLoading: Boolean = false,
     val showRatingDialog: Boolean = false,
-    @StringRes val snackBarMessage: Int = R.string.failed_to_submit_rating,
+    @StringRes val snackBarMessage: Int? = null,
     val showSnackBar: Boolean = false,
     val snackBarSuccess: Boolean = false
 )
--- feature/mediaDetails/mediaDetailsUi/src/main/java/com/feature/mediaDetails/mediaDetailsUi/ui/screen/tvShow/details/TvShowUiState.kt
+++ feature/mediaDetails/mediaDetailsUi/src/main/java/com/feature/mediaDetails/mediaDetailsUi/ui/screen/tvShow/details/TvShowUiState.kt
@@ -2,7 +2,6 @@ package com.feature.mediaDetails.mediaDetailsUi.ui.screen.tvShow.details
 
 import androidx.annotation.StringRes
 import androidx.paging.PagingData
-import com.feature.mediaDetails.mediaDetailsUi.R
 import com.feature.mediaDetails.mediaDetailsUi.ui.screen.MediaUi
 import com.feature.mediaDetails.mediaDetailsUi.ui.screen.SimilarMediaUI
 import com.feature.mediaDetails.mediaDetailsUi.ui.screen.movie.details.CastUi
@@ -25,7 +24,7 @@ data class TvShowDetailsScreenState(
     val isProductionCompaniesLoading: Boolean = false,
     val seasonsLoadingStates: Map<Int, Boolean> = emptyMap(),
     val showRatingDialog: Boolean = false,
-    @StringRes val snackBarMessage: Int = R.string.not_found_video,
+    @StringRes val snackBarMessage: Int? = null,
     val showSnackBar: Boolean = false,
     val snackBarSuccess: Boolean = false
 )
