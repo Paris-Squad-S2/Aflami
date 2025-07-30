8d81e160f67c442099905d68cffab414462a410c
a7med naser <ad0112727@gmail.com>
2025-07-30 17:48:35 +0300

    cancel pagination and set default page

--- domain/mediaDetails/src/main/java/com/domain/mediaDetails/useCase/tvShows/GetTvShowReviewsUseCase.kt
+++ domain/mediaDetails/src/main/java/com/domain/mediaDetails/useCase/tvShows/GetTvShowReviewsUseCase.kt
@@ -6,7 +6,7 @@ import com.domain.mediaDetails.repository.TvShowRepository
 class GetTvShowReviewsUseCase(
     private val tbShowRepository: TvShowRepository
 ) {
-    suspend operator fun invoke(tvShowId: Int, page: Int): List<Review> {
+    suspend operator fun invoke(tvShowId: Int, page: Int=1): List<Review> {
         return tbShowRepository.getTvShowReview(tvShowId, page)
     }
 
