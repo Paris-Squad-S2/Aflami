1e12a830898652620e4e4a6d38a9dd764dc86ac1
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-07-30 07:32:26 +0300

    Refactor: Remove comment line
    The `border` modifier has been removed from the `IconBox` composable within `TopAppBar.kt`. This eliminates the 1dp stroke with `Theme.colors.stroke` color and a `RoundedCornerShape(12.dp)` that was previously applied.

--- designsystem/src/main/java/com/paris_2/aflami/designsystem/components/TopAppBar.kt
+++ designsystem/src/main/java/com/paris_2/aflami/designsystem/components/TopAppBar.kt
@@ -133,7 +133,6 @@ private fun IconBox(
     Box(
         modifier = modifier
             .size(40.dp)
-//            .border(1.dp, color = Theme.colors.stroke, shape = RoundedCornerShape(12.dp))
             .clip(RoundedCornerShape(12.dp))
             .background(iconItem.backgroundColor)
             .then(
