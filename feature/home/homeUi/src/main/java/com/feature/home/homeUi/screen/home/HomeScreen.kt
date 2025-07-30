3ebe62d84183d608493cf3a5ad3306bba374f3ff
dina othman <dinaosman581@gmail.com>
2025-07-30 09:01:48 +0300

    localize some texts

--- feature/home/homeUi/src/main/java/com/feature/home/homeUi/screen/home/HomeScreen.kt
+++ feature/home/homeUi/src/main/java/com/feature/home/homeUi/screen/home/HomeScreen.kt
@@ -252,7 +252,7 @@ fun HomeScreenContent(
 
     TopAppBar(
         title = stringResource(R.string.aflami),
-        subtitle = "More than just watching.",
+        subtitle = stringResource(R.string.more_than_just_watching),
         titleTextStyle = Theme.textStyle.logoText,
         modifier = Modifier
             .background(topBarBackground)
