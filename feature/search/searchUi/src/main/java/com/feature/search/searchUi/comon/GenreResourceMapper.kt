69998a2befcb873e96b746c07f74dd18b0514865
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-31 21:24:10 +0300

    refactor convert genre mapping to use enum class for improved type safety

--- feature/search/searchUi/src/main/java/com/feature/search/searchUi/comon/GenreResourceMapper.kt
+++ feature/search/searchUi/src/main/java/com/feature/search/searchUi/comon/GenreResourceMapper.kt
@@ -1,40 +1,84 @@
 package com.feature.search.searchUi.comon
 
+import CategoryUiState
 import com.feature.search.searchUi.R
+
+enum class Genre(
+    val id: Int,
+    val displayName: String
+) {
+    Action(28, "Action"),
+    Adventure(12, "Adventure"),
+    Animation(16, "Animation"),
+    Comedy(35, "Comedy"),
+    Crime(80, "Crime"),
+    Documentary(99, "Documentary"),
+    Drama(18, "Drama"),
+    Family(10751, "Family"),
+    Fantasy(14, "Fantasy"),
+    History(36, "History"),
+    Horror(27, "Horror"),
+    Music(10402, "Music"),
+    Mystery(9648, "Mystery"),
+    Romance(10749, "Romance"),
+    ScienceFiction(878, "Science Fiction"),
+    TVMovie(10770, "TV Movie"),
+    Thriller(53, "Thriller"),
+    War(10752, "War"),
+    Western(37, "Western"),
+    ActionAdventure(10759, "Action & Adventure"),
+    Kids(10762, "Kids"),
+    News(10763, "News"),
+    Reality(10764, "Reality"),
+    SciFiFantasy(10765, "Sci-Fi & Fantasy"),
+    Soap(10766, "Soap"),
+    Talk(10767, "Talk"),
+    WarPolitics(10768, "War & Politics");
+
+    companion object {
+        fun fromId(id: Int): Genre? = values().find { it.id == id }
+        fun fromName(name: String): Genre? = values().find { it.displayName == name }
+    }
+}
 
 object GenreResourceMapper {
     private val genreToResourceMap = mapOf(
-        28 to R.drawable.ic_category_action,
-        12 to R.drawable.ic_category_adventure,
-        16 to R.drawable.ic_category_animation,
-        35 to R.drawable.ic_category_comedy,
-        80 to R.drawable.ic_category_crime,
-        99 to R.drawable.ic_category_documentary,
-        18 to R.drawable.ic_category_drama,
-        10751 to R.drawable.ic_category_family,
-        14 to R.drawable.ic_category_fantasy,
-        36 to R.drawable.ic_category_history,
-        27 to R.drawable.ic_category_horror,
-        10402 to R.drawable.ic_category_music,
-        9648 to R.drawable.ic_category_mystery,
-        10749 to R.drawable.ic_category_romance,
-        878 to R.drawable.ic_category_science_fiction,
-        10770 to R.drawable.ic_category_tv_movie,
-        53 to R.drawable.ic_category_thriller,
-        10752 to R.drawable.ic_category_war,
-        37 to R.drawable.ic_category_western,
-
-        10759 to R.drawable.ic_category_action_and_adventure,
-        10762 to R.drawable.ic_category_kids,
-        10763 to R.drawable.ic_category_news,
-        10764 to R.drawable.ic_category_reality,
-        10765 to R.drawable.ic_category_fantasy,
-        10766 to R.drawable.ic_category_soap,
-        10767 to R.drawable.ic_category_talk,
-        10768 to R.drawable.ic_category_war
+        Genre.Action to R.drawable.ic_category_action,
+        Genre.Adventure to R.drawable.ic_category_adventure,
+        Genre.Animation to R.drawable.ic_category_animation,
+        Genre.Comedy to R.drawable.ic_category_comedy,
+        Genre.Crime to R.drawable.ic_category_crime,
+        Genre.Documentary to R.drawable.ic_category_documentary,
+        Genre.Drama to R.drawable.ic_category_drama,
+        Genre.Family to R.drawable.ic_category_family,
+        Genre.Fantasy to R.drawable.ic_category_fantasy,
+        Genre.History to R.drawable.ic_category_history,
+        Genre.Horror to R.drawable.ic_category_horror,
+        Genre.Music to R.drawable.ic_category_music,
+        Genre.Mystery to R.drawable.ic_category_mystery,
+        Genre.Romance to R.drawable.ic_category_romance,
+        Genre.ScienceFiction to R.drawable.ic_category_science_fiction,
+        Genre.TVMovie to R.drawable.ic_category_tv_movie,
+        Genre.Thriller to R.drawable.ic_category_thriller,
+        Genre.War to R.drawable.ic_category_war,
+        Genre.Western to R.drawable.ic_category_western,
+        Genre.ActionAdventure to R.drawable.ic_category_action_and_adventure,
+        Genre.Kids to R.drawable.ic_category_kids,
+        Genre.News to R.drawable.ic_category_news,
+        Genre.Reality to R.drawable.ic_category_reality,
+        Genre.SciFiFantasy to R.drawable.ic_category_fantasy,
+        Genre.Soap to R.drawable.ic_category_soap,
+        Genre.Talk to R.drawable.ic_category_talk,
+        Genre.WarPolitics to R.drawable.ic_category_war
     )
 
-    fun getResourceId(genreId: Int): Int {
-        return genreToResourceMap[genreId] ?: R.drawable.ic_category_all
+    fun getResourceId(genre: Genre?): Int {
+        return genreToResourceMap[genre] ?: R.drawable.ic_category_all
+    }
+}
+
+object CategoryGenreMapper {
+    fun mapToGenre(category: CategoryUiState): Genre? {
+        return Genre.values().find { it.id == category.id }
     }
 }
