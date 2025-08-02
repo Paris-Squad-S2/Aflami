51fd30bb8d280ca815d30de2a7379c3c91c9a5d8
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-08-02 16:35:35 +0300

    refactor: remove DOMAIN_CATEGORIES module and update related imports

--- build.gradle.kts
+++ build.gradle.kts
@@ -103,7 +103,6 @@ dependencies {
     kover(project((Modules.DOMAIN_MEDIA)))
     kover(project((Modules.DOMAIN_LISTS)))
     kover(project((Modules.DOMAIN_HOME)))
-    kover(project((Modules.DOMAIN_CATEGORIES)))
     kover(project((Modules.REPOSITORY_USER)))
     kover(project((Modules.REPOSITORY_MOVIE)))
     kover(project((Modules.REPOSITORY_MOVIE)))
