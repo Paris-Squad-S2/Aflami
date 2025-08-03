4741a2a928fbdb9ac6360045dd34dac1fd86ca15
AhmedSalah <ahmedsaaalah21@gmail.com>
2025-08-04 02:00:33 +0300

    add watch history and rated category cards
    This commit adds two category cards to the profile screen: "Watch history" and "Rated".
    It also adds the necessary icons for these cards.

--- feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/screen/ProfileScreen.kt
+++ feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/screen/ProfileScreen.kt
@@ -1,29 +1,17 @@
 package com.feature.profile.profileUi.screen
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.unit.dp
 import com.feature.profile.profileUi.R
 import com.feature.profile.profileUi.screen.component.ProfileDetails
 import com.feature.profile.profileUi.screen.component.ProfileHeader
-import com.paris_2.aflami.designsystem.components.AppIcon
-import com.paris_2.aflami.designsystem.theme.Theme
+
+import com.paris_2.aflami.designsystem.components.CategoryCard
 
 
 @Composable
@@ -36,5 +24,20 @@ fun ProfileContent(modifier: Modifier = Modifier) {
     Column(modifier = modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         ProfileHeader()
         ProfileDetails()
+        Row {
+            CategoryCard(
+                "Watch history",
+                painterResource(R.drawable.ic_clock_3d),
+                onCategoryClick = { },
+
+                )
+            CategoryCard(
+                "Watch history",
+                painterResource(R.drawable.ic_star_3d),
+                onCategoryClick = { },
+
+                )
+
+        }
     }
 }
