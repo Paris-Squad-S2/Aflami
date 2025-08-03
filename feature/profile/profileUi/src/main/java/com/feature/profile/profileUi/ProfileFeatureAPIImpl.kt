d28a801a66812c7a72535ebd4a0577a953d69da0
AhmedSalah <ahmedsaaalah21@gmail.com>
2025-08-04 02:04:52 +0300

    Remove text composable from profile screen

--- feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/ProfileFeatureAPIImpl.kt
+++ feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/ProfileFeatureAPIImpl.kt
@@ -2,7 +2,6 @@ package com.feature.profile.profileUi
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.internal.ComposableFunction0
 import androidx.compose.ui.Alignment
