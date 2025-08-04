2925d0b38e40b871e7a59905d58ea7f594fad8cd
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-08-04 19:42:08 +0300

    refactor build.gradle.kts to consolidate dependencies into bundles

--- designsystem/build.gradle.kts
+++ designsystem/build.gradle.kts
@@ -49,20 +49,9 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(libs.bundles.compose)
+    testImplementation(libs.bundles.test)
+    androidTestImplementation(libs.bundles.test)
     debugImplementation(libs.androidx.ui.tooling)
 
     implementation(libs.accompanist.pager)
