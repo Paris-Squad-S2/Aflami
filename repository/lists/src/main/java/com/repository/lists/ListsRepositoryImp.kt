150652ea91eb59272e3f7240722b82c8763e005f
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-08-05 18:44:38 +0300

    add list management functions to remote data source and repository

--- repository/lists/src/main/java/com/repository/lists/ListsRepositoryImp.kt
+++ repository/lists/src/main/java/com/repository/lists/ListsRepositoryImp.kt
@@ -1,28 +1,52 @@
 package com.repository.lists
 
+import com.paris.domain.lists.entity.ListDetails
 import com.paris.domain.lists.entity.Lists
+import com.paris.domain.lists.entity.Response
+import com.paris.domain.lists.exception.ListsNetworkException
 import com.paris.domain.lists.repository.ListsRepository
 import com.repository.lists.dataSource.remote.ListsRemoteDataSource
+import com.repository.lists.exeptions.NetworkException
 import com.repository.lists.mapper.toDomain
 
 class ListsRepositoryImp(
     private val listRemoteDataSource: ListsRemoteDataSource
 ) : ListsRepository {
-    override suspend fun getLists(page: Int): List<Lists> {
+    override suspend fun getLists(page: Int): List<Lists> = handleListsExceptions {
         val accountId = listRemoteDataSource.getAccountId()
         return listRemoteDataSource.getLists(page = page, accountId = accountId).toDomain()
     }
 
-    /*    private suspend fun <T> safeCall(exception: AflamiException, call: suspend () -> T): T {
-            if (networkConnectionChecker.isConnected.value.not()) {
-                throw NoInternetConnectionException()
-            }
-            return try {
-                call()
-            } catch (e: AflamiException) {
-                throw e
-            } catch (_: Exception) {
-                throw exception
-            }
-        }*/
+    override suspend fun getListDetails(page: Int, listId: String): ListDetails = handleListsExceptions {
+        return listRemoteDataSource.getListDetails(page = page, listId = listId).toDomain()
+    }
+
+    override suspend fun deleteList(listId: String): Response = handleListsExceptions {
+        return listRemoteDataSource.deleteList(listId).toDomain()
+    }
+
+    override suspend fun createList(name: String): Response = handleListsExceptions {
+        return listRemoteDataSource.createList(name).toDomain()
+    }
+
+    override suspend fun addMovieToList(listId: String, movieId: Int): Response = handleListsExceptions {
+        return listRemoteDataSource.addMovieToList(listId , movieId).toDomain()
+    }
+
+    override suspend fun removeMovieFromList(listId: String, movieId: Int): Response = handleListsExceptions {
+        return listRemoteDataSource.removeMovieFromList(listId, movieId).toDomain()
+    }
+
+
+    private inline fun <T> handleListsExceptions(block: () -> T): T {
+        try {
+            return block()
+        } catch (e: NetworkException.ServerException) {
+            throw ListsNetworkException(e.message ?: "Server error")
+        } catch (e: NetworkException.UnknownException) {
+            throw ListsNetworkException(e.message ?: "Unknown authentication error")
+        } catch (e: Exception) {
+            throw ListsNetworkException(e.message ?: "Unknown authentication error")
+        }
+    }
 }
--- repository/lists/src/main/java/com/repository/lists/dataSource/remote/ListsRemoteDataSource.kt
+++ repository/lists/src/main/java/com/repository/lists/dataSource/remote/ListsRemoteDataSource.kt
@@ -1,8 +1,15 @@
 package com.repository.lists.dataSource.remote
 
+import com.repository.lists.model.dto.ListDetailsDto
 import com.repository.lists.model.dto.ListsDto
+import com.repository.lists.model.dto.ResponseDto
 
 interface ListsRemoteDataSource {
     suspend fun getLists(page: Int, accountId: String): ListsDto
     suspend fun getAccountId(): String
+    suspend fun getListDetails(page: Int, listId: String): ListDetailsDto
+    suspend fun deleteList(listId: String): ResponseDto
+    suspend fun createList(name: String): ResponseDto
+    suspend fun addMovieToList(listId: String , movieId: Int): ResponseDto
+    suspend fun removeMovieFromList(listId: String, movieId: Int): ResponseDto
 }
