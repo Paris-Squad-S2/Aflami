2b00c02f7adb00e34dadbbbe9dae4bb691d3caad
dina othman <dinaosman581@gmail.com>
2025-08-05 16:23:19 +0300

    add BaseViewModel

---
+++ feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/common/BaseViewModel.kt
@@ -0,0 +1,53 @@
+package com.feature.profile.profileUi.common
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavOptions
+import com.feature.profile.profileUi.navigation.ProfileDestination
+import com.feature.profile.profileUi.navigation.ProfileNavigator
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+open class BaseViewModel<S>(
+    initialState: S, protected val navigator: ProfileNavigator
+) : ViewModel() {
+
+    private val privateScreenState = MutableStateFlow(initialState)
+    val screenState: StateFlow<S> = privateScreenState.asStateFlow()
+
+    protected fun navigate(destination: ProfileDestination, navOptions: NavOptions? = null) =
+        viewModelScope.launch {
+            navigator.navigate(destination = destination, navOptions = navOptions)
+        }
+
+    protected fun navigateUp() = viewModelScope.launch { navigator.navigateUp() }
+
+    fun updateState(newState: S) {
+        privateScreenState.update { newState }
+    }
+
+    protected fun <T> tryToExecute(
+        onSuccess: (suspend (T) -> Unit)? = null,
+        onError: (String) -> Unit,
+        scope: CoroutineScope = viewModelScope,
+        execute: suspend () -> T,
+    ): Job {
+        val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+            onError(throwable.message ?: "Unexpected error")
+        }
+        return scope.launch(exceptionHandler) {
+            try {
+                val result = execute()
+                onSuccess?.invoke(result)
+            } catch (e: Exception) {
+                onError(e.message ?: "Unexpected error")
+            }
+        }
+    }
+}
