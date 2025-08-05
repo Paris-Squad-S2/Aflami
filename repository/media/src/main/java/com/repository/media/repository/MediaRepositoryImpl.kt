9c1a7dca4de4a9baa49513fe7af14867d7de08cd
dina othman <dinaosman581@gmail.com>
2025-08-05 20:50:45 +0300

    use local in MediaRepositoryImpl

--- repository/media/src/main/java/com/repository/media/repository/MediaRepositoryImpl.kt
+++ repository/media/src/main/java/com/repository/media/repository/MediaRepositoryImpl.kt
@@ -11,68 +11,85 @@ import com.paris_2.domain.media.exception.PopularMediaException
 import com.paris_2.domain.media.exception.TopRatingMediaException
 import com.paris_2.domain.media.exception.UpComingMediaException
 import com.paris_2.domain.media.repository.MediaRepository
+import com.repository.media.datasource.local.ContinueWatchingLocalDataSource
 import com.repository.media.datasource.local.HomeMediaLocalDataSource
 import com.repository.media.datasource.remote.MediaRemoteDataSource
+import com.repository.media.entity.Category
 import com.repository.media.mapper.toDomain
 import com.repository.media.mapper.toEntity
+import com.repository.media.mapper.toMediaEntity
 import com.repository.media.util.NetworkConnectionChecker
 import com.repository.media.util.detectLanguage
 
 class MediaRepositoryImpl(
     private val networkConnectionChecker: NetworkConnectionChecker,
     private val mediaRemoteDataSource: MediaRemoteDataSource,
+    private val continueWatchingLocalDataSource: ContinueWatchingLocalDataSource,
     private val homeMediaLocalDataSource: HomeMediaLocalDataSource
 ) : MediaRepository {
     private val language = detectLanguage()
     override suspend fun getPopularMedia(): List<Media> {
-
         return safeCall(PopularMediaException()) {
-            val popularMovies =
-                mediaRemoteDataSource.getPopularMovies(language = language).results?.mapNotNull {
-                    it.toDomain(MediaType.MOVIE)
-                } ?: emptyList()
+            val localMedia = homeMediaLocalDataSource.getMediaListByCategory(Category.POPULAR)
 
-            val popularTvShows =
-                mediaRemoteDataSource.getPopularTvShows(language = language).results?.mapNotNull {
-                    it.toDomain(MediaType.TVSHOW)
-                } ?: emptyList()
 
-            val combined = (popularMovies + popularTvShows)
-                .sortedByDescending { it.rating }
+            if (localMedia.isNotEmpty()) return@safeCall localMedia.mapNotNull { it.toDomain() }
 
+            val remoteMovies = mediaRemoteDataSource.getPopularMovies(language).results?.mapNotNull {
+                it.toDomain(MediaType.MOVIE)
+            } ?: emptyList()
+
+            val remoteTvShows = mediaRemoteDataSource.getPopularTvShows(language).results?.mapNotNull {
+                it.toDomain(MediaType.TVSHOW)
+            } ?: emptyList()
+
+            val combined = (remoteMovies + remoteTvShows).sortedByDescending { it.rating }
+
+            val entities = combined.map {
+                it.toMediaEntity(category = Category.POPULAR)
+            }
+            homeMediaLocalDataSource.addMediaList(entities)
             combined
         }
-
     }
-
     override suspend fun getTopRatingMedia(): List<Media> {
         return safeCall(TopRatingMediaException()) {
-            val topMovies =
-                mediaRemoteDataSource.getTopRatedMovies(language = language).results?.mapNotNull {
-                    it.toDomain(MediaType.MOVIE)
-                } ?: emptyList()
+            val localMedia = homeMediaLocalDataSource.getMediaListByCategory(Category.TOP_RATED)
 
-            val topTv =
-                mediaRemoteDataSource.getTopRatedTvShows(language = language).results?.mapNotNull {
-                    it.toDomain(MediaType.TVSHOW)
-                } ?: emptyList()
+            if (localMedia.isNotEmpty()) return@safeCall localMedia.mapNotNull { it.toDomain() }
 
-            val combined = (topMovies + topTv)
-                .sortedByDescending { it.rating }
+            val remoteMovies = mediaRemoteDataSource.getTopRatedMovies(language).results?.mapNotNull {
+                it.toDomain(MediaType.MOVIE)
+            } ?: emptyList()
 
+            val remoteTv = mediaRemoteDataSource.getTopRatedTvShows(language).results?.mapNotNull {
+                it.toDomain(MediaType.TVSHOW)
+            } ?: emptyList()
+
+            val combined = (remoteMovies + remoteTv).sortedByDescending { it.rating }
+
+            val entities = combined.map {
+                it.toMediaEntity(category = Category.TOP_RATED)
+            }
+            homeMediaLocalDataSource.addMediaList(entities)
             combined
         }
     }
 
     override suspend fun getUpComingMedia(): List<Media> {
         return safeCall(UpComingMediaException()) {
-            val upcomingMovies =
-                mediaRemoteDataSource.getUpcomingMovies(language = language).results?.mapNotNull {
+            val localMedia = homeMediaLocalDataSource.getMediaListByCategory(Category.UPCOMING)
+            if (localMedia.isNotEmpty()) return@safeCall localMedia.mapNotNull { it.toDomain() }
+            val upcomingMovies = mediaRemoteDataSource.getUpcomingMovies(language = language)
+                .results?.mapNotNull {
                     it.toDomain(MediaType.MOVIE)
                 } ?: emptyList()
+            val entities = upcomingMovies.map {
+                it.toMediaEntity(category = Category.UPCOMING)
+            }
+            homeMediaLocalDataSource.addMediaList(entities)
             upcomingMovies
         }
-
     }
 
     override suspend fun getNowPlayingMedia(): List<Media> {
@@ -86,13 +103,13 @@ class MediaRepositoryImpl(
 
     override suspend fun addMediaToContinueWatching(media: Media) {
         return safeCall(AddMediaToContinueWatchingException()){
-            homeMediaLocalDataSource.addMedia(media.toEntity())
+            continueWatchingLocalDataSource.addMedia(media.toEntity())
         }
     }
 
     override suspend fun getMediaFromLocal(): List<Media> {
         return safeCall(GetContinueWatchingMediaException()) {
-            homeMediaLocalDataSource.getAllMedia().map { it.toDomain() }
+            continueWatchingLocalDataSource.getAllMedia().map { it.toDomain() }
         }
     }
 
