8190a08d4019dd2628545c323c68256db127acc3
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-08-05 18:33:39 +0300

    add data transfer objects for list and media details

---
+++ repository/lists/src/main/java/com/repository/lists/model/dto/ResponseDto.kt
@@ -0,0 +1,13 @@
+package com.repository.lists.model.dto
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseDto(
+    @SerialName("status_code")
+    val statusCode: Int? = null,
+    @SerialName("status_message")
+    val statusMessage: String? = null
+)
