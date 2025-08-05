74f55913eed2a7413b5ed945e0f7b6b6625b25b4
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-08-05 18:36:59 +0300

    add dialogs for creating and deleting lists in the UI

---
+++ feature/lists/listsUi/src/main/java/com/feature/lists/listsUi/screens/listDetails/components/DeletelistDialog.kt
@@ -0,0 +1,77 @@
+package com.feature.lists.listsUi.screens.listDetails.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.feature.lists.listsUi.R
+import com.paris_2.aflami.designsystem.components.AppDialog
+import com.paris_2.aflami.designsystem.components.AppText
+import com.paris_2.aflami.designsystem.components.ButtonState
+import com.paris_2.aflami.designsystem.components.ButtonType
+import com.paris_2.aflami.designsystem.components.CustomButton
+import com.paris_2.aflami.designsystem.theme.Theme
+
+@Composable
+fun DeleteListDialog(
+    onDismiss: () -> Unit,
+    onDeleteClicked: () -> Unit,
+    showDialog: Boolean
+) {
+    if (!showDialog) return
+    AppDialog(
+        onDismiss = onDismiss,
+        title = R.string.create_new_list,
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+                .padding(top = 24.dp),
+        ) {
+            Image(
+                painter = painterResource(R.drawable.img_warning),
+                contentDescription = null,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = 12.dp),
+                )
+
+            AppText(
+                text = stringResource(R.string.confirm_delete),
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 24.dp),
+                style = Theme.textStyle.title.small
+            )
+            CustomButton(
+                text = R.string.delete,
+                onClick = onDeleteClicked,
+                type = ButtonType.Primary,
+                isNegative = true,
+                state = ButtonState.Normal,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 12.dp)
+                    .padding(horizontal = 16.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewLoginDialog() {
+    DeleteListDialog(
+        onDismiss = {},
+        onDeleteClicked = {},
+        showDialog = true,
+    )
+}
