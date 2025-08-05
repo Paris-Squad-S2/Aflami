4d031b311e395e7bd83adee896044c072adadfc3
AhmedSalah <ahmedsaaalah21@gmail.com>
2025-08-05 06:39:13 +0300

    Add the profile setup item

--- feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/screen/component/ProfileSetUp.kt
+++ feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/screen/component/ProfileSetUp.kt
@@ -1,9 +1,13 @@
 package com.feature.profile.profileUi.screen.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -48,9 +52,25 @@ fun SetUpItem(modifier: Modifier = Modifier, icon: Int, title: String, onClick: 
             .padding(bottom = 8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        AppIcon(
-            imageVector = ImageVector.vectorResource(icon), contentDescription = null
-        )
+        Box(
+            modifier = Modifier
+                .border(
+                    width = 1.dp, color = Theme.colors.stroke,
+                    shape = RoundedCornerShape(12.dp)
+                )
+                .background(
+                    color = Theme.colors.surfaceHigh,
+                    shape = RoundedCornerShape(12.dp)
+                )
+
+                .padding(8.dp)
+        ) {
+            AppIcon(
+                imageVector = ImageVector.vectorResource(icon),
+                contentDescription = null,
+                tint = Theme.colors.text.body,
+            )
+        }
         AppText(
             text = title,
             style = Theme.textStyle.label.large,
@@ -63,12 +83,15 @@ fun SetUpItem(modifier: Modifier = Modifier, icon: Int, title: String, onClick: 
             text = "ENG",
             style = Theme.textStyle.label.small,
             color = Theme.colors.text.body,
+            modifier = Modifier.padding(end = 8.dp)
         )
+
         AppIcon(
             imageVector = ImageVector.vectorResource(R.drawable.ic_arrow_left),
             contentDescription = null,
-            tint = Theme.colors.text.body
+            tint = Theme.colors.text.hint,
         )
+
 
     }
 }
