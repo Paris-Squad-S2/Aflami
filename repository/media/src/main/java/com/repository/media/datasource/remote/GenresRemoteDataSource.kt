2130b6188ef6e18cd7c2ee9a7f8646aa01efb73e
dina othman <dinaosman581@gmail.com>
2025-08-05 13:06:34 +0300

    update GenresRemoteDataSource

--- repository/media/src/main/java/com/repository/media/datasource/remote/GenresRemoteDataSource.kt
+++ repository/media/src/main/java/com/repository/media/datasource/remote/GenresRemoteDataSource.kt
@@ -4,4 +4,5 @@ import com.repository.media.dto.GenresDto
 
 interface GenresRemoteDataSource {
     suspend fun getMoviesGenres(language: String): GenresDto
+    suspend fun getAllGenres(language: String): GenresDto
 }
