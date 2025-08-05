95d23df030c982c0ce20f19c67081300d3f63a38
AhmedSalah <ahmedsaaalah21@gmail.com>
2025-08-05 06:19:50 +0300

    update profile screen
    - Add `ProfileSetUp` composable
    - Add icons for profile setup section

--- feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/screen/ProfileScreen.kt
+++ feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/screen/ProfileScreen.kt
@@ -2,16 +2,25 @@ package com.feature.profile.profileUi.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.feature.profile.profileUi.R
 import com.feature.profile.profileUi.screen.component.ProfileDetails
 import com.feature.profile.profileUi.screen.component.ProfileHeader
+import com.feature.profile.profileUi.screen.component.ProfileSetUp
 
 import com.paris_2.aflami.designsystem.components.CategoryCard
+import com.paris_2.aflami.designsystem.theme.Theme
 
 
 @Composable
@@ -24,20 +33,31 @@ fun ProfileContent(modifier: Modifier = Modifier) {
     Column(modifier = modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         ProfileHeader()
         ProfileDetails()
-        Row {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .padding(top = 24.dp)
+        ) {
             CategoryCard(
                 "Watch history",
                 painterResource(R.drawable.ic_clock_3d),
                 onCategoryClick = { },
-
-                )
+                modifier = Modifier.weight(1F)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
             CategoryCard(
-                "Watch history",
+                "My rating",
                 painterResource(R.drawable.ic_star_3d),
                 onCategoryClick = { },
-
-                )
-
+                modifier = Modifier.weight(1F)
+            )
         }
+        HorizontalDivider(
+            thickness = 1.dp,
+            color = Theme.colors.stroke,
+            modifier = Modifier.padding(vertical = 24.dp)
+        )
+        ProfileSetUp()
     }
 }
---
+++ feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/screen/component/ProfileSetUp.kt
@@ -0,0 +1,74 @@
+package com.feature.profile.profileUi.screen.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import com.feature.profile.profileUi.R
+import com.paris_2.aflami.designsystem.components.AppIcon
+import com.paris_2.aflami.designsystem.components.AppText
+import com.paris_2.aflami.designsystem.theme.Theme
+
+@Composable
+fun ProfileSetUp(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+    ) {
+        SetUpItem(
+            onClick = {},
+            title = "Language",
+            icon = R.drawable.ic_translate
+        )
+        SetUpItem(
+            onClick = {},
+            title = "App Theme",
+            icon = R.drawable.ic_moon
+        )
+        SetUpItem(
+            onClick = {},
+            title = "Setting",
+            icon = R.drawable.ic_customize
+        )
+    }
+}
+
+@Composable
+fun SetUpItem(modifier: Modifier = Modifier, icon: Int, title: String, onClick: () -> Unit) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(bottom = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        AppIcon(
+            imageVector = ImageVector.vectorResource(icon), contentDescription = null
+        )
+        AppText(
+            text = title,
+            style = Theme.textStyle.label.large,
+            color = Theme.colors.text.title,
+            modifier = Modifier
+                .padding(start = 12.dp)
+                .weight(1F)
+        )
+        AppText(
+            text = "ENG",
+            style = Theme.textStyle.label.small,
+            color = Theme.colors.text.body,
+        )
+        AppIcon(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_arrow_left),
+            contentDescription = null,
+            tint = Theme.colors.text.body
+        )
+
+    }
+}
