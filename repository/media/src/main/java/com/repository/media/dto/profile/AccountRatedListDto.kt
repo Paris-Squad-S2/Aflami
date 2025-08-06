cfeda0107987fe6f346a4883c6d3b07fa4cac0b9
dina othman <dinaosman581@gmail.com>
2025-08-06 14:58:42 +0300

    update AccountRatedListDto

--- repository/media/src/main/java/com/repository/media/dto/profile/AccountRatedListDto.kt
+++ repository/media/src/main/java/com/repository/media/dto/profile/AccountRatedListDto.kt
@@ -1,8 +1,16 @@
 package com.repository.media.dto.profile
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class AccountRatedListDto(
+    @SerialName("page")
     val page: Int = 0,
+    @SerialName("results")
     val results: List<RatedMediaDto>,
+    @SerialName("total_pages")
     val total_pages: Int = 0,
+    @SerialName("total_results")
     val total_results: Int = 0
 )
