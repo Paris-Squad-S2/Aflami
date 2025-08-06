176c41871ca13df7eaf3dd1bd1821a71b32d1421
dina othman <dinaosman581@gmail.com>
2025-08-06 15:19:31 +0300

    update RatedTvShowDto

--- repository/media/src/main/java/com/repository/media/dto/profile/RatedTvShowDto.kt
+++ repository/media/src/main/java/com/repository/media/dto/profile/RatedTvShowDto.kt
@@ -4,13 +4,13 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class AccountRatedListDto(
+data class RatedTvShowDto(
     @SerialName("page")
-    val page: Int = 0,
+    val page: Int,
     @SerialName("results")
-    val results: List<RatedMediaDto>,
+    val results: List<TvShowResult>,
     @SerialName("total_pages")
-    val total_pages: Int = 0,
+    val total_pages: Int,
     @SerialName("total_results")
-    val total_results: Int = 0
+    val total_results: Int
 )
