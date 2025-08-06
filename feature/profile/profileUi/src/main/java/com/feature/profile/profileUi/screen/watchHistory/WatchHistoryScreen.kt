76f75683c447cd4b1e8c082ff37361dc8dc31074
dina othman <dinaosman581@gmail.com>
2025-08-06 10:59:45 +0300

    update WatchHistoryScreen

--- feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/screen/watchHistory/WatchHistoryScreen.kt
+++ feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/screen/watchHistory/WatchHistoryScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.feature.profile.profileUi.screen.components.MediaCard
@@ -114,11 +113,4 @@ fun WatchHistoryScreenContent(
             )
         }
     }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun TopRatingScreenContentPreview() {
-    WatchHistoryScreen()
 }
