e71653dc88f495a503235cb289c007e6750c4b58
dina othman <dinaosman581@gmail.com>
2025-08-07 12:57:45 +0300

    update TvResult

--- repository/media/src/main/java/com/repository/media/dto/profile/RatedTvShowDtoo.kt
+++ repository/media/src/main/java/com/repository/media/dto/profile/RatedTvShowDtoo.kt
@@ -1,8 +1,16 @@
 package com.repository.media.dto.profile
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class RatedTvShowDtoo(
+    @SerialName("page")
     val page: Int,
+    @SerialName("results")
     val results: List<TvShowResult>,
+    @SerialName("total_pages")
     val total_pages: Int,
+    @SerialName("total_results")
     val total_results: Int
 )
