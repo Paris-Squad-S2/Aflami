c991dd9bd59fe5b2fa8c98da21faeff2f596536b
dina othman <dinaosman581@gmail.com>
2025-08-07 11:11:57 +0300

    update dependencies

--- feature/profile/profileUi/build.gradle.kts
+++ feature/profile/profileUi/build.gradle.kts
@@ -80,22 +80,14 @@ dependencies {
     ksp(libs.hilt.android.compiler)
 
     //test
-    testImplementation(libs.junit)
-    testImplementation(libs.mockk)
     testImplementation(libs.junit.jupiter.api)
     testRuntimeOnly(libs.junit.jupiter.engine)
-    testImplementation(libs.kotlinx.coroutines.test)
-    testImplementation(libs.truth)
-    androidTestImplementation(libs.androidx.junit)
-
-    // Junit 5
-    testImplementation(libs.junit.jupiter.api)
-    testImplementation(libs.junit.jupiter.engine)
-    testImplementation(libs.junit.jupiter.params)
-    testImplementation(kotlin("test"))
 
     testImplementation(libs.mockk)
     testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.truth)
+    testImplementation(kotlin("test"))
+
 
     //kotlinx serialization
     implementation(libs.kotlinx.serialization.json)
