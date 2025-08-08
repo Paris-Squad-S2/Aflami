7724dd5773286cc705d4bb3ec8616d3eacfb5313
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-08-08 19:40:51 +0300

    refactor build.gradle.kts and libs.versions.toml to consolidate Retrofit dependencies into a bundle

--- datasource/remote/tvShow/build.gradle.kts
+++ datasource/remote/tvShow/build.gradle.kts
@@ -51,10 +51,7 @@ dependencies {
 
 
     //retrofit
-    implementation(libs.retrofit)
-    implementation(libs.retrofit.converter)
-    implementation(libs.logging.interceptor)
-    implementation(libs.okhttp)
+    implementation(libs.bundles.retrofit)
 
     //Hilt
     implementation(libs.hilt.android)
