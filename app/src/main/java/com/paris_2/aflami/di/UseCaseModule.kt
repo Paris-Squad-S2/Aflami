f98166bbebf409428c67caf6587b0804648e50ca
AhmedSalah <ahmedsaaalah21@gmail.com>
2025-08-08 19:37:24 +0300

    Refactor: Save username upon login and add delete session functionality
    This commit introduces two main changes to the user authentication flow:
    
    1.  **Save Username:** The `UserRepositoryImpl` now saves the username to local data source upon successful login.
    2.  **Delete Session ID:** A new `deleteSessionId` method has been added to the `UserRepository` interface and its implementation `UserRepositoryImpl` to allow for the deletion of the locally stored session ID.
    
    These changes enhance user session management.

--- app/src/main/java/com/paris_2/aflami/di/UseCaseModule.kt
+++ app/src/main/java/com/paris_2/aflami/di/UseCaseModule.kt
@@ -32,10 +32,10 @@ import com.paris_2.domain.media.useCase.IncrementCategoryInteractionUseCase
 import com.paris_2.domain.media.useCase.SearchByQueryUseCase
 import com.paris_2.domain.media.useCase.SortingMediaByCategoriesInteractionUseCase
 import com.paris_2.domain.media.useCase.movie.AddRatingToMovieUseCase
+import com.paris_2.domain.media.useCase.movie.DeleteMovieRatingUseCase
 import com.paris_2.domain.media.useCase.movie.GetMovieCastUseCase
 import com.paris_2.domain.media.useCase.movie.GetMovieDetailsUseCase
 import com.paris_2.domain.media.useCase.movie.GetMovieGalleryUseCase
-import com.paris_2.domain.media.useCase.movie.DeleteMovieRatingUseCase
 import com.paris_2.domain.media.useCase.movie.GetMovieRecommendationsUseCase
 import com.paris_2.domain.media.useCase.movie.GetMovieReviewsUseCase
 import com.paris_2.domain.media.useCase.movie.GetMovieVideoUseCase
@@ -53,8 +53,9 @@ import com.paris_2.domain.media.useCase.tvShows.GetTvShowVideoUseCase
 import com.paris_2.domain.media.useCase.tvShows.GetTvShowsProductionCompaniesUseCase
 import com.paris_2.domain.user.repository.SettingRepository
 import com.paris_2.domain.user.repository.UserRepository
-import com.paris_2.domain.user.usecase.GetAccountIdUseCase
 import com.paris_2.domain.user.usecase.CompleteOnboardingUseCase
+import com.paris_2.domain.user.usecase.DeleteSessionIdUseCase
+import com.paris_2.domain.user.usecase.GetAccountIdUseCase
 import com.paris_2.domain.user.usecase.GetForgetPasswordUrlUseCase
 import com.paris_2.domain.user.usecase.GetRegisterUrlUseCase
 import com.paris_2.domain.user.usecase.GetSessionIdUseCase
@@ -235,6 +236,10 @@ object UseCaseModule {
         GetAccountIdUseCase(userRepository)
 
     @Provides
+    fun provideDeleteSessionIdUseCase(userRepository: UserRepository) =
+        DeleteSessionIdUseCase(userRepository)
+
+    @Provides
     fun provideGuestLoginUseCase(userRepository: UserRepository) =
         GuestLoginUseCase(userRepository)
 
@@ -287,4 +292,3 @@ object UseCaseModule {
         SettingsUseCase(settingRepository)
 
 }
-
