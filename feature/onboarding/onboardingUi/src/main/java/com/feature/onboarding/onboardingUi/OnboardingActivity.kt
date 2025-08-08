68ba9025993712ce522c931ce716fe9c84bdc6b1
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-08-08 21:56:01 +0300

    refactor: enhance onboarding activity to support edge-to-edge display and status bar customization

--- feature/onboarding/onboardingUi/src/main/java/com/feature/onboarding/onboardingUi/OnboardingActivity.kt
+++ feature/onboarding/onboardingUi/src/main/java/com/feature/onboarding/onboardingUi/OnboardingActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowInsetsControllerCompat
 import com.feature.onboarding.onboardingUi.ui.OnboardingScreen
 import com.paris_2.aflami.designsystem.theme.AflamiTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,6 +21,20 @@ class OnboardingActivity : ComponentActivity() {
 
         setContent {
             AflamiTheme {
+
+                val context = LocalContext.current
+                val view = LocalView.current
+                val activity = context as? ComponentActivity
+
+                LaunchedEffect(Unit) {
+                    activity?.window?.also { window ->
+                        WindowInsetsControllerCompat(window, view).apply {
+                            isAppearanceLightStatusBars = false
+                            isAppearanceLightNavigationBars = false
+                        }
+                    }
+                }
+
                 OnboardingScreen()
             }
         }
