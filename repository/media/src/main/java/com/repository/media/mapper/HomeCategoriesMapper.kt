6a07d05914b3d1ff6054f4ae442934b5c6715c01
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-09 15:29:30 +0300

    Refactor: Update HomeCategoriesMapper to use Genre enum
    This commit refactors the `HomeCategoriesMapper.kt` to replace the `Category` entity with the `Genre` enum.
    
    The following changes were made:
    - Renamed `toCategory()` to `toGenre()` and updated its logic to find the corresponding `Genre` enum entry based on the display name. If no match is found, it defaults to `Genre.Unknown`.
    - Renamed `toCategoryList()` to `toGenreList()` in both the `List<GenreDto>` extension and the `GenresDto` extension to reflect the change to using the `Genre` enum.

--- repository/media/src/main/java/com/repository/media/mapper/HomeCategoriesMapper.kt
+++ repository/media/src/main/java/com/repository/media/mapper/HomeCategoriesMapper.kt
@@ -1,21 +1,18 @@
 package com.repository.media.mapper
 
-import com.paris_2.domain.media.entity.Category
+import com.paris_2.domain.media.entity.Genre
 import com.repository.media.dto.GenreDto
 import com.repository.media.dto.GenresDto
 
 
-fun GenreDto.toCategory(): Category {
-    return Category(
-        id = this.id ?: 0,
-        name = this.name.orEmpty()
-    )
+fun GenreDto.toGenre(): Genre {
+    return Genre.entries.find { it.displayName == this.name } ?: Genre.Unknown
 }
 
-fun List<GenreDto>.toCategoryList(): List<Category> {
-    return this.map { it.toCategory() }
+fun List<GenreDto>.toGenreList(): List<Genre> {
+    return this.map { it.toGenre() }
 }
 
-fun GenresDto.toCategoryList(): List<Category> {
-    return this.genreDto?.toCategoryList() ?: emptyList()
+fun GenresDto.toGenreList(): List<Genre> {
+    return this.genreDto?.toGenreList() ?: emptyList()
 }
