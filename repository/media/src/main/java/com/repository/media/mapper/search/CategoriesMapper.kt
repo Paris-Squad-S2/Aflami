ee249ebb0ff0e4b35aff826aa1d0fa3719031592
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-09 15:24:19 +0300

    Refactor: Enhance genre mapping and add utility functions for ID conversion
    This commit introduces a more robust genre mapping system.
    - A `genreIdMap` is added to map genre IDs to `Genre` enums.
    - A reverse map `genreToIdMap` and a helper function `genreToId` are created for converting `Genre` enums back to IDs.
    - The `genreFromId` function now handles unknown IDs gracefully by returning `Genre.Unknown`.
    - `toCategories` and `toCategoryModel` are replaced by `toGenres` and `toGenresList` for clearer naming and to reflect the use of the `Genre` enum.
    - The mapping functions now leverage the new maps and helper functions for ID-to-enum conversion and vice-versa.

--- repository/media/src/main/java/com/repository/media/mapper/search/CategoriesMapper.kt
+++ repository/media/src/main/java/com/repository/media/mapper/search/CategoriesMapper.kt
@@ -1,16 +1,52 @@
 package com.repository.media.mapper.search
 
-import com.paris_2.domain.media.entity.Category
+import com.paris_2.domain.media.entity.Genre
 import com.repository.media.dto.GenreDto
+import com.repository.media.dto.GenresDto
 
+val genreIdMap = mapOf(
+    28 to Genre.Action,
+    12 to Genre.Adventure,
+    16 to Genre.Animation,
+    35 to Genre.Comedy,
+    80 to Genre.Crime,
+    99 to Genre.Documentary,
+    18 to Genre.Drama,
+    10751 to Genre.Family,
+    14 to Genre.Fantasy,
+    36 to Genre.History,
+    27 to Genre.Horror,
+    10402 to Genre.Music,
+    9648 to Genre.Mystery,
+    10749 to Genre.Romance,
+    878 to Genre.ScienceFiction,
+    10770 to Genre.TVMovie,
+    53 to Genre.Thriller,
+    10752 to Genre.War,
+    37 to Genre.Western,
+    10759 to Genre.ActionAdventure,
+    10762 to Genre.Kids,
+    10763 to Genre.News,
+    10764 to Genre.Reality,
+    10765 to Genre.SciFiFantasy,
+    10766 to Genre.Soap,
+    10767 to Genre.Talk,
+    10768 to Genre.WarPolitics
+)
 
-fun List<GenreDto>.toCategories(): List<Category> {
-    return this.map { it.toCategoryModel() }
+private val genreToIdMap = genreIdMap.entries.associate { (id, genre) ->
+    genre to id
+}
+fun genreToId(genre: Genre): Int = genreToIdMap[genre] ?: 0
+
+fun genreFromId(id: Int): Genre {
+    return genreIdMap[id] ?: Genre.Unknown
 }
 
-fun GenreDto.toCategoryModel(): Category {
-    return Category(
-        id = this.id ?: 0,
-        name = this.name.orEmpty()
-    )
+fun List<GenreDto>.toGenres(): List<Genre> {
+    return this.mapNotNull { it.id?.let(::genreFromId) }
+}
+
+fun GenresDto.toGenresList(): List<Genre> {
+    return this.genreDto?.toGenres() ?: emptyList()
 }
