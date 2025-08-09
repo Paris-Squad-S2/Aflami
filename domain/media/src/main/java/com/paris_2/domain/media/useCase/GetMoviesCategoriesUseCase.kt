02c8e3692bd157f5bf2d4bc7ca23cc9c0e8b159b
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-09 15:29:17 +0300

    Refactor: Rename Category to Genre in GetMoviesCategoriesUseCase
    The `Category` data class has been renamed to `Genre` for clarity and consistency within the `GetMoviesCategoriesUseCase`. This change is reflected in the use case's return type and its interaction with the `MoviesCategoriesRepository`.

--- domain/media/src/main/java/com/paris_2/domain/media/useCase/GetMoviesCategoriesUseCase.kt
+++ domain/media/src/main/java/com/paris_2/domain/media/useCase/GetMoviesCategoriesUseCase.kt
@@ -1,12 +1,12 @@
 package com.paris_2.domain.media.useCase
 
-import com.paris_2.domain.media.entity.Category
+import com.paris_2.domain.media.entity.Genre
 import com.paris_2.domain.media.repository.MoviesCategoriesRepository
 
 class GetMoviesCategoriesUseCase(
     private val categoriesRepository: MoviesCategoriesRepository
 ) {
-    suspend operator fun invoke(): List<Category> {
+    suspend operator fun invoke(): List<Genre> {
         return categoriesRepository.getMoviesCategories()
     }
 }
