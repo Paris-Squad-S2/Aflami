8265b27fe2978fc75735eeb7f8d4b18b22159d01
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-09 15:24:33 +0300

    Refactor: Update CategoryInteractionMapper to use genre names
    The `CategoryInteractionMapper` now maps between `GenreUserInteractionEntity` and `GenreUserInteraction` using genre names instead of genre IDs.
    
    This change involves:
    - Modifying `toCategoryUserInteractionModel` to use `genreFromId()` for conversion.
    - Modifying `toCategoryUserInteractionEntity` to use `genreToId()` for conversion.

--- repository/media/src/main/java/com/repository/media/mapper/search/CategoryInteractionMapper.kt
+++ repository/media/src/main/java/com/repository/media/mapper/search/CategoryInteractionMapper.kt
@@ -5,14 +5,14 @@ import com.repository.media.entity.GenreUserInteractionEntity
 
 fun GenreUserInteractionEntity.toCategoryUserInteractionModel(): GenreUserInteraction {
     return GenreUserInteraction(
-        genreId = this.genreId,
+        genre = genreFromId(genreId),
         interactionCount = this.interactionCount
     )
 }
 
 fun GenreUserInteraction.toCategoryUserInteractionEntity(): GenreUserInteractionEntity {
     return GenreUserInteractionEntity(
-        genreId = this.genreId,
+        genreId = genreToId(genre),
         interactionCount = this.interactionCount
     )
 }
