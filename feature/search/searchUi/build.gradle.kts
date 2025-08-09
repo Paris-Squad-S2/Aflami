d6c9eb69bd476e4f606de72fab397729f8c74a85
a7med naser <ad0112727@gmail.com>
2025-08-09 06:43:23 +0300

    handle app theme in search screen

--- feature/search/searchUi/build.gradle.kts
+++ feature/search/searchUi/build.gradle.kts
@@ -111,6 +111,7 @@ dependencies {
     testImplementation(libs.kotlinx.coroutines.test)
 
     implementation(project(Modules.DOMAIN_MEDIA))
+    implementation(project(Modules.DOMAIN_USER))
     implementation(project(Modules.DESIGN_SYSTEM))
     implementation(project(Modules.SAFE_IMAGE_VIEWER))
 
