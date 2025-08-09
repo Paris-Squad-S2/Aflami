383738bd6871b0a0ac00fe5f03427d18f7e04110
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-09 15:27:10 +0300

    Feat: Add genre resource mapper
    This commit introduces a new `GenreResourceMapper` object in the `searchUi` module. This mapper provides a way to get a drawable resource ID for a given `Genre` enum value.
    
    The `GenreResourceMapper` contains a map of `Genre` enum values to their corresponding drawable resource IDs. It also includes a `getResourceId` function that takes a `Genre` as input and returns the associated resource ID. If a genre is not found in the map, it defaults to `R.drawable.ic_category_all`.

---
+++ feature/search/searchUi/src/main/java/com/feature/search/searchUi/mapper/GenreResourceMapper.kt
@@ -0,0 +1,40 @@
+package com.feature.search.searchUi.mapper
+
+import com.feature.search.searchUi.R
+import com.paris_2.domain.media.entity.Genre
+
+object GenreResourceMapper {
+    private val genreToResourceMap = mapOf(
+        Genre.Action to R.drawable.ic_category_action,
+        Genre.Adventure to R.drawable.ic_category_adventure,
+        Genre.Animation to R.drawable.ic_category_animation,
+        Genre.Comedy to R.drawable.ic_category_comedy,
+        Genre.Crime to R.drawable.ic_category_crime,
+        Genre.Documentary to R.drawable.ic_category_documentary,
+        Genre.Drama to R.drawable.ic_category_drama,
+        Genre.Family to R.drawable.ic_category_family,
+        Genre.Fantasy to R.drawable.ic_category_fantasy,
+        Genre.History to R.drawable.ic_category_history,
+        Genre.Horror to R.drawable.ic_category_horror,
+        Genre.Music to R.drawable.ic_category_music,
+        Genre.Mystery to R.drawable.ic_category_mystery,
+        Genre.Romance to R.drawable.ic_category_romance,
+        Genre.ScienceFiction to R.drawable.ic_category_science_fiction,
+        Genre.TVMovie to R.drawable.ic_category_tv_movie,
+        Genre.Thriller to R.drawable.ic_category_thriller,
+        Genre.War to R.drawable.ic_category_war,
+        Genre.Western to R.drawable.ic_category_western,
+        Genre.ActionAdventure to R.drawable.ic_category_action_and_adventure,
+        Genre.Kids to R.drawable.ic_category_kids,
+        Genre.News to R.drawable.ic_category_news,
+        Genre.Reality to R.drawable.ic_category_reality,
+        Genre.SciFiFantasy to R.drawable.ic_category_fantasy,
+        Genre.Soap to R.drawable.ic_category_soap,
+        Genre.Talk to R.drawable.ic_category_talk,
+        Genre.WarPolitics to R.drawable.ic_category_war
+    )
+
+    fun getResourceId(genre: Genre): Int {
+        return genreToResourceMap[genre] ?: R.drawable.ic_category_all
+    }
+}
