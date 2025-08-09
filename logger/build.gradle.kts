6cdba712d3c8d71cc9049486e81e05add481cab7
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-08-09 05:13:53 +0300

    Refactor: Consolidate Firebase dependencies and simplify implementation in build.gradle.kts

--- logger/build.gradle.kts
+++ logger/build.gradle.kts
@@ -34,17 +34,8 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
+    implementation(libs.bundles.android)
+    testImplementation(libs.bundles.test)
     implementation(platform(libs.firebase.bom))
-
-    implementation(libs.firebase.analytics)
-    implementation(libs.firebase.perf)
-    implementation(libs.firebase.crashlytics)
+    implementation(libs.bundles.firebase)
 }
