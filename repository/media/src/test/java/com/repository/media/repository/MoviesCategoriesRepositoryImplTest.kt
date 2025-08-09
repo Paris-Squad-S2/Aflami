8e1a7c5a5b3517cf1901a316bc5508e976b1e9ff
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-08-09 15:45:43 +0300

    Refactor: Replace genre IDs with Category enum constants in tests

--- repository/media/src/test/java/com/repository/media/repository/MoviesCategoriesRepositoryImplTest.kt
+++ repository/media/src/test/java/com/repository/media/repository/MoviesCategoriesRepositoryImplTest.kt
@@ -43,7 +43,7 @@ class MoviesCategoriesRepositoryImplTest {
                 GenreDto(id = 18, name = "Drama")
             )
         )
-        val categories = listOf(Category(28, "Action"), Category(18, "Drama"))
+        val categories = listOf(Category.ACTION, Category.DRAMA)
         coEvery { genresRemoteDataSource.getMoviesGenres("en-US") } returns genresDto
         coEvery { settingLocalDataSource.getLanguage() } returns MutableStateFlow("en")
 
