0c9ae8b5bae9f38ca9858aa3346f803cf3de3c99
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-09 15:29:04 +0300

    Refactor: Rename Category to Genre in GetAllCategoriesUseCase
    This commit renames the `Category` entity to `Genre` within the `GetAllCategoriesUseCase` and updates its return type accordingly.

--- domain/media/src/main/java/com/paris_2/domain/media/useCase/GetAllCategoriesUseCase.kt
+++ domain/media/src/main/java/com/paris_2/domain/media/useCase/GetAllCategoriesUseCase.kt
@@ -1,12 +1,12 @@
 package com.paris_2.domain.media.useCase
 
-import com.paris_2.domain.media.entity.Category
+import com.paris_2.domain.media.entity.Genre
 import com.paris_2.domain.media.repository.CategoriesRepository
 
 class GetAllCategoriesUseCase(
     private val categoriesRepository: CategoriesRepository,
 ) {
-    suspend operator fun invoke(): List<Category> {
+    suspend operator fun invoke(): List<Genre> {
         return categoriesRepository.getAllCategories()
     }
 }
