87a94ace43b2efbe0f80815ec42809801449e0f7
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-08-09 05:46:27 +0300

    Refactor: Remove unused test dependencies from build.gradle.kts

--- safeimageviewer/build.gradle.kts
+++ safeimageviewer/build.gradle.kts
@@ -50,17 +50,12 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-
     // Compose dependencies
     implementation(libs.bundles.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.bundles.coil)
 
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.material3) //TODO: Remove it
     implementation(libs.androidx.lifecycle.viewmodel.compose)
 
     // TensorFlow Lite - core only (minimal dependencies)
