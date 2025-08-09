27ef5906b2ed99fe58f2c84021737dd945e7d03e
a7med naser <ad0112727@gmail.com>
2025-08-09 06:42:51 +0300

    handle app theme in on boarding screens

--- feature/onboarding/onboardingUi/src/main/java/com/feature/onboarding/onboardingUi/OnboardingActivity.kt
+++ feature/onboarding/onboardingUi/src/main/java/com/feature/onboarding/onboardingUi/OnboardingActivity.kt
@@ -6,17 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.feature.onboarding.onboardingUi.ui.OnboardingScreen
 import com.paris_2.aflami.designsystem.theme.AflamiTheme
+import com.paris_2.domain.user.usecase.SettingsUseCase
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class OnboardingActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var settingsUseCase: SettingsUseCase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         setContent {
-            AflamiTheme {
+            AflamiTheme(settingsUseCase.isDarkTheme()) {
                 OnboardingScreen()
             }
         }
