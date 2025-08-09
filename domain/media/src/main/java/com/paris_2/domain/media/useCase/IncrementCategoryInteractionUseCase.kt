8bda5f89b575d831b61551abb851e9bbe4c16f0c
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-09 15:30:34 +0300

    Refactor: Update IncrementCategoryInteractionUseCase to use Genre objects
    The `IncrementCategoryInteractionUseCase` now accepts a list of `Genre` objects instead of a list of genre IDs. This change simplifies the use case and aligns it with the domain entity.

--- domain/media/src/main/java/com/paris_2/domain/media/useCase/IncrementCategoryInteractionUseCase.kt
+++ domain/media/src/main/java/com/paris_2/domain/media/useCase/IncrementCategoryInteractionUseCase.kt
@@ -1,16 +1,17 @@
 package com.paris_2.domain.media.useCase
 
+import com.paris_2.domain.media.entity.Genre
 import com.paris_2.domain.media.entity.GenreUserInteraction
 import com.paris_2.domain.media.repository.GenresInteractionRepository
 
 class IncrementCategoryInteractionUseCase(
     private val genresInteractionRepository: GenresInteractionRepository
 ) {
-    suspend operator fun invoke(genreIds: List<Int>) {
-        genreIds.forEach { genreId ->
-            val count = genresInteractionRepository.getCategoryInteractions(genreId) ?: 0
+    suspend operator fun invoke(genre: List<Genre>) {
+        genre.forEach { genre ->
+            val count = genresInteractionRepository.getCategoryInteractions(genre) ?: 0
             genresInteractionRepository.upsertInteraction(
-                GenreUserInteraction(genreId = genreId, interactionCount = count + 1)
+                GenreUserInteraction(genre = genre, interactionCount = count + 1)
             )
         }
     }
