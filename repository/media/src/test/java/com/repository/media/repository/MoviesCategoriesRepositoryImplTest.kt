6cbd41da8349d2d61e0e24c56c37b7821fd7badb
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-09 16:01:21 +0300

    Refactor: Update MoviesCategoriesRepositoryImplTest to use Genre entity

--- repository/media/src/test/java/com/repository/media/repository/MoviesCategoriesRepositoryImplTest.kt
+++ repository/media/src/test/java/com/repository/media/repository/MoviesCategoriesRepositoryImplTest.kt
@@ -1,13 +1,13 @@
 package com.repository.media.repository
 
 import com.google.common.truth.Truth.assertThat
-import com.paris_2.domain.media.entity.Category
+import com.paris_2.domain.media.entity.Genre
 import com.paris_2.domain.media.exception.NoCategoriesFoundException
 import com.paris_2.repository.user.dataSource.local.LanguageLocalDataSourceRepository
 import com.repository.media.datasource.remote.GenresRemoteDataSource
 import com.repository.media.dto.GenreDto
 import com.repository.media.dto.GenresDto
-import com.repository.media.mapper.toCategoryList
+import com.repository.media.mapper.toGenreList
 import com.repository.media.util.NetworkConnectionChecker
 import io.mockk.coEvery
 import io.mockk.every
@@ -43,11 +43,11 @@ class MoviesCategoriesRepositoryImplTest {
                 GenreDto(id = 18, name = "Drama")
             )
         )
-        val categories = listOf(Category(28, "Action"), Category(18, "Drama"))
+        val categories = listOf(Genre.Action, Genre.Drama)
         coEvery { genresRemoteDataSource.getMoviesGenres("en-US") } returns genresDto
         coEvery { languageLocalDataSourceRepository.getLanguage() } returns MutableStateFlow("en")
 
-        assertThat(genresDto.toCategoryList()).isEqualTo(categories)
+        assertThat(genresDto.toGenreList()).isEqualTo(categories)
     }
 
     @Test
@@ -55,7 +55,7 @@ class MoviesCategoriesRepositoryImplTest {
         val genresDto = mockk<GenresDto>(relaxed = true)
         coEvery { genresRemoteDataSource.getMoviesGenres(any()) } returns genresDto
         coEvery { languageLocalDataSourceRepository.getLanguage() } returns MutableStateFlow("en")
-        every { genresDto.toCategoryList() } returns emptyList()
+        every { genresDto.toGenreList() } returns emptyList()
 
         assertThrows<NoCategoriesFoundException> {
             repo.getMoviesCategories()
