d528c63c9a8965b803247ba678a2d98dd19ba8ea
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-09 15:27:01 +0300

    Refactor: Update FilterUpComingMediaByCategoriesUseCase to use Genre entities
    The `FilterUpComingMediaByCategoriesUseCase` now accepts a list of `Genre` objects instead of a list of integer category IDs. This change simplifies the filtering logic by directly comparing `Genre` objects within the media's genres list.

--- domain/media/src/main/java/com/paris_2/domain/media/useCase/FilterUpComingMediaByCategoriesUseCase.kt
+++ domain/media/src/main/java/com/paris_2/domain/media/useCase/FilterUpComingMediaByCategoriesUseCase.kt
@@ -2,15 +2,14 @@ package com.paris_2.domain.media.useCase
 
 import com.paris_2.domain.media.entity.Media
 import com.paris_2.domain.media.repository.MediaRepository
+import com.paris_2.domain.media.entity.Genre
 
 class FilterUpComingMediaByCategoriesUseCase(
     private val mediaRepository: MediaRepository
 ) {
-    suspend operator fun invoke(categories: List<Int>): List<Media> {
+    suspend operator fun invoke(categories: List<Genre>): List<Media> {
         return mediaRepository.getUpComingMedia().filter { media ->
-            media.categoryIds.any {
-                categories.contains(it)
-            }
+            media.genres.any { categories.contains(it) }
         }
     }
 }
