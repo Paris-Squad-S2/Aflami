777da864a1688bb9eaed81fcf5428f4d2c8a04da
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-08-09 04:16:34 +0300

    Refactor: Update dependencies and mark unused material3 library for removal

--- feature/guessGame/guessGameUi/build.gradle.kts
+++ feature/guessGame/guessGameUi/build.gradle.kts
@@ -39,6 +39,6 @@ android {
 dependencies {
     implementation(libs.bundles.compose)
     implementation(platform(libs.androidx.compose.bom))
-
     implementation(project(Modules.FEATURE_GUESS_GAME_API))
+    implementation(libs.androidx.material3) // TODO: Remove it
 }
