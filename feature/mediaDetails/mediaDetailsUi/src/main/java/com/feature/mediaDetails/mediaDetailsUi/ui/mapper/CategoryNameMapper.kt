6b1075aeeac8f6ddc7f5bbb9dc084e252760d612
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-10 16:03:10 +0300

    convert UPPER_SNAKE_CASE to PascalCase

--- feature/mediaDetails/mediaDetailsUi/src/main/java/com/feature/mediaDetails/mediaDetailsUi/ui/mapper/CategoryNameMapper.kt
+++ feature/mediaDetails/mediaDetailsUi/src/main/java/com/feature/mediaDetails/mediaDetailsUi/ui/mapper/CategoryNameMapper.kt
@@ -4,34 +4,34 @@ import com.paris_2.domain.media.entity.Category
 
 
 fun Category.toUiCategory(): CategoryMediaUi = when (this) {
-    Category.ACTION -> CategoryMediaUi.ACTION
-    Category.ADVENTURE -> CategoryMediaUi.ADVENTURE
-    Category.ANIMATION -> CategoryMediaUi.ANIMATION
-    Category.COMEDY -> CategoryMediaUi.COMEDY
-    Category.CRIME -> CategoryMediaUi.CRIME
-    Category.DOCUMENTARY -> CategoryMediaUi.DOCUMENTARY
-    Category.DRAMA -> CategoryMediaUi.DRAMA
-    Category.FAMILY -> CategoryMediaUi.FAMILY
-    Category.FANTASY -> CategoryMediaUi.FANTASY
-    Category.HISTORY -> CategoryMediaUi.HISTORY
-    Category.HORROR -> CategoryMediaUi.HORROR
-    Category.MUSIC -> CategoryMediaUi.MUSIC
-    Category.MYSTERY -> CategoryMediaUi.MYSTERY
-    Category.ROMANCE -> CategoryMediaUi.ROMANCE
-    Category.SCIENCE_FICTION -> CategoryMediaUi.SCIENCE_FICTION
-    Category.TV_MOVIE -> CategoryMediaUi.TV_MOVIE
-    Category.THRILLER -> CategoryMediaUi.THRILLER
-    Category.WAR -> CategoryMediaUi.WAR
-    Category.WESTERN -> CategoryMediaUi.WESTERN
-    Category.ACTION_ADVENTURE -> CategoryMediaUi.ACTION_ADVENTURE
-    Category.KIDS -> CategoryMediaUi.KIDS
-    Category.NEWS -> CategoryMediaUi.NEWS
-    Category.REALITY -> CategoryMediaUi.REALITY
-    Category.SCIFI_FANTASY -> CategoryMediaUi.SCIFI_FANTASY
-    Category.SOAP -> CategoryMediaUi.SOAP
-    Category.TALK -> CategoryMediaUi.TALK
-    Category.WAR_POLITICS -> CategoryMediaUi.WAR_POLITICS
-    Category.UNKNOWN -> CategoryMediaUi.UNKNOWN
+    Category.Action -> CategoryMediaUi.Action
+    Category.Adventure -> CategoryMediaUi.Adventure
+    Category.Animation -> CategoryMediaUi.Animation
+    Category.Comedy -> CategoryMediaUi.Comedy
+    Category.Crime -> CategoryMediaUi.Crime
+    Category.Documentary -> CategoryMediaUi.Documentary
+    Category.Drama -> CategoryMediaUi.Drama
+    Category.Family -> CategoryMediaUi.Family
+    Category.Fantasy -> CategoryMediaUi.Fantasy
+    Category.History -> CategoryMediaUi.History
+    Category.Horror -> CategoryMediaUi.Horror
+    Category.Music -> CategoryMediaUi.Music
+    Category.Mystery -> CategoryMediaUi.Mystery
+    Category.Romance -> CategoryMediaUi.Romance
+    Category.ScienceFiction -> CategoryMediaUi.ScienceFiction
+    Category.TvMovie -> CategoryMediaUi.TvMovie
+    Category.Thriller -> CategoryMediaUi.Thriller
+    Category.War -> CategoryMediaUi.War
+    Category.Western -> CategoryMediaUi.Western
+    Category.ActionAdventure -> CategoryMediaUi.ActionAdventure
+    Category.Kids -> CategoryMediaUi.Kids
+    Category.News -> CategoryMediaUi.News
+    Category.Reality -> CategoryMediaUi.Reality
+    Category.ScifiFantasy -> CategoryMediaUi.ScifiFantasy
+    Category.Soap -> CategoryMediaUi.Soap
+    Category.Talk -> CategoryMediaUi.Talk
+    Category.WarPolitics -> CategoryMediaUi.WarPolitics
+    Category.Unknown -> CategoryMediaUi.Unknown
 }
 
 fun Category.toDisplayName(): Int = this.toUiCategory().displayNameResId
