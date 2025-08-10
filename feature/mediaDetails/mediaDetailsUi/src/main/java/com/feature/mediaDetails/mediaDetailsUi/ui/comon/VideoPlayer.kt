38a56174dc5fdc8ddd02b3fd4892ded72333f1a5
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-10 22:13:38 +0300

    Fix: Reorder imports in VideoPlayer.kt
    The import statements in `VideoPlayer.kt` have been reordered to follow standard Kotlin conventions. `androidx.compose.runtime.getValue` and `androidx.compose.runtime.remember` were swapped.

--- feature/mediaDetails/mediaDetailsUi/src/main/java/com/feature/mediaDetails/mediaDetailsUi/ui/comon/VideoPlayer.kt
+++ feature/mediaDetails/mediaDetailsUi/src/main/java/com/feature/mediaDetails/mediaDetailsUi/ui/comon/VideoPlayer.kt
@@ -6,10 +6,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.key
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
