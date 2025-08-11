6d8a9b4045d41e4a529f789b2207439e0f1ac631
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-08-11 07:52:10 +0300

    add splash screen with animated vector and update theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -129,6 +129,7 @@ dependencies {
     implementation(libs.bundles.room)
     implementation(libs.bundles.retrofit)
     implementation(libs.bundles.serialization)
+    implementation(libs.androidx.core.splashscreen)
 
     ksp(libs.hilt.android.compiler)
     ksp(libs.room.compiler)
