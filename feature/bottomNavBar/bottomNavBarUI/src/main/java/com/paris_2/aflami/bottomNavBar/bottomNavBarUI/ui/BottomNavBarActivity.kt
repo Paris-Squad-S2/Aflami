82026234d0948de9cdc6395b95dd06d58df7c476
mohammed-akkad <momoo362514@gmail.com>
2025-08-11 14:45:14 +0300

    refactor activity

--- feature/bottomNavBar/bottomNavBarUI/src/main/java/com/paris_2/aflami/bottomNavBar/bottomNavBarUI/ui/BottomNavBarActivity.kt
+++ feature/bottomNavBar/bottomNavBarUI/src/main/java/com/paris_2/aflami/bottomNavBar/bottomNavBarUI/ui/BottomNavBarActivity.kt
@@ -1,8 +1,5 @@
 package com.paris_2.aflami.bottomNavBar.bottomNavBarUI.ui
 
-import android.content.Context
-import android.content.res.Configuration
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,7 +14,6 @@ import com.paris_2.aflami.bottomNavBar.bottomNavBarUI.ui.main.InstallSavedAppLan
 import com.paris_2.aflami.designsystem.theme.AflamiTheme
 import com.paris_2.domain.user.usecase.SettingsUseCase
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.Locale
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -63,27 +59,4 @@ class BottomNavBarActivity : AppCompatActivity() {
 
     }
 
-
-    override fun attachBaseContext(newBase: Context) {
-        val prefs = newBase.getSharedPreferences("settings", MODE_PRIVATE)
-        val lang = prefs.getString("language_code", "en") ?: "en"
-        val localizedContext = updateLocale(newBase, lang)
-        super.attachBaseContext(localizedContext)
-    }
-
-    private fun updateLocale(context: Context, language: String): Context {
-        val locale = Locale(language)
-        Locale.setDefault(locale)
-
-        val config = Configuration(context.resources.configuration)
-        config.setLocale(locale)
-
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            context.createConfigurationContext(config)
-        } else {
-            @Suppress("DEPRECATION")
-            context.resources.updateConfiguration(config, context.resources.displayMetrics)
-            context
-        }
-    }
 }
--- feature/home/homeUi/src/main/java/com/feature/home/homeUi/screen/topRatingMovies/TopRatingActivity.kt
+++ feature/home/homeUi/src/main/java/com/feature/home/homeUi/screen/topRatingMovies/TopRatingActivity.kt
@@ -1,8 +1,5 @@
 package com.feature.home.homeUi.screen.topRatingMovies
 
-import android.content.Context
-import android.content.res.Configuration
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -10,7 +7,6 @@ import com.feature.home.homeUi.screen.main.InstallSavedAppLanguage
 import com.paris_2.aflami.designsystem.theme.AflamiTheme
 import com.paris_2.domain.user.usecase.SettingsUseCase
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.Locale
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -24,29 +20,6 @@ class TopRatingActivity : AppCompatActivity() {
             AflamiTheme(settingsUseCase.isDarkTheme()) {
                 TopRatingMoviesScreen()
             }
-        }
-    }
-
-    override fun attachBaseContext(newBase: Context) {
-        val prefs = newBase.getSharedPreferences("settings", MODE_PRIVATE)
-        val lang = prefs.getString("language_code", "en") ?: "en"
-        val localizedContext = updateLocale(newBase, lang)
-        super.attachBaseContext(localizedContext)
-    }
-
-    private fun updateLocale(context: Context, language: String): Context {
-        val locale = Locale(language)
-        Locale.setDefault(locale)
-
-        val config = Configuration(context.resources.configuration)
-        config.setLocale(locale)
-
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            context.createConfigurationContext(config)
-        } else {
-            @Suppress("DEPRECATION")
-            context.resources.updateConfiguration(config, context.resources.displayMetrics)
-            context
         }
     }
 }
