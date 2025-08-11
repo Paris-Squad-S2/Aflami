2a76b23243ccb9a0e4de43cf81e1e18dcfaddf4d
mohammed-akkad <momoo362514@gmail.com>
2025-08-11 14:03:59 +0300

    update watch history screen

--- feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/screen/watchHistory/WatchHistoryUiState.kt
+++ feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/screen/watchHistory/WatchHistoryUiState.kt
@@ -6,7 +6,17 @@ data class WatchHistoryUiState(
     val watchHistoryMedia: List<MediaUiState>,
     val isLoading: Boolean,
     val errorMessage: String?,
+    val contentRestriction: ContentRestriction = ContentRestriction.STRICT,
+    val nsfwThreshold: Float = 0.8f,
+    val genderThreshold: Float = 0.6f
 )
+
+enum class ContentRestriction() {
+    STRICT,
+    MODERATE,
+    OFF
+}
+
 
 data class MediaUiState(
     val id: Int,
--- feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/screen/watchHistory/WatchHistoryViewModel.kt
+++ feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/screen/watchHistory/WatchHistoryViewModel.kt
@@ -1,11 +1,14 @@
 package com.feature.profile.profileUi.screen.watchHistory
 
+import androidx.lifecycle.viewModelScope
 import com.feature.mediaDetails.mediaDetailsApi.MediaDetailsFeatureAPI
 import com.feature.profile.profileUi.common.BaseViewModel
-import com.feature.profile.profileUi.mapper.toMediaUiStateList
 import com.feature.profile.profileUi.mapper.toMediaType
+import com.feature.profile.profileUi.mapper.toMediaUiStateList
 import com.paris_2.domain.media.useCase.FilterWatchHistoryUseCase
+import com.paris_2.domain.user.usecase.SettingsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -13,6 +16,7 @@ import javax.inject.Inject
 class WatchHistoryViewModel @Inject constructor(
     private val filterWatchHistoryUseCase: FilterWatchHistoryUseCase,
     private val mediaDetailsFeatureAPI: MediaDetailsFeatureAPI,
+    private val settingsUseCase: SettingsUseCase,
 ) : WatchHistoryInteractionListener, BaseViewModel<WatchHistoryUiState>(
     initialState = WatchHistoryUiState(
         isLoading = false,
@@ -22,8 +26,43 @@ class WatchHistoryViewModel @Inject constructor(
 ){
     private var selectedMediaType: MediaTypeUi = MediaTypeUi.MOVIE
     init {
+        getRestriction()
         loadWatchHistoryMedia(selectedMediaType)
     }
+
+    private fun getRestriction() {
+        viewModelScope.launch {
+            val restriction = settingsUseCase.getRestriction()
+            updateState(
+                screenState.value.copy(
+                    contentRestriction = ContentRestriction.valueOf(restriction)
+                )
+            )
+            when (screenState.value.contentRestriction) {
+                ContentRestriction.STRICT -> updateState(
+                    screenState.value.copy(
+                        nsfwThreshold = 0.8f,
+                        genderThreshold = 0.6f
+                    )
+                )
+
+                ContentRestriction.MODERATE -> updateState(
+                    screenState.value.copy(
+                        nsfwThreshold = 0.4f,
+                        genderThreshold = 0.6f
+                    )
+                )
+
+                ContentRestriction.OFF -> updateState(
+                    screenState.value.copy(
+                        nsfwThreshold = 0f,
+                        genderThreshold = 0f
+                    )
+                )
+            }
+        }
+    }
+
     private fun loadWatchHistoryMedia(mediaType: MediaTypeUi) {
         tryToExecute(
             execute = {
