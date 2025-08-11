61a1892a7a3d23d9e93d1067c42d3d1094dc6e8f
mohammed-akkad <momoo362514@gmail.com>
2025-08-11 14:07:49 +0300

    use data store instead of shared preferences

--- datasource/local/user/src/main/java/com/paris_2/dataSource/local/user/SettingLocalDataSourceImp.kt
+++ datasource/local/user/src/main/java/com/paris_2/dataSource/local/user/SettingLocalDataSourceImp.kt
@@ -1,65 +1,78 @@
 package com.paris_2.dataSource.local.user
 
-import android.content.Context
-import android.content.SharedPreferences
-import androidx.core.content.edit
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import com.paris_2.repository.user.dataSource.local.SettingLocalDataSource
-import dagger.hilt.android.qualifiers.ApplicationContext
 import jakarta.inject.Inject
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.mapLatest
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class SettingLocalDataSourceImp @Inject constructor(
-    @ApplicationContext private val context: Context,
+    private val dataStore: DataStore<Preferences>,
 ) : SettingLocalDataSource {
 
-    companion object {
-        private const val PREF_NAME = "settings"
-        private const val LANGUAGE_KEY = "language_code"
-        private const val KEY_ONBOARDING_COMPLETED = "onboarding_completed"
-        private const val KEY_IS_DARK_THEME = "is_dark_theme"
-    }
-
-    private val prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
-
-    private val _languageFlow = MutableStateFlow(getLanguageSync())
-    override fun getLanguage(): Flow<String> = _languageFlow.asStateFlow()
-
-    private val preferenceChangeListener =
-        SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences, key ->
-            if (key == LANGUAGE_KEY) {
-                val newLanguage = sharedPreferences.getString(LANGUAGE_KEY, "en") ?: "en"
-                _languageFlow.value = newLanguage
-            }
+    override fun getLanguage(): Flow<String> {
+        return dataStore.data.mapLatest {
+            it[LANGUAGE_KEY] ?: ENGLISH
         }
-
-    init {
-        prefs.registerOnSharedPreferenceChangeListener(preferenceChangeListener)
     }
 
     override suspend fun setLanguage(language: String) {
-        prefs.edit { putString(LANGUAGE_KEY, language) }
+        dataStore.setValue(LANGUAGE_KEY, language)
     }
 
-    override fun setOnboardingCompleted() {
-        prefs.edit { putBoolean(KEY_ONBOARDING_COMPLETED, true) }
+    override suspend fun setOnboardingCompleted() {
+        dataStore.setValue(KEY_ONBOARDING_COMPLETED, true)
     }
 
-    override fun isOnboardingCompleted(): Boolean {
-        return prefs.getBoolean(KEY_ONBOARDING_COMPLETED, false)
+    override suspend fun isOnboardingCompleted(): Boolean {
+        return dataStore.data.mapLatest {
+            it[KEY_ONBOARDING_COMPLETED] ?: false
+        }.first()
     }
 
-    override fun setTheme(isDarkTheme: Boolean) {
-        prefs.edit { putBoolean(KEY_IS_DARK_THEME, isDarkTheme) }
+    override suspend fun setTheme(isDarkTheme: Boolean) {
+        dataStore.setValue(KEY_IS_DARK_THEME, isDarkTheme)
     }
 
-    override fun getTheme(): Boolean {
-       return prefs.getBoolean(KEY_IS_DARK_THEME, true)
+    override fun getTheme(): Flow<Boolean> {
+        return dataStore.data.mapLatest {
+            it[KEY_IS_DARK_THEME] ?: true
+        }
     }
 
-    private fun getLanguageSync(): String {
-        return prefs.getString(LANGUAGE_KEY, "en") ?: "en"
+    override suspend fun setRestriction(restriction: String) {
+        dataStore.setValue(RESTRICTION, restriction)
+    }
+
+    override suspend fun getRestriction(): String {
+        return dataStore.data.mapLatest {
+            it[RESTRICTION] ?: STRICT
+        }.first()
+    }
+
+    private suspend fun <T> DataStore<Preferences>.setValue(
+        key: Preferences.Key<T>,
+        value: T,
+    ) {
+        this.edit { preferences ->
+            preferences[key] = value
+        }
+    }
+
+    private companion object {
+        val LANGUAGE_KEY = stringPreferencesKey("language_code")
+        val KEY_ONBOARDING_COMPLETED = booleanPreferencesKey("onboarding_completed")
+        val RESTRICTION = stringPreferencesKey("restriction")
+        val KEY_IS_DARK_THEME = booleanPreferencesKey("is_dark_theme")
+        const val ENGLISH = "en"
+        const val STRICT = "STRICT"
     }
 
 }
