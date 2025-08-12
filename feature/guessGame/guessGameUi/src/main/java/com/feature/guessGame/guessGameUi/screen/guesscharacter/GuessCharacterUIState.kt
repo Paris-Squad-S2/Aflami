acf355a8db2270c49b8ec93a5a6b634087438eb1
AhmedSalah <ahmedsaaalah21@gmail.com>
2025-08-13 00:24:31 +0300

    Feat: Add GuessCharacterUIState
    This commit introduces the `GuessCharacterUIState` and `Question` data classes for the guess character feature.
    
    `GuessCharacterUIState` manages UI-related data including error messages, loading state, list of questions, correctness of choice, image blur level, and score.
    `Question` holds the image and a list of possible answers for a single question.

---
+++ feature/guessGame/guessGameUi/src/main/java/com/feature/guessGame/guessGameUi/screen/guesscharacter/GuessCharacterUIState.kt
@@ -0,0 +1,18 @@
+package com.feature.guessGame.guessGameUi.screen.guesscharacter
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class GuessCharacterUIState(
+    val error: String? = null,
+    val isLoading: Boolean = false,
+    val question: List<Question> = emptyList(),
+    val isChoiceCorrect: Boolean = false,
+    val imageBlur: Dp = 8.dp,
+    val score: Int = 0,
+)
+
+data class Question(
+    val image: String = "",
+    val answers: List<String> = emptyList(),
+)
