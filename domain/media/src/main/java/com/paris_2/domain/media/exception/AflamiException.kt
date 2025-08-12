7572ec6a1d018cdb1e6d22ce39dd8c114622ce65
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-08-12 17:44:04 +0300

    Refactor: Simplify AflamiException hierarchy
    Removed several unused custom exception classes and updated `NoInternetConnectionException` to use a default message.

--- domain/media/src/main/java/com/paris_2/domain/media/exception/AflamiException.kt
+++ domain/media/src/main/java/com/paris_2/domain/media/exception/AflamiException.kt
@@ -2,38 +2,7 @@ package com.paris_2.domain.media.exception
 
 open class AflamiException(message: String) : Exception(message)
 
-class NoInternetConnectionException(message: String = "No internet connection") : AflamiException(message)
-//class NoMediaForCountryException : AflamiException("No movies found for the given country")
-//class NoMediaForActorException : AflamiException("No media found for the given actor")
-//class NoMediaForSearchException : AflamiException("No media matched your search term")
-//class NoCategoriesFoundException : AflamiException("No categories found ")
-//
-//class NoTvShowFoundException(message: String = "No found tv show") : AflamiException(message)
-//class NoGalleryFoundException(message: String = "no found gallery ") :
-//    AflamiException(message)
-//class NoCastFoundException(message: String = "no found Cast") : AflamiException(message)
-//class NoVideoFoundException(message: String = "no found video") : AflamiException(message)
-//class NoSeasonFoundException(message: String = "no found season tv show") : AflamiException(message)
-//class NoMovieFoundException(message: String = "No found movie") : AflamiException(message)
-//class NoReviewFoundException(message: String = "No found review") : AflamiException(message)
-//class NoSimilarFoundException(message: String = "No found similar") : AflamiException(message)
-//class NoProductionCompanyFoundException(message: String = "No found production company") :
-//    AflamiException(message)
-//class FailedToAddRatingException(message: String = "Failed to add rating") :
-//    AflamiException(message)
+class NoInternetConnectionException() :
+    AflamiException("No internet connection")
 
-class FailedException(message: String) :
-    AflamiException(message)
-
-//class FailedToDeleteRatingException(message: String = "Failed to delete rating") :
-//    AflamiException(message)
-
-//class PopularMediaException(message: String = "No popular media found") : AflamiException(message)
-//class TopRatingMediaException(message: String = "No top rating media found") : AflamiException(message)
-//class UpComingMediaException(message: String = "No upcoming media found") : AflamiException(message)
-//class MediaPlayingException(message: String = "No media currently playing") : AflamiException(message)
-//class AddMediaToContinueWatchingException(message: String = "Can't add  media to Continue Watching List") : AflamiException(message)
-//class GetContinueWatchingMediaException(message: String = "Something went wrong while fetching continue watching media") : AflamiException(message)
-//
-//class NoRatedMediaFoundException(message: String = "No Rated Media Found Exception") : AflamiException(message)
-//
+class FailedException(message: String) : AflamiException(message)
