0e892a27f842ba3764c8f1229f330c98e0728ff0
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-08-12 23:37:24 +0300

    refactor signing config and update dependencies- Move signing config to separate SigningExtensions.kt file- Update Firebase Crashlytics version to3.0.6- Add firebase-perf dependency with version1.4.2- Remove duplicate Firebase plugin declarations- Enable buildConfig for logger module

--- logger/src/main/java/com/parise_2/firebase/firebase/FireBaseCrashlyticsLogger.kt
+++ logger/src/main/java/com/parise_2/firebase/firebase/FireBaseCrashlyticsLogger.kt
@@ -2,9 +2,12 @@ package com.parise_2.firebase.firebase
 
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.parise_2.firebase.repo.Logger
+import com.parise_2.logger.BuildConfig
 
 class FireBaseCrashlyticsLogger : Logger {
     override fun logException(exception: Exception) {
-        FirebaseCrashlytics.getInstance().recordException(exception)
+        if (!BuildConfig.DEBUG) {
+            FirebaseCrashlytics.getInstance().recordException(exception)
+        }
     }
 }
