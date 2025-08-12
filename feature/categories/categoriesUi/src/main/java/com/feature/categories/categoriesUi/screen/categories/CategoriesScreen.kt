9df31f77e581b1ddee4b24df200591cf118837da
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-08-12 13:04:54 +0300

    Populate movie categories in CategoriesScreen
    Populates the `moviesCategories` list in `CategoriesScreenUIState` with various movie genres, including their names, icons, and corresponding `Category` enum values.
    
    Also includes:
    - Added string resources for new category names in English and Arabic.
    - Added drawable resources for category icons.
    - Added `navigationBarsPadding()` to the `CategoriesScreen` layout.

--- feature/categories/categoriesUi/src/main/java/com/feature/categories/categoriesUi/screen/categories/CategoriesScreen.kt
+++ feature/categories/categoriesUi/src/main/java/com/feature/categories/categoriesUi/screen/categories/CategoriesScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.grid.GridCells
--- feature/categories/categoriesUi/src/main/java/com/feature/categories/categoriesUi/screen/categories/CategoriesScreenUIState.kt
+++ feature/categories/categoriesUi/src/main/java/com/feature/categories/categoriesUi/screen/categories/CategoriesScreenUIState.kt
@@ -2,6 +2,7 @@ package com.feature.categories.categoriesUi.screen.categories
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import com.feature.categories.categoriesUi.R
 import com.paris_2.domain.media.entity.Category
 
 data class CategoriesScreenUIState(
@@ -19,11 +20,101 @@ enum class Status {
 data class CategoriesUIState(
     val selectedTabIndex: Int = 0,
     val moviesCategories: List<CategoryUiState> = listOf(
-//        CategoryUiState(
-//            category = Category.Action,
-//            name = R.string.action,
-//            icon = R.drawable.
-//        ),
+        CategoryUiState(
+            category = Category.Action,
+            name = R.string.action,
+            icon = R.drawable.img_category_action
+        ),
+        CategoryUiState(
+            category = Category.Western,
+            name = R.string.western,
+            icon = R.drawable.img_category_western
+        ),
+        CategoryUiState(
+            category = Category.Romance,
+            name = R.string.romance,
+            icon = R.drawable.img_category_romance
+        ),
+        CategoryUiState(
+            category = Category.Adventure,
+            name = R.string.adventure,
+            icon = R.drawable.img_category_adventure
+        ),
+        CategoryUiState(
+            category = Category.History,
+            name = R.string.history,
+            icon = R.drawable.img_category_history
+        ),
+        CategoryUiState(
+            category = Category.Family,
+            name = R.string.family,
+            icon = R.drawable.img_category_family
+        ),
+        CategoryUiState(
+            category = Category.TvMovie,
+            name = R.string.tv_movie,
+            icon = R.drawable.img_category_tv_movie
+        ),
+        CategoryUiState(
+            category = Category.Horror,
+            name = R.string.horror,
+            icon = R.drawable.img_category_horror
+        ),
+        CategoryUiState(
+            category = Category.War,
+            name = R.string.war,
+            icon = R.drawable.img_category_war
+        ),
+        CategoryUiState(
+            category = Category.Comedy,
+            name = R.string.comedy,
+            icon = R.drawable.img_category_comedy
+        ),
+        CategoryUiState(
+            category = Category.Mystery,
+            name = R.string.mystery,
+            icon = R.drawable.img_category_mystery
+        ),
+        CategoryUiState(
+            category = Category.Music,
+            name = R.string.music,
+            icon = R.drawable.img_category_music
+        ),
+        CategoryUiState(
+            category = Category.Thriller,
+            name = R.string.thriller,
+            icon = R.drawable.img_category_thriller
+        ),
+        CategoryUiState(
+            category = Category.ScienceFiction,
+            name = R.string.science_fiction,
+            icon = R.drawable.img_category_science_fiction
+        ),
+        CategoryUiState(
+            category = Category.Documentary,
+            name = R.string.documentary,
+            icon = R.drawable.img_category_documentary
+        ),
+        CategoryUiState(
+            category = Category.Fantasy,
+            name = R.string.fantasy,
+            icon = R.drawable.img_category_fantasy
+        ),
+        CategoryUiState(
+            category = Category.Drama,
+            name = R.string.drama,
+            icon = R.drawable.img_category_drama
+        ),
+        CategoryUiState(
+            category = Category.Crime,
+            name = R.string.crime,
+            icon = R.drawable.img_category_crime
+        ),
+        CategoryUiState(
+            category = Category.Animation,
+            name = R.string.animation,
+            icon = R.drawable.img_category_animation
+        ),
     ),
     val tvShowsCategories: List<CategoryUiState> = emptyList(),
 )
