1e0980069e8641cfcd97c3b92a4e7354a521a55b
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-13 09:50:49 +0300

    Add mappers for UserPoints
    Added `toDomain` and `toEntity` extension functions for mapping between `UserGamePointsEntity` and the domain model `UserPoints`.

--- repository/guessGame/src/main/java/com/repository/guessgame/mapper/Mapper.kt
+++ repository/guessGame/src/main/java/com/repository/guessgame/mapper/Mapper.kt
@@ -1,9 +1,11 @@
 package com.repository.guessgame.mapper
 
-import com.paris_2.domain.game.models.Actor
-import com.paris_2.domain.game.models.ActorMedia
+import com.paris_2.domain.game.entity.Actor
+import com.paris_2.domain.game.entity.ActorMedia
+import com.paris_2.domain.game.entity.UserPoints
 import com.repository.guessgame.dto.ActorDto
 import com.repository.guessgame.dto.ActorMediaDto
+import com.repository.guessgame.entity.UserGamePointsEntity
 import kotlinx.datetime.LocalDate
 
 fun ActorDto.toDomain() : Actor? {
@@ -24,5 +26,19 @@ fun ActorMediaDto.toDomain() : ActorMedia?{
         name = title ?: "",
         posterImg = posterPath ?: "",
         yearOfRelease = parsedDate
+    )
+}
+
+fun UserGamePointsEntity.toDomain() : UserPoints{
+    return UserPoints(
+        userId = userId,
+        gamePoints = gamePoints
+    )
+}
+
+fun UserPoints.toEntity() : UserGamePointsEntity{
+    return UserGamePointsEntity(
+        userId = userId,
+        gamePoints = gamePoints
     )
 }
