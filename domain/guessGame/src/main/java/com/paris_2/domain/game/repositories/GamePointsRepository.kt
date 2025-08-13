c00192599796ba1b1c9bc5b543d98294d3fa298a
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-13 09:48:37 +0300

    add game points repo and repo implementation

---
+++ domain/guessGame/src/main/java/com/paris_2/domain/game/repositories/GamePointsRepository.kt
@@ -0,0 +1,7 @@
+package com.paris_2.domain.game.repositories
+
+import com.paris_2.domain.game.entity.UserPoints
+
+interface GamePointsRepository {
+    suspend fun saveUserGamePoints(userPoints: UserPoints)
+}
