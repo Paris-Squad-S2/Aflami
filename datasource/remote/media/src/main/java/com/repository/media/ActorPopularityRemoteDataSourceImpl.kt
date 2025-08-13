8dd3227e98520ec479ab3cb2336b587e5e012277
MuhammedWael9991 <mw26200400@gmail.com>
2025-08-13 17:27:10 +0300

    feat: add pagination support to getPopularActors API call

--- datasource/remote/media/src/main/java/com/repository/media/ActorPopularityRemoteDataSourceImpl.kt
+++ datasource/remote/media/src/main/java/com/repository/media/ActorPopularityRemoteDataSourceImpl.kt
@@ -3,11 +3,12 @@ package com.repository.media
 import com.repository.guessgame.datasource.remote.ActorPopularityRemoteDataSource
 import com.repository.guessgame.dto.ActorPopularityListDto
 import com.repository.media.services.ActorPopularityApiService
+import kotlin.random.Random
 
 class ActorPopularityRemoteDataSourceImpl(
     private val apiService : ActorPopularityApiService
 ): ActorPopularityRemoteDataSource {
     override suspend fun getPopularActors(language: String): ActorPopularityListDto {
-        return apiService.getPopularActors(language)
+        return apiService.getPopularActors(language, Random.nextInt(1, 500))
     }
 }
--- datasource/remote/media/src/main/java/com/repository/media/services/ActorPopularityApiService.kt
+++ datasource/remote/media/src/main/java/com/repository/media/services/ActorPopularityApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Query
 interface ActorPopularityApiService{
     @GET("person/popular")
     suspend fun getPopularActors(
-        @Query("language")
-        language: String,
+        @Query("language") language: String,
+        @Query("page") page: Int,
     ) : ActorPopularityListDto
 }
