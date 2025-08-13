3fe22d65f370e461a41565e43bc068c1f56a4be3
MuhammedWael9991 <mw26200400@gmail.com>
2025-08-13 17:33:22 +0300

    feat: implement SubmitAnswerUseCase for processing user answers and updating game session score

--- domain/guessGame/src/main/java/com/paris_2/domain/game/usecases/SubmitAnswerUseCase.kt
+++ domain/guessGame/src/main/java/com/paris_2/domain/game/usecases/SubmitAnswerUseCase.kt
@@ -1,4 +1,15 @@
 package com.paris_2.domain.game.usecases
 
+import com.paris_2.domain.game.entity.GameSession
+
 class SubmitAnswerUseCase {
+    operator fun invoke(session: GameSession, selectedAnswer: String) {
+        val question = session.getCurrentQuestion() ?: return
+        if (!question.usedHint && question.options.any { it.isCorrect }) {
+            if (selectedAnswer == question.correctAnswer) {
+                session.score += if (question.usedHint) 5 else 10
+            }
+        }
+        session.moveToNextQuestion()
+    }
 }
