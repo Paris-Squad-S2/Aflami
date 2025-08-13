379a2bc0f0faba923590493c0eb2a28a50cb067c
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-13 07:21:52 +0300

    Add mappers for Actor and ActorMedia
    New extension functions `toDomain()` have been added to convert `ActorDto` to `Actor` and `ActorMediaDto` to `ActorMedia` respectively. These mappers handle potential null values in DTO fields and include date parsing for `ActorMediaDto`.

---
+++ repository/guessGame/src/main/java/com/repository/guessgame/mapper/Mapper.kt
@@ -0,0 +1,28 @@
+package com.repository.guessgame.mapper
+
+import com.paris_2.domain.game.models.Actor
+import com.paris_2.domain.game.models.ActorMedia
+import com.repository.guessgame.dto.ActorDto
+import com.repository.guessgame.dto.ActorMediaDto
+import kotlinx.datetime.LocalDate
+
+fun ActorDto.toDomain() : Actor? {
+    return Actor(
+      id = id ?: -1,
+        name = originalName ?: "",
+        imageUri = profilePath ?: "",
+        media = knownFor?.mapNotNull { it?.toDomain() } ?: emptyList()
+    )
+}
+
+fun ActorMediaDto.toDomain() : ActorMedia?{
+    val parsedDate = releaseDate?.let {
+        runCatching { LocalDate.parse(it) }.getOrNull()
+    } ?: return null
+    return ActorMedia(
+        id = id ?: -1,
+        name = title ?: "",
+        posterImg = posterPath ?: "",
+        yearOfRelease = parsedDate
+    )
+}
