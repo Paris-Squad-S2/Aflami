d56c8c6a0d2c7d48a2b0ea1f57beb98e0ab25af5
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-13 07:20:19 +0300

    Add new service to fetch popular actors
    A new service `ActorPopularityApiService` has been created to fetch a list of popular actors.
    It includes the `getPopularActors` suspend function that takes a language string as a parameter and returns an `ActorPopularityListDto`.

---
+++ datasource/remote/media/src/main/java/com/repository/media/services/ActorPopularityApiService.kt
@@ -0,0 +1,13 @@
+package com.repository.media.services
+
+import com.repository.guessgame.dto.ActorPopularityListDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ActorPopularityApiService{
+    @GET("person/popular")
+    suspend fun getPopularActors(
+        @Query("language")
+        language: String,
+    ) : ActorPopularityListDto
+}
