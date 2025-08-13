df78a49787bb6bcfde7518f01881031b6cde2bf5
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-13 09:50:57 +0300

    Add SaveUserGamePoints use case
    A new use case, `SaveUserGamePoints`, has been added to the `guessGame` domain.
    This use case is responsible for saving the user's game points using the `GamePointsRepository`.

---
+++ domain/guessGame/src/main/java/com/paris_2/domain/game/usecases/SaveUserGamePoints.kt
@@ -0,0 +1,12 @@
+package com.paris_2.domain.game.usecases
+
+import com.paris_2.domain.game.entity.UserPoints
+import com.paris_2.domain.game.repositories.GamePointsRepository
+
+class SaveUserGamePoints(
+    private val repository: GamePointsRepository
+) {
+    suspend operator fun invoke(points: UserPoints) {
+        repository.saveUserGamePoints(points)
+    }
+}
