7f94f0510c28fb63d21e56041a174b8eb3cc5ca1
MuhammedWael9991 <mw26200400@gmail.com>
2025-08-13 14:43:53 +0300

    feat: add Room DAO annotation to GamePointsDao for database integration

--- datasource/local/guessGame/src/main/java/com/datasource/local/guessGame/dao/GamePointsDao.kt
+++ datasource/local/guessGame/src/main/java/com/datasource/local/guessGame/dao/GamePointsDao.kt
@@ -1,8 +1,10 @@
 package com.datasource.local.guessGame.dao
 
+import androidx.room.Dao
 import androidx.room.Upsert
 import com.repository.guessgame.entity.UserGamePointsEntity
 
+@Dao
 interface GamePointsDao {
     @Upsert
     suspend fun upsertUserGamePoints(userGamePoints: UserGamePointsEntity)
