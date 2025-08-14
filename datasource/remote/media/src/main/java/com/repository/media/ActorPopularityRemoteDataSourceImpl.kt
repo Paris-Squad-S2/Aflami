05cd2a7388ddd02a1c02e213f465566ac807755d
MuhammedWael9991 <mw26200400@gmail.com>
2025-08-14 17:36:43 +0300

    feat: inject ActorPopularityApiService into ActorPopularityRemoteDataSourceImpl constructor

--- datasource/remote/media/src/main/java/com/repository/media/ActorPopularityRemoteDataSourceImpl.kt
+++ datasource/remote/media/src/main/java/com/repository/media/ActorPopularityRemoteDataSourceImpl.kt
@@ -3,9 +3,10 @@ package com.repository.media
 import com.repository.guessgame.datasource.remote.ActorPopularityRemoteDataSource
 import com.repository.guessgame.dto.ActorPopularityListDto
 import com.repository.media.services.ActorPopularityApiService
+import javax.inject.Inject
 import kotlin.random.Random
 
-class ActorPopularityRemoteDataSourceImpl(
+class ActorPopularityRemoteDataSourceImpl @Inject constructor(
     private val apiService : ActorPopularityApiService
 ): ActorPopularityRemoteDataSource {
     override suspend fun getPopularActors(language: String): ActorPopularityListDto {
