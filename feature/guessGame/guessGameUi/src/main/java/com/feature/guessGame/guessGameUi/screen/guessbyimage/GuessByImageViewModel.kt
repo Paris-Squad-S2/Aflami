f2f45beeba8e79993da992164e95244c1bbffacb
MuhammedWael9991 <mw26200400@gmail.com>
2025-08-14 14:04:37 +0300

    feat: implement loadQuestionSession method in GuessByImageViewModel for actor guessing game

--- feature/guessGame/guessGameUi/src/main/java/com/feature/guessGame/guessGameUi/screen/guessbyimage/GuessByImageViewModel.kt
+++ feature/guessGame/guessGameUi/src/main/java/com/feature/guessGame/guessGameUi/screen/guessbyimage/GuessByImageViewModel.kt
@@ -1,11 +1,45 @@
 package com.feature.guessGame.guessGameUi.screen.guessbyimage
 
 import com.feature.guessGame.guessGameUi.common.BaseViewModel
+import com.paris_2.domain.game.entity.GameSession
+import com.paris_2.domain.game.usecases.guessActor.GuessActorSessionUseCase
 import javax.inject.Inject
 
 class GuessByImageViewModel @Inject constructor(
+    private val guessActorSessionUseCase: GuessActorSessionUseCase,
+) : BaseViewModel<GuessCharacterUIState>(GuessCharacterUIState()) {
 
-) :
-    BaseViewModel<GuessCharacterUIState>(GuessCharacterUIState()) {
+    init {
+        loadQuestionSession()
+    }
+
+    fun loadQuestionSession() {
+        tryToExecute(
+            execute = { guessActorSessionUseCase.startNewSession(GameSession.GameLevel.EASY) },
+            onSuccess = { session ->
+                val uiQuestions = session.questions.map { q ->
+                    Question(
+                        image = q.content,
+                        answers = q.options.map { it.selectedAnswer },
+                        isCorrect = false,
+                        isSelected = false
+                    )
+                }
+
+                updateState(
+                    newState = screenState.value.copy(
+                        question = uiQuestions
+                    )
+                )
+            },
+            onError = { error ->
+                updateState(
+                    newState = screenState.value.copy(
+                        error = error
+                    )
+                )
+            }
+        )
+    }
 
 }
