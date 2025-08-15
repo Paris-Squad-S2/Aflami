419fc5b7e6b13dcf1a566c6503bccee1561f8bd8
Renad_Alalfy <renadalalfy@gmail.com>
2025-08-16 00:37:00 +0300

    Add points string resource and update ProfileDetails

--- feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/screen/components/ProfileDetails.kt
+++ feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/screen/components/ProfileDetails.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import com.feature.profile.profileUi.R
@@ -49,7 +50,7 @@ fun ProfileDetails(modifier: Modifier = Modifier, username: String,userPoints: I
             verticalAlignment = Alignment.CenterVertically
         ) {
             AppText(
-                text = "$userPoints Pts.",
+                text = stringResource(id = R.string.pts, userPoints),
                 style = Theme.textStyle.label.small,
                 color = Theme.colors.onPrimaryColors.onPrimary
             )
