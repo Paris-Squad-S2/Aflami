fc7d8f3d41d2ea99dd7e070531f36f7cb99820b6
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-15 21:47:47 +0300

    Refactor: Update user points after game completion
    This commit updates the `ResultViewModel` to save the user's total game points when the game finishes.
    
    The `ResultViewModel` now:
    - Injects `GetAccountIdUseCase` and `UpdatePointsUseCase`.
    - In its `init` block, launches a coroutine to fetch the current user's ID and then calls `updatePointsUseCase` to save the `totalGamePoints` received via navigation arguments.

--- feature/guessGame/guessGameUi/src/main/java/com/feature/guessGame/guessGameUi/screen/resultScreen/ResultViewModel.kt
+++ feature/guessGame/guessGameUi/src/main/java/com/feature/guessGame/guessGameUi/screen/resultScreen/ResultViewModel.kt
@@ -1,22 +1,36 @@
 package com.feature.guessGame.guessGameUi.screen.resultScreen
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
 import com.feature.guessGame.guessGameUi.common.BaseViewModel
 import com.feature.guessGame.guessGameUi.navigation.GuessGameDestinations
 import com.feature.guessGame.guessGameUi.navigation.QuestionType
 import com.feature.guessGame.guessGameUi.screen.guessGameScreen.DifficultySettings
 import com.feature.guessGame.guessGameUi.screen.guessGameScreen.mapper.toInt
+import com.paris_2.domain.game.usecases.UpdatePointsUseCase
+import com.paris_2.domain.user.usecase.GetAccountIdUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ResultViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-) : BaseViewModel<ResultUiState>(ResultUiState()), ResultInteractionListener {
+    private val getAccountIdUseCase: GetAccountIdUseCase,
+    private val updatePointsUseCase: UpdatePointsUseCase,
+
+    ) : BaseViewModel<ResultUiState>(ResultUiState()), ResultInteractionListener {
 
     private val args = savedStateHandle.toRoute<GuessGameDestinations.FinishGameScreen>()
     private val gameLevel = args.gameLevel
+
+    init {
+        viewModelScope.launch {
+            val userId = getAccountIdUseCase.invoke()
+            updatePointsUseCase.invoke(userId!!, args.totalGamePoints)
+        }
+    }
 
     override fun onExitClicked() {
         navigate(destination = GuessGameDestinations.GuessGameScreen)
