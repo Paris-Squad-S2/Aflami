4ed38f5e39eb48b05fc2c796b31adad3703e2555
Renad_Alalfy <renadalalfy@gmail.com>
2025-08-15 20:17:57 +0300

    Refactor: Add vertical scroll to GuessByImageScreen

--- feature/guessGame/guessGameUi/src/main/java/com/feature/guessGame/guessGameUi/screen/guessbyimage/GuessByImageScreen.kt
+++ feature/guessGame/guessGameUi/src/main/java/com/feature/guessGame/guessGameUi/screen/guessbyimage/GuessByImageScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
@@ -86,6 +88,7 @@ fun GuessByImageContent(
             Column(
                 modifier = Modifier
                     .fillMaxSize()
+                    .verticalScroll(rememberScrollState())
                     .padding(horizontal = 16.dp, vertical = 8.dp)
             ) {
                 QuestionIndicator(
