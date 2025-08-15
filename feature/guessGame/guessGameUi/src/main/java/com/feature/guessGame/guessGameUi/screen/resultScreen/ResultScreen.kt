ee03db60dc041ea55dd675cc525146cabc28dc9a
MuhammedWael9991 <mw26200400@gmail.com>
2025-08-15 11:46:09 +0300

    feat: add status bar padding to ResultScreen layout

--- feature/guessGame/guessGameUi/src/main/java/com/feature/guessGame/guessGameUi/screen/resultScreen/ResultScreen.kt
+++ feature/guessGame/guessGameUi/src/main/java/com/feature/guessGame/guessGameUi/screen/resultScreen/ResultScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -55,7 +56,10 @@ fun ResultScreenContent(
     listener: ResultInteractionListener
 
 ) {
-    Column {
+    Column(
+        modifier = Modifier
+            .statusBarsPadding()
+    ) {
         AppTopBar(
             title = stringResource(id = gameType.getTitleResId()),
             leadingIcons = listOf(
