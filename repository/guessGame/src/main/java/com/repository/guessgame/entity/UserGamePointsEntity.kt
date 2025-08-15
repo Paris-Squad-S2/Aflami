60c2e6760456ac6bcf8fb8904857c0f693c82bc9
Mahmoud Abdelnaby <mahmoud.mohamad68@gmail.com>
2025-08-15 22:28:56 +0300

    Refactor: Remove default value for userId in UserGamePointsEntity
    The `userId` in `UserGamePointsEntity` should not have a default value.
    This commit removes the default value of `0` for `userId`.

--- repository/guessGame/src/main/java/com/repository/guessgame/entity/UserGamePointsEntity.kt
+++ repository/guessGame/src/main/java/com/repository/guessgame/entity/UserGamePointsEntity.kt
@@ -6,6 +6,6 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "user_game_points")
 data class UserGamePointsEntity(
     @PrimaryKey
-    val userId: Int = 0,
+    val userId: Int ,
     val gamePoints: Int
 )
