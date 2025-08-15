c024ef93be12cd1344801e740cd71c12849b9031
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-08-15 23:01:59 +0300

    Refactor: Centralize status bar color logic in AflamiTheme
    Moved the logic for setting the status and navigation bar appearance (light/dark) from `OnboardingActivity` into the `AflamiTheme` composable. This allows for a centralized way to control the system bar colors based on the current theme (dark/light) and offers an optional override.
    
    `OnboardingActivity` now passes `isLightStatusBars = false` to `AflamiTheme` to maintain its specific dark status bar appearance.

--- feature/onboarding/onboardingUi/src/main/java/com/feature/onboarding/onboardingUi/OnboardingActivity.kt
+++ feature/onboarding/onboardingUi/src/main/java/com/feature/onboarding/onboardingUi/OnboardingActivity.kt
@@ -1,14 +1,9 @@
 package com.feature.onboarding.onboardingUi
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowInsetsControllerCompat
 import com.feature.onboarding.onboardingUi.ui.OnboardingScreen
 import com.feature.onboarding.onboardingUi.ui.main.InstallSavedAppLanguage
 import com.paris_2.aflami.designsystem.theme.AflamiTheme
@@ -28,20 +23,7 @@ class OnboardingActivity : AppCompatActivity() {
 
         setContent {
             InstallSavedAppLanguage(this)
-            AflamiTheme(settingsUseCase.isDarkTheme()) {
-
-                val context = LocalContext.current
-                val view = LocalView.current
-                val activity = context as? ComponentActivity
-
-                LaunchedEffect(Unit) {
-                    activity?.window?.also { window ->
-                        WindowInsetsControllerCompat(window, view).apply {
-                            isAppearanceLightStatusBars = false
-                            isAppearanceLightNavigationBars = false
-                        }
-                    }
-                }
+            AflamiTheme(isDarkTheme = settingsUseCase.isDarkTheme(), isLightStatusBars = false) {
                 OnboardingScreen()
             }
         }
