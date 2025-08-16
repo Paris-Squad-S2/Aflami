db65237c7ccc8c1a94a1ccd0a4af116028e97109
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-08-16 19:16:22 +0300

    Remove splash screen installation from MainActivity

--- app/src/main/java/com/paris_2/aflami/MainActivity.kt
+++ app/src/main/java/com/paris_2/aflami/MainActivity.kt
@@ -63,7 +63,6 @@ class MainActivity : AppCompatActivity() {
             releaseSplash = true
         }
 
-        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             InstallSavedAppLanguage(this)
