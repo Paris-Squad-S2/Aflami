50ad51783819477b4b49cfdd98e23cc32af5dbca
mohammed-akkad <momoo362514@gmail.com>
2025-08-17 10:00:43 +0300

    refactor profile screen

--- feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/screen/ProfileScreen.kt
+++ feature/profile/profileUi/src/main/java/com/feature/profile/profileUi/screen/ProfileScreen.kt
@@ -133,7 +133,10 @@ fun ProfileContent(
                 color = Theme.colors.stroke,
                 modifier = Modifier.padding(vertical = 24.dp)
             )
-            ProfileSetUp(interactionListener = profileInteractionListener)
+            ProfileSetUp(
+                interactionListener = profileInteractionListener,
+                typeMode = state.profile.theme.name
+            )
         }
 
 
@@ -149,7 +152,7 @@ fun ProfileContent(
         AppThemeDialog(
             isVisible = state.profile.isThemeDialogOpen,
             onDismiss = profileInteractionListener::onDismissAppearanceDialog,
-            onThemeSelected = {appearance->
+            onThemeSelected = { appearance ->
                 profileInteractionListener.onAppearanceApplyClicked(appearance)
                 val intent =
                     context.packageManager.getLaunchIntentForPackage(context.packageName)
