1659bfa88df20fc083323f3f6fe3e921c4b4d4b1
MuhammedWael9991 <mw26200400@gmail.com>
2025-08-17 12:38:13 +0300

    refactor getUserGamePoints to return Flow and improve point handling

--- domain/guessGame/src/main/java/com/paris_2/domain/game/usecases/UpdatePointsUseCase.kt
+++ domain/guessGame/src/main/java/com/paris_2/domain/game/usecases/UpdatePointsUseCase.kt
@@ -1,18 +1,17 @@
 package com.paris_2.domain.game.usecases
 
 import com.paris_2.domain.game.repositories.GamePointsRepository
+import kotlinx.coroutines.flow.first
 
 class UpdatePointsUseCase(
     private val gamePointsRepository: GamePointsRepository
 ) {
     suspend operator fun invoke(userId: Int, points: Int) {
-        val userPoints = gamePointsRepository.getUserGamePoints(userId)
-        val currentPoints = userPoints.gamePoints ?: 0
+        val userPoints = gamePointsRepository.getUserGamePoints(userId).first()
+        val currentPoints = userPoints.gamePoints
         val updatedPoints = currentPoints + points
         gamePointsRepository.saveUserGamePoints(
-            userPoints.copy(
-                gamePoints = updatedPoints
-            )
+            userPoints.copy(gamePoints = updatedPoints)
         )
     }
 }
--- domain/guessGame/src/main/java/com/paris_2/domain/game/usecases/UseHintUseCase.kt
+++ domain/guessGame/src/main/java/com/paris_2/domain/game/usecases/UseHintUseCase.kt
@@ -2,18 +2,22 @@ package com.paris_2.domain.game.usecases
 
 import com.paris_2.domain.game.entity.GameSession
 import com.paris_2.domain.game.repositories.GamePointsRepository
+import kotlinx.coroutines.flow.first
 
 class UseHintUseCase(
     private val gamePointsRepository: GamePointsRepository
 ) {
     suspend operator fun invoke(session: GameSession, userId: Int): Boolean {
-        val userPoints = gamePointsRepository.getUserGamePoints(userId)
+        val userPoints = gamePointsRepository.getUserGamePoints(userId).first()
+
         return if (userPoints.gamePoints >= 10) {
             session.getCurrentQuestion()?.usedHint = true
             gamePointsRepository.saveUserGamePoints(
                 userPoints.copy(gamePoints = userPoints.gamePoints - 10)
             )
             true
-        } else false
+        } else {
+            false
+        }
     }
 }
