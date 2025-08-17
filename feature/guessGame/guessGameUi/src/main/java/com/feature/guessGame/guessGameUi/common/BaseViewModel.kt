991cff6d18ef9519eac9daacb609eefda5eb8a22
MuhammedWael9991 <mw26200400@gmail.com>
2025-08-17 14:01:34 +0300

    refactor BaseViewModel to streamline flow collection and error handling

--- feature/guessGame/guessGameUi/src/main/java/com/feature/guessGame/guessGameUi/common/BaseViewModel.kt
+++ feature/guessGame/guessGameUi/src/main/java/com/feature/guessGame/guessGameUi/common/BaseViewModel.kt
@@ -11,6 +11,9 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -61,17 +64,9 @@ open class BaseViewModel<S> @Inject constructor(
         onError: (String) -> Unit = {},
         scope: CoroutineScope = viewModelScope
     ): Job {
-        val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
-            onError(throwable.message ?: "Unexpected error")
-        }
-        return scope.launch(exceptionHandler) {
-            try {
-                flow.collect { value ->
-                    onEach(value)
-                }
-            } catch (e: Exception) {
-                onError(e.message ?: "Unexpected error")
-            }
-        }
+        return flow
+            .onEach { value -> onEach(value) }
+            .catch { e -> onError(e.message ?: "Unexpected error") }
+            .launchIn(scope)
     }
 }
