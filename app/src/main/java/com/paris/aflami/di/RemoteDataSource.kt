9833434d96343480462a65c594e504aa6ee9e5a9
Zeinab979 <zeinabwhdan3677@gmail.com>
2025-08-22 01:43:44 +0300

    add annotation unused

--- app/src/main/java/com/paris/aflami/di/RemoteDataSource.kt
+++ app/src/main/java/com/paris/aflami/di/RemoteDataSource.kt
@@ -1,23 +1,24 @@
 package com.paris.aflami.di
 
-import com.repository.media.MovieRemoteDataSourceImpl
-import com.repository.media.TvShowDetailsRemoteDataSourceImpl
-import com.repository.media.datasource.remote.TvShowDetailsRemoteDataSource
 import com.repository.guessgame.datasource.remote.ActorPopularityRemoteDataSource
 import com.repository.media.ActorPopularityRemoteDataSourceImpl
 import com.repository.media.GenresRemoteDataSourceImpl
 import com.repository.media.MediaRemoteDataSourceImpl
+import com.repository.media.MovieRemoteDataSourceImpl
 import com.repository.media.SearchRemoteDataSourceImpl
+import com.repository.media.TvShowDetailsRemoteDataSourceImpl
 import com.repository.media.datasource.remote.GenresRemoteDataSource
 import com.repository.media.datasource.remote.MediaRemoteDataSource
-import com.repository.media.datasource.remote.SearchRemoteDataSource
 import com.repository.media.datasource.remote.MovieRemoteDataSource
+import com.repository.media.datasource.remote.SearchRemoteDataSource
+import com.repository.media.datasource.remote.TvShowDetailsRemoteDataSource
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+@Suppress("unused")
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RemoteDataSourceModule {
