8b58c686ebbf7ab570928df587dc26832f0564ad
MuhammedWael9991 <mw26200400@gmail.com>
2025-08-22 00:57:42 +0300

    refactor remove unused drawWithContent block in Shimmerable

--- feature/home/homeUi/src/main/java/com/feature/home/homeUi/screen/home/components/Shimmerable.kt
+++ feature/home/homeUi/src/main/java/com/feature/home/homeUi/screen/home/components/Shimmerable.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.graphics.Shape
@@ -22,7 +21,4 @@ fun Modifier.shimmerable(
     return this
         .shimmer()
         .background(shape = shape, color = color)
-        .drawWithContent {
-        }
-
 }
